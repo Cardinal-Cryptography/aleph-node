d9579ca4c6014adb81f1882d729b9ff3ec569f8b
Joshua J. Bouw <dev@joshuajbouw.com>
2021-02-08 14:33:12 +0700

    Add temp module with code that will come from Aleph later

--- finality-aleph/src/communication/mod.rs
+++ finality-aleph/src/communication/mod.rs
@@ -1,22 +1,13 @@
 mod gossip;
 pub(super) mod peer;
 
-use crate::communication::gossip::GossipValidator;
+use crate::{communication::gossip::GossipValidator, temp::EpochId};
 use sc_network_gossip::{GossipEngine, Network};
-use sp_runtime::traits::{Block, Hash, Header, NumberFor, Zero};
+use sp_runtime::traits::{Block, Hash, Header};
 use std::{
-    fmt::{Display, Formatter, Result as FmtResult},
+    fmt::{Formatter, Result as FmtResult},
     sync::{Arc, Mutex},
 };
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Epoch(pub u64);
-
-impl Display for Epoch {
-    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        write!(f, "{}", self.0)
-    }
-}
 
 /// Name of the notifications protocol used by Aleph Zero. This is how messages
 /// are subscribed to to ensure that we are gossiping and communicating with our
@@ -29,6 +20,7 @@ pub struct NetworkBridge<B: Block, N: Network<B>> {
     validator: Arc<GossipValidator<B>>,
 }
 
-pub(crate) fn global_topic<B: Block>(epoch: Epoch) -> B::Hash {
-    <<B::Header as Header>::Hashing as Hash>::hash(format!("{}-GLOBAL", epoch).as_bytes())
-}
+// TODO: remove if verified it won't be use as the protocol is async.
+// pub(crate) fn global_topic<B: Block>(epoch: EpochId) -> B::Hash {
+//     <<B::Header as Header>::Hashing as Hash>::hash(format!("{}-GLOBAL", epoch).as_bytes())
+// }
--- finality-aleph/src/lib.rs
+++ finality-aleph/src/lib.rs
@@ -18,5 +18,66 @@ pub type AuthorityId = app::Public;
 
 pub type AuthoritySignature = app::Signature;
 
-// TODO: To be added from protocol, dummy for now.
-pub struct Sync {}
+pub fn check_unit_signature_with_buffer<H, N>(
+    unit: &
+)
+
+/// Temporary structs and traits until initial version of Aleph is published.
+pub(crate) mod temp {
+    use codec::{Decode, Encode};
+    use sp_runtime::traits::Hash;
+    use std::fmt::{Display, Formatter, Result as FmtResult};
+
+    #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Encode, Decode)]
+    pub struct EpochId(pub u64);
+
+    impl Display for EpochId {
+        fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    impl From<u64> for EpochId {
+        fn from(id: u64) -> Self {
+            EpochId(id)
+        }
+    }
+
+    pub struct CreatorId(pub u64);
+
+    impl From<u64> for CreatorId {
+        fn from(id: u64) -> Self {
+            CreatorId(id)
+        }
+    }
+
+    #[derive(Clone, Debug, Default, Eq, PartialEq, Hash)]
+    pub struct NodeMap<T>(Vec<T>);
+
+    #[derive(Clone, Debug, Default, PartialEq)]
+    pub struct ControlHash<H: Hash> {
+        parents: NodeMap<bool>,
+        hash: H,
+    }
+
+    pub struct CHUnit<H: Hash> {
+        creator: CreatorId,
+        round: u64,
+        epoch_id: u64,
+        hash: H,
+        control_hash: ControlHash<H>,
+        best_block: H,
+    }
+
+    pub struct Unit<H: Hash> {
+        creator: CreatorId,
+        round: u32,
+        epoch_id: EpochId,
+        hash: H,
+        control_hash: ControlHash<H>,
+        parents: NodeMap<Option<H>>,
+        best_block: H,
+    }
+}
+
+use temp::*;
