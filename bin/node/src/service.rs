1c6a193b06911b908710e3fe6ff04d3690f82e10
Michal Swietek <michal.swietek@cardinals.cc>
2021-03-18 16:20:03 +0100

    First version of aleph binary

---
+++ bin/node/src/service.rs
@@ -0,0 +1,150 @@
+//! Service and ServiceFactory implementation. Specialized wrapper over substrate service.
+
+use aleph_runtime::{self, opaque::Block, RuntimeApi};
+use sc_client_api::ExecutorProvider;
+use sc_executor::native_executor_instance;
+pub use sc_executor::NativeExecutor;
+use sc_service::{error::Error as ServiceError, Configuration, TaskManager};
+use sp_consensus_aura::sr25519::AuthorityPair as AuraPair;
+use sp_inherents::InherentDataProviders;
+use std::sync::Arc;
+
+// Our native executor instance.
+native_executor_instance!(
+    pub Executor,
+    aleph_runtime::api::dispatch,
+    aleph_runtime::native_version,
+);
+
+type FullClient = sc_service::TFullClient<Block, RuntimeApi, Executor>;
+type FullBackend = sc_service::TFullBackend<Block>;
+type FullSelectChain = sc_consensus::LongestChain<FullBackend, Block>;
+
+pub fn new_partial(
+    config: &Configuration,
+) -> Result<
+    sc_service::PartialComponents<
+        FullClient,
+        FullBackend,
+        FullSelectChain,
+        sp_consensus::DefaultImportQueue<Block, FullClient>,
+        sc_transaction_pool::FullPool<Block, FullClient>,
+        sc_consensus_aura::AuraBlockImport<Block, FullClient, Arc<FullClient>, AuraPair>,
+    >,
+    ServiceError,
+> {
+    if config.keystore_remote.is_some() {
+        return Err(ServiceError::Other(format!(
+            "Remote Keystores are not supported."
+        )));
+    }
+    let inherent_data_providers = InherentDataProviders::new();
+
+    let (client, backend, keystore_container, task_manager) =
+        sc_service::new_full_parts::<Block, RuntimeApi, Executor>(&config)?;
+    let client = Arc::new(client);
+
+    let select_chain = sc_consensus::LongestChain::new(backend.clone());
+
+    let transaction_pool = sc_transaction_pool::BasicPool::new_full(
+        config.transaction_pool.clone(),
+        config.role.is_authority().into(),
+        config.prometheus_registry(),
+        task_manager.spawn_handle(),
+        client.clone(),
+    );
+
+    let aura_block_import = sc_consensus_aura::AuraBlockImport::<_, _, _, AuraPair>::new(
+        client.clone(),
+        client.clone(),
+    );
+
+    let import_queue = sc_consensus_aura::import_queue::<_, _, _, AuraPair, _, _>(
+        sc_consensus_aura::slot_duration(&*client)?,
+        aura_block_import.clone(),
+        None,
+        client.clone(),
+        inherent_data_providers.clone(),
+        &task_manager.spawn_handle(),
+        config.prometheus_registry(),
+        sp_consensus::CanAuthorWithNativeVersion::new(client.executor().clone()),
+    )?;
+
+    Ok(sc_service::PartialComponents {
+        client,
+        backend,
+        task_manager,
+        import_queue,
+        keystore_container,
+        select_chain,
+        transaction_pool,
+        inherent_data_providers,
+        other: aura_block_import,
+    })
+}
+
+/// Builds a new service for a full client.
+pub fn new_full(config: Configuration) -> Result<TaskManager, ServiceError> {
+    let sc_service::PartialComponents {
+        client,
+        task_manager,
+        import_queue,
+        keystore_container,
+        select_chain,
+        transaction_pool,
+        inherent_data_providers,
+        other: block_import,
+        ..
+    } = new_partial(&config)?;
+
+    let (network, _, _, network_starter) =
+        sc_service::build_network(sc_service::BuildNetworkParams {
+            config: &config,
+            client: client.clone(),
+            transaction_pool: transaction_pool.clone(),
+            spawn_handle: task_manager.spawn_handle(),
+            import_queue,
+            on_demand: None,
+            block_announce_validator_builder: None,
+        })?;
+
+    let role = config.role.clone();
+    let force_authoring = config.force_authoring;
+    let backoff_authoring_blocks: Option<()> = None;
+    let prometheus_registry = config.prometheus_registry().cloned();
+
+    if role.is_authority() {
+        let proposer_factory = sc_basic_authorship::ProposerFactory::new(
+            task_manager.spawn_handle(),
+            client.clone(),
+            transaction_pool,
+            prometheus_registry.as_ref(),
+        );
+
+        let can_author_with =
+            sp_consensus::CanAuthorWithNativeVersion::new(client.executor().clone());
+
+        let aura = sc_consensus_aura::start_aura::<_, _, _, _, _, AuraPair, _, _, _, _>(
+            sc_consensus_aura::slot_duration(&*client)?,
+            client.clone(),
+            select_chain,
+            block_import,
+            proposer_factory,
+            network.clone(),
+            inherent_data_providers.clone(),
+            force_authoring,
+            backoff_authoring_blocks,
+            keystore_container.sync_keystore(),
+            can_author_with,
+        )?;
+
+        // the AURA authoring task is considered essential, i.e. if it
+        // fails we take down the service with it.
+        task_manager
+            .spawn_essential_handle()
+            .spawn_blocking("aura", aura);
+    }
+
+    network_starter.start_network();
+    Ok(task_manager)
+}
