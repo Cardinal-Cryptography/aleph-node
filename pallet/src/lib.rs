449ed9d5cd2c8f189ef0f04195f90f8e4b46eddb
Michał Seweryn <michal.seweryn@cardinals.cc>
2021-04-06 13:39:11 +0200

    Basic pallet

--- pallet/src/lib.rs
+++ pallet/src/lib.rs
@@ -1,3 +1,21 @@
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+    use super::*;
+    use std::marker::PhantomData;
+
+    #[pallet::generate_store($visibility_of_trait_store trait Store)]
+    pub struct Pallet<T>(PhantomData<T>);
+
+    // #[pallet::storage]
+    // #[pallet::getter(fn authorities)]
+    // pub(super) type Authorities<T: Config> = StorageValue<_, Vec<T::AuthorityId>, ValueQuery>;
+
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ primitives/src/lib.rs
@@ -0,0 +1,4 @@
+use sp_core::crypto::KeyTypeId;
+
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"alp0");
+
