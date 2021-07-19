41c32dd9c5accf856aaeadb5b0b096ff0c5106b7
filip <fbielejec@gmail.com>
2021-07-19 22:34:21 +0200

    create dir

--- docker/run_node.sh
+++ docker/run_node.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+if [[ -z "$CHAIN_NAMEBATCHNUM" ]]; then
+  echo "You must provide CHAIN_NAME environment variable" 1>&2
+  exit 1
+fi
+
+if [[ -z "$BASE_PATH" ]]; then
+  echo "You must provide BASE_PATH environment variable" 1>&2
+  exit 1
+fi
+
+if [[ -z "$NODE_NAME" ]]; then
+  echo "You must provide NODE_NAME environment variable" 1>&2
+  exit 1
+fi
+
+if [[ -z "$NODE_KEY_PATH" ]]; then
+  echo "You must provide NODE_KEY_PATH environment variable" 1>&2
+  exit 1
+fi
+
+if [[ -z "$RESERVED_NODES" ]]; then
+  echo "You must provide RESERVED_NODES environment variable" 1>&2
+  exit 1
+fi
+
 ./aleph-node --validator \
  --chain $CHAIN_NAME \
  --base-path $BASE_PATH \
