96739797afffd15451791f85287abe0f5e3c7406
maciejnems <macieknems@gmail.com>
2021-08-10 14:59:36 +0200

    Remove session and block time asserts

--- bin/node/src/service.rs
+++ bin/node/src/service.rs
@@ -196,20 +196,12 @@ pub fn new_full(mut config: Configuration) -> Result<TaskManager, ServiceError> 
             .session_period(&BlockId::Number(Zero::zero()))
             .unwrap(),
     );
-    assert!(
-        session_period.0 > 0,
-        "SessionPeriod needs to be strictly bigger than 0"
-    );
 
     let millisecs_per_block = MillisecsPerBlock(
         client
             .runtime_api()
             .millisecs_per_block(&BlockId::Number(Zero::zero()))
             .unwrap(),
-    );
-    assert!(
-        millisecs_per_block.0 > 0,
-        "MillisecsPerBlock needs to be strictly bigger than 0"
     );
 
     let role = config.role.clone();
