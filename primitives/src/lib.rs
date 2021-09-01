0b9924111b37aef36dd7dc0ac2abb827d0e18506
Michal Swietek <michal.swietek@cardinals.cc>
2021-09-01 20:58:06 +0200

    change package versions in Cargo.toml

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -15,7 +15,6 @@ mod app {
     use sp_application_crypto::{app_crypto, ed25519};
     app_crypto!(ed25519, crate::KEY_TYPE);
 }
-pub use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 
 sp_application_crypto::with_pair! {
     pub type AuthorityPair = app::Pair;
