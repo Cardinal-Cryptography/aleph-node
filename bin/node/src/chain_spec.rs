e4c3d4f3b8431db1e02a016bd0353b3719b38dcc
filip <fbielejec@gmail.com>
2021-09-02 16:25:27 +0200

    refactoring chain spec generation

--- bin/node/src/chain_spec.rs
+++ bin/node/src/chain_spec.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use aleph_primitives::{
     AuthorityId as AlephId, DEFAULT_MILLISECS_PER_BLOCK, DEFAULT_SESSION_PERIOD,
 };
@@ -9,23 +7,18 @@ use aleph_runtime::{
 };
 use hex_literal::hex;
 use sc_service::ChainType;
-use sp_application_crypto::key_types;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
-use sp_core::{ed25519, sr25519, Pair, Public};
+use sp_core::{sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
-use std::{env::VarError, fmt::Display, str::FromStr};
 
 const FAUCET_HASH: [u8; 32] =
     hex!("eaefd9d9b42915bda608154f17bb03e407cbf244318a0499912c2fb1cd879b74");
 
-pub(crate) const LOCAL_AUTHORITIES: [&str; 8] = [
+pub const LOCAL_AUTHORITIES: [&str; 8] = [
     "Damian", "Tomasz", "Zbyszko", "Hansu", "Adam", "Matt", "Antoni", "Michal",
 ];
 
-pub(crate) const KEY_PATH: &str = "/tmp/authorities_keys";
-
-pub(crate) const TESTNET_ID: &str = "a0tnet1";
-pub(crate) const DEVNET_ID: &str = "dev";
+pub const DEVNET_ID: &str = "dev";
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
@@ -49,12 +42,12 @@ where
 
 #[derive(Clone)]
 pub struct AuthorityKeys {
-    account_id: AccountId,
-    aura_key: AuraId,
-    aleph_key: AlephId,
+    pub account_id: AccountId,
+    pub aura_key: AuraId,
+    pub aleph_key: AlephId,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct ChainParams {
     session_period: u32,
     millisecs_per_block: u64,
@@ -63,113 +56,17 @@ pub struct ChainParams {
 impl ChainParams {
     pub fn from_cli(session_period: Option<u32>, millisecs_per_block: Option<u64>) -> Self {
         ChainParams {
-            session_period: Self::param(
-                "session period",
-                session_period,
-                "SESSION_PERIOD",
-                DEFAULT_SESSION_PERIOD,
-            ),
-            millisecs_per_block: Self::param(
-                "millisecs per block",
-                millisecs_per_block,
-                "MILLISECS_PER_BLOCK",
-                DEFAULT_MILLISECS_PER_BLOCK,
-            ),
-        }
-    }
-
-    fn param<T: FromStr + Display>(
-        debug_name: &str,
-        cli_value: Option<T>,
-        var: &str,
-        default: T,
-    ) -> T
-    where
-        <T as FromStr>::Err: ToString,
-    {
-        cli_value
-            .or_else(|| Self::parse_env_var(var))
-            .unwrap_or_else(|| {
-                log::debug!(
-                    "{} parameter not specified, using default value: {}",
-                    debug_name,
-                    default
-                );
-                default
-            })
-    }
-
-    fn parse_env_var<T: FromStr>(var: &str) -> Option<T>
-    where
-        <T as FromStr>::Err: ToString,
-    {
-        match std::env::var(var) {
-            Ok(value) => match value.parse() {
-                Ok(value) => Some(value),
-                Err(err) => {
-                    panic!(
-                        "error parsing environment variable {}: {}",
-                        var,
-                        err.to_string()
-                    );
-                }
-            },
-            Err(VarError::NotPresent) => None,
-            Err(err @ VarError::NotUnicode(_)) => {
-                panic!("environment variable {} is not unicode: {}", var, err);
-            }
+            session_period: session_period.unwrap_or(DEFAULT_SESSION_PERIOD),
+            millisecs_per_block: millisecs_per_block.unwrap_or(DEFAULT_MILLISECS_PER_BLOCK),
         }
     }
 }
 
-fn read_keys(n_members: usize) -> Vec<AuthorityKeys> {
-    let auth_keys: HashMap<u32, Vec<[u8; 32]>> =
-        if let Ok(auth_keys) = std::fs::read_to_string(KEY_PATH) {
-            serde_json::from_str(&auth_keys).expect("should contain list of keys")
-        } else {
-            return Default::default();
-        };
-
-    let aura_keys = auth_keys
-        .get(&key_types::AURA.into())
-        .unwrap()
-        .iter()
-        .copied()
-        .map(|bytes| AuraId::from(sr25519::Public::from_raw(bytes)));
-
-    let aleph_keys = auth_keys
-        .get(&aleph_primitives::KEY_TYPE.into())
-        .unwrap()
-        .iter()
-        .copied()
-        .map(|bytes| AlephId::from(ed25519::Public::from_raw(bytes)));
-
-    let account_ids = LOCAL_AUTHORITIES
-        .iter()
-        .map(get_account_id_from_seed::<sr25519::Public>);
-
-    aura_keys
-        .zip(aleph_keys)
-        .zip(account_ids)
-        .take(n_members)
-        .map(|((aura_key, aleph_key), account_id)| AuthorityKeys {
-            account_id,
-            aura_key,
-            aleph_key,
-        })
-        .collect()
-}
-
-pub fn development_config(chain_params: ChainParams) -> Result<ChainSpec, String> {
+pub fn development_config(
+    chain_params: ChainParams,
+    authorities: Vec<AuthorityKeys>,
+) -> Result<ChainSpec, String> {
     let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
-
-    let n_members = std::fs::read_to_string("/tmp/n_members")
-        .expect("Committee size is not specified")
-        .trim()
-        .parse::<usize>()
-        .expect("Wrong committee size");
-
-    let authorities = read_keys(n_members);
 
     let rich_accounts: Vec<_> = [
         "Alice",
@@ -195,7 +92,7 @@ pub fn development_config(chain_params: ChainParams) -> Result<ChainSpec, String
         DEVNET_ID,
         ChainType::Development,
         move || {
-            testnet_genesis(
+            genesis(
                 wasm_binary,
                 // Initial PoA authorities
                 authorities.clone(),
@@ -226,16 +123,12 @@ pub fn development_config(chain_params: ChainParams) -> Result<ChainSpec, String
     ))
 }
 
-pub fn testnet1_config(chain_params: ChainParams) -> Result<ChainSpec, String> {
+pub fn config(
+    chain_params: ChainParams,
+    authorities: Vec<AuthorityKeys>,
+    chain_id: &str,
+) -> Result<ChainSpec, String> {
     let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
-
-    let n_members = std::fs::read_to_string("/tmp/n_members")
-        .expect("Committee size is not specified")
-        .trim()
-        .parse::<usize>()
-        .expect("Wrong committee size");
-
-    let authorities = read_keys(n_members);
 
     let sudo_account: AccountId = hex![
         // 5F4SvwaUEQubiqkPF8YnRfcN77cLsT2DfG4vFeQmSXNjR7hD
@@ -243,21 +136,23 @@ pub fn testnet1_config(chain_params: ChainParams) -> Result<ChainSpec, String> {
     ]
     .into();
 
-    // Give money to the faucet account.
+    // Give money to the faucet account
     let faucet: AccountId = FAUCET_HASH.into();
     let rich_accounts = vec![faucet, sudo_account.clone()];
+
     Ok(ChainSpec::from_genesis(
         // Name
-        "Aleph Zero",
+        "AlephZero",
         // ID
-        TESTNET_ID,
+        chain_id,
         ChainType::Live,
         move || {
-            testnet_genesis(
+            genesis(
                 wasm_binary,
+                // Initial PoA authorities
                 authorities.clone(),
-                sudo_account.clone(),
                 // Pre-funded accounts
+                sudo_account.clone(),
                 rich_accounts.clone(),
                 chain_params,
             )
@@ -272,7 +167,7 @@ pub fn testnet1_config(chain_params: ChainParams) -> Result<ChainSpec, String> {
         Some(
             [(
                 "tokenSymbol".to_string(),
-                serde_json::Value::String("TZERO".into()),
+                serde_json::Value::String("DZERO".into()),
             )]
             .iter()
             .cloned()
@@ -283,21 +178,21 @@ pub fn testnet1_config(chain_params: ChainParams) -> Result<ChainSpec, String> {
     ))
 }
 
-/// Configure initial storage state for FRAME modules.
-fn testnet_genesis(
+/// Configure initial storage state for FRAME modules
+fn genesis(
     wasm_binary: &[u8],
     authorities: Vec<AuthorityKeys>,
     root_key: AccountId,
     rich_accounts: Vec<AccountId>,
     chain_params: ChainParams,
 ) -> GenesisConfig {
-    let session_period = chain_params.session_period;
-    let millisecs_per_block = chain_params.millisecs_per_block;
-    log::debug!(
-        "session-period: {}, millisecs-per-block: {}",
+    let ChainParams {
         session_period,
-        millisecs_per_block
-    );
+        millisecs_per_block,
+    } = chain_params;
+
+    log::debug!("chain parameters {:?}", &chain_params);
+
     GenesisConfig {
         system: SystemConfig {
             // Add Wasm runtime to storage.
@@ -322,10 +217,7 @@ fn testnet_genesis(
             key: root_key,
         },
         aleph: AlephConfig {
-            authorities: authorities
-                .iter()
-                .map(|auth| auth.aleph_key.clone())
-                .collect(),
+            authorities: vec![],
             session_period,
             millisecs_per_block,
         },
---
+++ bin/node/src/commands.rs
@@ -0,0 +1,178 @@
+use crate::chain_spec::{self, get_account_id_from_seed, AuthorityKeys, LOCAL_AUTHORITIES};
+use crate::cli::{Cli, ExtraParams};
+use aleph_primitives::AuthorityId as AlephId;
+use log::info;
+use sc_cli::{CliConfiguration, Error, KeystoreParams, NodeKeyParams, SharedParams};
+use sc_keystore::LocalKeystore;
+use sc_service::config::{BasePath, KeystoreConfig};
+use sp_application_crypto::key_types;
+use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+use sp_core::crypto::KeyTypeId;
+use sp_core::{ed25519, sr25519};
+use sp_keystore::SyncCryptoStore;
+use std::io::Write;
+use std::sync::Arc;
+use structopt::StructOpt;
+
+/// The `bootstrap-chain` command is used generate keys for the genesis authorities
+/// keys are written to the keystore of the authorities
+/// and the chain specification is printed to stdout in the JSON format
+#[derive(Debug, StructOpt)]
+pub struct BootstrapChainCmd {
+    /// Force raw genesis storage output.
+    #[structopt(long = "raw")]
+    pub raw: bool,
+
+    #[allow(missing_docs)]
+    #[structopt(flatten)]
+    pub keystore_params: KeystoreParams,
+
+    #[allow(missing_docs)]
+    #[structopt(flatten)]
+    pub shared_params: SharedParams,
+
+    #[allow(missing_docs)]
+    #[structopt(flatten)]
+    pub node_key_params: NodeKeyParams,
+}
+
+impl CliConfiguration for BootstrapChainCmd {
+    fn shared_params(&self) -> &SharedParams {
+        &self.shared_params
+    }
+
+    fn node_key_params(&self) -> Option<&NodeKeyParams> {
+        Some(&self.node_key_params)
+    }
+}
+
+impl BootstrapChainCmd {
+    fn upsert_aura_key(
+        &self,
+        keystore: &LocalKeystore,
+        key_type: KeyTypeId,
+    ) -> Result<AuraId, Error> {
+        let key = SyncCryptoStore::sr25519_public_keys(&*keystore, key_type)
+            .into_iter()
+            .next()
+            .map_or_else(
+                || {
+                    SyncCryptoStore::sr25519_generate_new(&*keystore, key_type, None)
+                        .map_err(|_| Error::KeyStoreOperation)
+                },
+                Ok,
+            );
+
+        match key {
+            Ok(k) => {
+                let bytes = k.as_array_ref();
+                Ok(AuraId::from(sr25519::Public::from_raw(*bytes)))
+            }
+            Err(why) => panic!("{}", why),
+        }
+    }
+
+    fn upsert_aleph_key(
+        &self,
+        keystore: &LocalKeystore,
+        key_type: KeyTypeId,
+    ) -> Result<AlephId, Error> {
+        let key = SyncCryptoStore::ed25519_public_keys(&*keystore, key_type)
+            .into_iter()
+            .next()
+            .map_or_else(
+                || {
+                    SyncCryptoStore::ed25519_generate_new(&*keystore, key_type, None)
+                        .map_err(|_| Error::KeyStoreOperation)
+                },
+                Ok,
+            );
+
+        match key {
+            Ok(k) => {
+                let bytes = k.as_array_ref();
+                Ok(AlephId::from(ed25519::Public::from_raw(*bytes)))
+            }
+            Err(why) => panic!("{}", why),
+        }
+    }
+
+    pub fn run(&self, cli: &Cli) -> Result<(), Error> {
+        let chain_id = self.shared_params.chain_id(self.shared_params.is_dev());
+
+        let mut genesis_authorities: Vec<AuthorityKeys> = Vec::new();
+
+        LOCAL_AUTHORITIES.iter().for_each(|authority| {
+            let authority_keystore = self
+                .open_keystore(authority, &chain_id)
+                .unwrap_or_else(|_| panic!("Cannot open keystore for {}", authority));
+
+            let aura_key = self
+                .upsert_aura_key(&authority_keystore, key_types::AURA)
+                .unwrap();
+            let aleph_key = self
+                .upsert_aleph_key(&authority_keystore, aleph_primitives::KEY_TYPE)
+                .unwrap();
+            let account_id = get_account_id_from_seed::<sr25519::Public>(authority);
+
+            // NOTE: we could generate libp2p secrets here and add PeerIds to bootstrap nodes list,
+            // see Substrate's GenerateNodeKeyCmd
+
+            genesis_authorities.push(AuthorityKeys {
+                account_id,
+                aura_key,
+                aleph_key,
+            });
+        });
+
+        info!("Building chain spec");
+
+        let ExtraParams {
+            session_period,
+            millisecs_per_block,
+        } = cli.extra;
+
+        let chain_params = chain_spec::ChainParams::from_cli(session_period, millisecs_per_block);
+        let chain_spec = match chain_id.as_str() {
+            chain_spec::DEVNET_ID => {
+                chain_spec::development_config(chain_params, genesis_authorities)
+            }
+            _ => chain_spec::config(chain_params, genesis_authorities, &chain_id),
+        };
+
+        match chain_spec {
+            Ok(spec) => {
+                let json = sc_service::chain_ops::build_spec(&spec, self.raw)?;
+                if std::io::stdout().write_all(json.as_bytes()).is_err() {
+                    let _ = std::io::stderr().write_all(b"Error writing to stdout\n");
+                }
+            }
+            Err(why) => panic!("{}", why),
+        }
+
+        Ok(())
+    }
+
+    fn open_keystore(&self, authority: &str, chain_id: &str) -> Result<Arc<LocalKeystore>, Error> {
+        let base_path: BasePath = self
+            .shared_params
+            .base_path()
+            .unwrap()
+            .path()
+            .join(authority)
+            .into();
+
+        info!(
+            "Writing to keystore for authority {} and chain id {} under path {:?}",
+            authority, chain_id, base_path
+        );
+
+        let config_dir = base_path.config_dir(chain_id);
+        match self.keystore_params.keystore_config(&config_dir)? {
+            (_, KeystoreConfig::Path { path, password }) => {
+                Ok(Arc::new(LocalKeystore::open(path, password)?))
+            }
+            _ => unreachable!("keystore_config always returns path and password; qed"),
+        }
+    }
+}
