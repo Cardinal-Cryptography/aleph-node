04583c5134ac0c7212e28d8a29fc4236d814ca4a
Michal Swietek <michal.swietek@cardinals.cc>
2021-09-27 13:31:28 +0200

    Addressing filip comments v2

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -358,6 +358,7 @@ impl pallet_vesting::Config for Runtime {
 
 pub const MILLICENTS: Balance = 1_000_000_000;
 pub const CENTS: Balance = 1_000 * MILLICENTS; // assume this is worth about a cent.
+                                               // This value is copied from polkadot, we should do our own calculations
 pub const fn deposit(items: u32, bytes: u32) -> Balance {
     items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
 }
