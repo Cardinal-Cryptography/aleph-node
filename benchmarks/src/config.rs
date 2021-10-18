b272295efc51076746bc9f5fbc9ba6b83aaaf3dc
filip <fbielejec@gmail.com>
2021-10-18 13:07:16 +0200

    init

---
+++ benchmarks/src/config.rs
@@ -0,0 +1,13 @@
+use clap::{AppSettings, Parser};
+
+#[derive(Parser)]
+struct Config {
+    /// Sets a custom config file. Could have been an Option<T> with no default too
+    #[clap(short, long, default_value = "default.conf")]
+    config: String,
+    /// Some input. Because this isn't an Option<T> it's required to be used
+    input: String,
+    /// A level of verbosity, and can be used multiple times
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+}
---
+++ benchmarks/src/main.rs
@@ -0,0 +1,5 @@
+mod config;
+
+fn main() {
+    println!("Hello, world!");
+}
