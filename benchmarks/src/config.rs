c0f6fa17d552e2bfc84d47dba46c87b390dadc8b
filip <fbielejec@gmail.com>
2021-10-30 13:52:23 +0200

    presigned txs

--- benchmarks/src/config.rs
+++ benchmarks/src/config.rs
@@ -1,14 +1,5 @@
 use clap::Parser;
-use sp_core::{sr25519, Pair};
 use std::{fs, path::PathBuf};
-
-//   let TOTAL_TRANSACTIONS = argv.total_transactions ? argv.total_transactions : 25000;
-//   let TPS = argv.scale ? argv.scale : 100;
-//   let TOTAL_THREADS = argv.total_threads ? argv.total_threads : 10;
-
-//   let TOTAL_BATCHES = TOTAL_TRANSACTIONS / TPS;
-//   let TRANSACTION_PER_BATCH = TPS / TOTAL_THREADS;
-//   let TOTAL_USERS = TPS;
 
 /// Benchmarking tool expects to find key phrase files for the accounts
 /// to send txs from under <BASE_PATH>/<ACCOUNT_ID>/<KEY_FILENAME>
@@ -17,15 +8,15 @@ use std::{fs, path::PathBuf};
 pub struct Config {
     /// URL address(es) of the nodes to send transactions to
     #[clap(long, default_value = "127.0.0.1:9943")]
-    pub node_url: String,
+    pub nodes: Vec<String>,
 
     /// how many transactions to send
-    #[clap(long, default_value = "100")]
+    #[clap(long, default_value = "10000")]
     pub transactions: u64,
 
-    /// what througput to use (transactions/s)
-    #[clap(long, default_value = "10")]
-    pub througput: u64,
+    /// what throughput to use (transactions/s)
+    #[clap(long, default_value = "2000")]
+    pub throughput: u64,
 
     /// how many threads to create
     #[clap(long, default_value = "10")]
@@ -36,37 +27,11 @@ pub struct Config {
     pub phrase: String,
 }
 
-// TODO : read from file or stdin
-// pub fn read_keypair(file: String) -> sr25519::Pair {
-//     let phrase = fs::read_to_string(&file)
-//         .unwrap_or_else(|_err| panic!("Could not read the phrase form the secret file: {}", file));
-//     sr25519::Pair::from_phrase(&phrase, None)
-//         .expect("not a secret phrase")
-//         .0
-// }
-
 pub fn read_phrase(phrase: String) -> String {
     let file = PathBuf::from(&phrase);
     if file.is_file() {
-        std::fs::read_to_string(phrase)
-            .unwrap()
-            .trim_end()
-            .to_owned()
+        fs::read_to_string(phrase).unwrap().trim_end().to_owned()
     } else {
         phrase.into()
     }
 }
-
-// pub fn accounts(
-//     base_path: String,
-//     account_ids: Vec<String>,
-//     key_filename: String,
-// ) -> Vec<sr25519::Pair> {
-//     account_ids
-//         .into_iter()
-//         .map(|id| {
-//             let file = format!("{}/{}/{}", &base_path, id, key_filename);
-//             read_keypair(file)
-//         })
-//         .collect::<Vec<sr25519::Pair>>()
-// }
--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -4,22 +4,17 @@ use clap::Parser;
 use codec::{Compact, Encode};
 use config::Config;
 use futures::future::join_all;
-use futures::Future;
 use hdrhistogram::Histogram as HdrHistogram;
-use log::{debug, info};
-use ndarray::Array3;
-use sp_core::{sr25519, DeriveJunction, Pair, H256};
-// use std::fmt::Result;
-use std::os::unix::thread;
-use std::pin::Pin;
+use log::{debug, info, warn};
+use sp_core::{sr25519, DeriveJunction, Pair};
+use sp_runtime::MultiAddress;
 use std::sync::{Arc, Mutex};
-use std::task::{Context, Poll};
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 use substrate_api_client::rpc::WsRpcClient;
 use substrate_api_client::{
-    compose_call, compose_extrinsic, compose_extrinsic_offline, AccountId, Api, GenericAddress,
-    UncheckedExtrinsicV4, XtStatus,
+    compose_call, compose_extrinsic_offline, AccountId, Api, GenericAddress, UncheckedExtrinsicV4,
+    XtStatus,
 };
 
 #[tokio::main]
@@ -31,9 +26,15 @@ async fn main() -> Result<(), anyhow::Error> {
     let (account, _seed) =
         sr25519::Pair::from_phrase(&config::read_phrase(config.phrase), None).unwrap();
 
-    let connection = create_connection(format!("ws://{}", &config.node_url));
-    let transfer_value = connection.get_existential_deposit().unwrap();
-    let total_users = config.througput;
+    let pool = create_connection_pool(config.nodes);
+    let connection = pool.get(0).unwrap();
+
+    let total_users = config.throughput;
+
+    let existential_deposit = connection.get_existential_deposit().unwrap();
+    let transfer_amount = 1u128;
+    let tx_fee = 1000_000_000; /*73_268_000u128;*/
+    let total_amount = existential_deposit + tx_fee + transfer_amount;
 
     debug!(
         "Using account {} to derive and fund accounts",
@@ -42,90 +43,73 @@ async fn main() -> Result<(), anyhow::Error> {
 
     assert!(
         get_funds(connection.clone(), &AccountId::from(account.public()))
-            .ge(&(transfer_value * total_users as u128)),
-        "Too poor"
+            .ge(&(total_amount * total_users as u128)),
+        "Account is too poor"
     );
 
     let mut users = Vec::with_capacity(total_users as usize);
     let mut nonces = Vec::with_capacity(total_users as usize);
+    let mut account_nonce = get_nonce(connection.clone(), &AccountId::from(account.public()));
 
     for index in 0..total_users {
         let path = Some(DeriveJunction::soft(index));
         let (derived, _seed) = account.clone().derive(path.into_iter(), None).unwrap();
 
-        // let call = compose_call!(
-        //     connection.metadata,
-        //     "Balances",
-        //     "transfer",
-        //     GenericAddress::Id(AccountId::from(derived.public())),
-        //     Compact(transfer_value)
-        // );
+        // transfer existential deposit + tx fees (upper bound) + transfer_amount
+        let tx = sign_tx(
+            connection.clone(),
+            account.clone(),
+            account_nonce,
+            AccountId::from(derived.public()),
+            total_amount,
+        )
+        .await;
 
-        // let nonce = get_nonce(connection.clone(), &AccountId::from(account.public()));
+        let hash = if index.eq(&(total_users - 1)) {
+            connection
+                .send_extrinsic(tx.hex_encode(), XtStatus::Finalized)
+                .expect("Could not send transaction")
+        } else {
+            connection
+                .send_extrinsic(tx.hex_encode(), XtStatus::Ready)
+                .expect("Could not send transaction")
+        };
 
-        // let tx: UncheckedExtrinsicV4<_> = compose_extrinsic_offline!(
-        //     account,
-        //     call,
-        //     nonce,
-        //     Era::Immortal,
-        //     connection.genesis_hash,
-        //     connection.genesis_hash,
-        //     connection.runtime_version.spec_version,
-        //     connection.runtime_version.transaction_version
-        // );
+        account_nonce += 1;
 
-        let tx: UncheckedExtrinsicV4<_> = compose_extrinsic!(
-            connection.clone().set_signer(account.clone()),
-            "Balances",
-            "transfer",
-            GenericAddress::Id(AccountId::from(derived.public())),
-            Compact(transfer_value)
-        );
-
-        // send and watch transfer tx until finalized
-        let tx_hash = connection
-            .send_extrinsic(tx.hex_encode(), XtStatus::Finalized)
-            .unwrap()
-            .unwrap();
-
-        let funds = get_funds(connection.clone(), &AccountId::from(derived.public()));
         let nonce = get_nonce(connection.clone(), &AccountId::from(derived.public()));
 
         println!(
-            "account {} with nonce {} received funds in tx {}. Account free funds {}",
+            "account {} with nonce {} will receive funds, tx hash {:?}",
             &derived.public(),
             nonce,
-            tx_hash,
-            funds
+            hash
         );
 
         nonces.push(nonce);
         users.push(derived);
     }
 
+    // sleep some to wait for the last tx hash to be finalized
+    // async { sleep(Duration::from_millis(2000)) }.await;
+
     debug!("all accounts have received funds");
 
-    // let mut nonces = Vec::with_capacity(total_users as usize);
-
     let total_threads = config.threads;
-    let total_batches = config.transactions / config.througput;
+    let total_batches = config.transactions / config.throughput;
     let users_per_thread = total_users / total_threads;
-    let transaction_per_batch = config.througput / config.threads;
+    let transactions_per_batch = config.throughput / config.threads;
 
-    // TODO : prepare txs
-    // let mut txs = Array3::<UncheckedExtrinsicV4<_>>::default((
-    //     total_threads as usize,
-    //     total_batches as usize,
-    //     total_users as usize,
-    // ));
-
-    // let to = AccountId::from(account.public());
+    // prepare txs
+    let mut txs = Vec::new();
+    let mut tx_counter = 0;
 
     for thread in 0..total_threads {
+        let mut txs_batches = Vec::new();
         for batch in 0..total_batches {
+            let mut txs_batch = Vec::new();
             for user in (thread * users_per_thread)..(thread + 1) * users_per_thread {
                 let connection = connection.clone();
-
                 let from = users.get(user as usize).unwrap().to_owned();
 
                 let call = compose_call!(
@@ -133,10 +117,8 @@ async fn main() -> Result<(), anyhow::Error> {
                     "Balances",
                     "transfer",
                     GenericAddress::Id(AccountId::from(account.public())),
-                    Compact(transfer_value)
+                    Compact(transfer_amount)
                 );
-
-                // let u = users.get(user as usize).unwrap().to_owned();
 
                 let tx: UncheckedExtrinsicV4<_> = compose_extrinsic_offline!(
                     from,
@@ -150,41 +132,126 @@ async fn main() -> Result<(), anyhow::Error> {
                 );
 
                 nonces[user as usize] += 1;
+                tx_counter += 1;
+                txs_batch.push(tx);
 
-                println!("tx {:?}", tx.hex_encode());
-
-                println!("thread: {} batch: {} user: {}", thread, batch, user);
+                println!(
+                    "thread: {} batch: {} user: {}",
+                    thread,
+                    batch,
+                    &from.public()
+                );
             }
+            txs_batches.push(txs_batch);
         }
+        txs.push(txs_batches);
     }
 
-    // for batch in 0..total_batches {
-    //     let mut futures_batch = Vec::new();
+    println!("Prepared {} signed txs", tx_counter);
 
-    //     println!("Prepared batch {}", batch);
+    let histogram = Arc::new(Mutex::new(
+        HdrHistogram::<u64>::new_with_bounds(1, u64::max_value(), 3).unwrap(),
+    ));
 
-    //     for thread in 0..total_batches {
-    //         for tx in 0..transaction_per_batch {
-    // let tx = txs[thread][batch][tx];
-    //             futures_batch.push(send_tx(connection.clone(), tx));
-    //         }
-    //     }
-    //     join_all(futures_batch).await;
-    // }
+    println!(
+        "threads: {}, batches: {}, txs per batch: {}",
+        &txs.len(),
+        &txs[0].len(),
+        &txs[0][0].len()
+    );
+
+    let tick = Instant::now();
+    // send txs
+    for batch in 0..total_batches {
+        let mut futures_batch = Vec::new();
+
+        println!("Prepared batch {}", batch);
+
+        for thread in 0..total_threads {
+            println!("sending from thread {}", thread);
+
+            for tx in 0..transactions_per_batch {
+                // retrieve signed txs
+                let signed_tx = &txs[thread as usize][batch as usize][tx as usize];
+
+                // send txs using a connection pool
+                futures_batch.push(send_tx(
+                    pool.get(tx as usize % pool.len()).unwrap().to_owned(),
+                    signed_tx.to_owned(),
+                    Arc::clone(&histogram),
+                ));
+            }
+        }
+        // block on batches of futures
+        join_all(futures_batch).await;
+    }
+
+    let tock = tick.elapsed().as_millis();
+    let histogram = histogram.lock().unwrap();
+
+    println!("Summary:\n Transactions sent: {}\n Total time:        {} ms\n Slowest tx:        {} ms\n Fastest tx:        {} ms\n Average:           {:.1} ms\n Throughput:        {:.1} tx/s",
+             histogram.len (),
+             tock,
+             histogram.max (),
+             histogram.min (),
+             histogram.mean (),
+             1000.0 * histogram.len () as f64 / tock as f64
+    );
 
     Ok(())
+}
+
+async fn sign_tx(
+    connection: Api<sr25519::Pair, WsRpcClient>,
+    signer: sr25519::Pair,
+    nonce: u32,
+    to: AccountId,
+    amount: u128,
+) -> UncheckedExtrinsicV4<([u8; 2], MultiAddress<AccountId, ()>, codec::Compact<u128>)> {
+    let call = compose_call!(
+        connection.metadata,
+        "Balances",
+        "transfer",
+        GenericAddress::Id(to),
+        Compact(amount)
+    );
+
+    compose_extrinsic_offline!(
+        signer,
+        call,
+        nonce,
+        Era::Immortal,
+        connection.genesis_hash,
+        connection.genesis_hash,
+        connection.runtime_version.spec_version,
+        connection.runtime_version.transaction_version
+    )
 }
 
 async fn send_tx<Call>(
     connection: Api<sr25519::Pair, WsRpcClient>,
     tx: UncheckedExtrinsicV4<Call>,
-) -> Option<H256>
-where
+    histogram: Arc<Mutex<HdrHistogram<u64>>>,
+) where
     Call: Encode,
 {
+    let start_time = Instant::now();
+
     connection
         .send_extrinsic(tx.hex_encode(), XtStatus::Ready)
-        .expect("Could not send transaction")
+        .expect("Could not send transaction");
+
+    let elapsed_time = start_time.elapsed().as_millis();
+
+    let mut hist = histogram.lock().unwrap();
+    *hist += elapsed_time as u64;
+}
+
+fn create_connection_pool(nodes: Vec<String>) -> Vec<Api<sr25519::Pair, WsRpcClient>> {
+    nodes
+        .into_iter()
+        .map(|url| create_connection(format!("ws://{}", &url)))
+        .collect()
 }
 
 fn create_connection(url: String) -> Api<sr25519::Pair, WsRpcClient> {
@@ -192,7 +259,7 @@ fn create_connection(url: String) -> Api<sr25519::Pair, WsRpcClient> {
     match Api::<sr25519::Pair, _>::new(client) {
         Ok(api) => api,
         Err(_) => {
-            println!("[+] Can't create_connection atm, will try again in 1s");
+            warn!("[+] Can't create_connection atm, will try again in 1s");
             sleep(Duration::from_millis(1000));
             create_connection(url)
         }
