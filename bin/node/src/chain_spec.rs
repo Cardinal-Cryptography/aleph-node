3c436b8793b8f87a26a86835fa3c043094ea1780
filip <fbielejec@gmail.com>
2021-11-02 15:03:10 +0100

    expose mnemonic for dev chains to facilitate e2e testing

--- bin/node/src/chain_spec.rs
+++ bin/node/src/chain_spec.rs
@@ -7,6 +7,7 @@ use aleph_runtime::{
     AccountId, AlephConfig, AuraConfig, BalancesConfig, GenesisConfig, SessionConfig, SessionKeys,
     Signature, SudoConfig, SystemConfig, VestingConfig, WASM_BINARY,
 };
+use bip39::{Language, Mnemonic};
 use hex_literal::hex;
 use libp2p::PeerId;
 use sc_service::config::BasePath;
@@ -18,25 +19,25 @@ use sp_application_crypto::Ss58Codec;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
+use std::collections::HashSet;
+use std::fs;
 use std::{path::PathBuf, str::FromStr};
 use structopt::StructOpt;
 
-const FAUCET_HASH: [u8; 32] =
-    hex!("eaefd9d9b42915bda608154f17bb03e407cbf244318a0499912c2fb1cd879b74");
-
 pub const DEVNET_ID: &str = "dev";
+
+pub const KNOWNS_ACCOUNTS: [&str; 7] = [
+    "Alice",
+    "Alice//stash",
+    "Bob",
+    "Bob//stash",
+    "Charlie",
+    "Dave",
+    "Eve",
+];
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
-
-/// Generate a crypto pair from seed.
-pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
-    TPublic::Pair::from_string(&format!("//{}", seed), None)
-        .expect("static values are valid; qed")
-        .public()
-}
-
-type AccountPublic = <Signature as Verify>::Signer;
 
 #[derive(Clone)]
 pub struct SerializablePeerId {
@@ -71,12 +72,45 @@ impl<'de> Deserialize<'de> for SerializablePeerId {
     }
 }
 
+type AccountPublic = <Signature as Verify>::Signer;
+
+/// Generate a crypto pair from seed.
+pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
+    TPublic::Pair::from_string(&format!("//{}", seed), None)
+        .expect("static values are valid; qed")
+        .public()
+}
+
 /// Generate an account ID from seed.
 pub fn get_account_id_from_seed<TPublic: Public>(seed: &&str) -> AccountId
 where
     AccountPublic: From<<TPublic::Pair as Pair>::Public>,
 {
     AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
+}
+
+/// Given a "seed" integer generates a keypair for signing transactions
+/// mnemonic used to generate the pair is written to base_path/authority_key_file  
+/// returns a hex encoding of a public key (an AccountId)
+pub fn account_key(id: u8, base_path: BasePath, authority_key_file: String) -> AccountId {
+    let entropy = &[
+        id, 0xE4, 0x6B, 0xB1, 0x3A, 0x74, 0x6E, 0xA4, 0x1C, 0xDD, 0xE4, 0x5C, 0x90, 0x84, 0x6A,
+        0x79,
+    ];
+    let mnemonic = Mnemonic::from_entropy(entropy, Language::English)
+        .expect("Could not generate a mnemonic from entropy");
+    let (pair, _seed) = sr25519::Pair::from_entropy(entropy, None);
+    let account_id = pair.public();
+    let authority = account_id.to_string();
+
+    let dir = base_path.path().join(authority);
+    let file = dir.join(&authority_key_file);
+
+    // NOTE: we do not care if file is overwritten since the phrase is deterministic
+    fs::create_dir_all(dir).expect("Could not create directory");
+    fs::write(file, mnemonic.phrase()).expect("Could not write authority mnemonic");
+
+    account_id.into()
 }
 
 #[derive(Clone, Deserialize, Serialize)]
@@ -92,6 +126,7 @@ pub struct ChainParams {
     /// Pass the chain id.
     ///
     /// It can be a predefined one (dev) or an arbitrary chain id passed to the genesis block
+    /// `dev` chain id means that a set of known accounts will be used to form a comittee   
     #[structopt(long, value_name = "CHAIN_SPEC", default_value = "a0dnet1")]
     pub chain_id: String,
 
@@ -103,6 +138,12 @@ pub struct ChainParams {
     /// Resulting keys will be stored at: base_path/account_id/node_key_file for each node
     #[structopt(long, default_value = "p2p_secret")]
     pub node_key_file: String,
+
+    /// Specify filename to write the nodes private key to
+    /// Used in conjunction with `--n-members` argument
+    /// Resulting keys will be stored at: base_path/account_id/account_key_file for each node
+    #[structopt(long, default_value = "account_secret")]
+    pub account_key_file: String,
 
     #[structopt(long)]
     pub session_period: Option<u32>,
@@ -122,13 +163,22 @@ pub struct ChainParams {
     /// Pass the AccountIds of authorities forming the committe at the genesis
     ///
     /// Expects a delimited collection of AccountIds
-    /// If this argument is not found n_members is used instead to generate a collection of size `n_members`
-    /// filled with randomly generated Ids
-    #[structopt(long, require_delimiter = true)]
+    #[structopt(long, require_delimiter = true, conflicts_with_all = &["n_members"])]
     account_ids: Option<Vec<String>>,
 
+    /// Pass the AccountId of the sudo account
+    ///
+    /// If the chain-id is "dev" it will default to the first generated account (Alice)
+    /// and use a default pre-defined id in any other case
     #[structopt(long)]
-    n_members: Option<u32>,
+    sudo_account_id: Option<String>,
+
+    /// Pass a number of nodes that will form the committe : 1 - 255
+    ///
+    /// IMPORTANT : this method generates comittee keys from a known entropy
+    /// and should be used ONLY in development environments
+    #[structopt(long, conflicts_with_all = &["account_ids"])]
+    n_members: Option<u8>,
 }
 
 impl ChainParams {
@@ -186,14 +236,39 @@ impl ChainParams {
                     .n_members
                     .expect("Pass account-ids or n-members argument");
 
-                (0..n_members)
-                    .into_iter()
-                    .map(|id| {
-                        let seed = id.to_string();
-                        get_account_id_from_seed::<sr25519::Public>(&seed.as_str())
-                    })
-                    .collect()
+                // NOTE : chain id "dev" means that
+                // a known set of accounts is generated from KNOWN_ACCOUNTS seed values
+                // this follows the default Substrate behaviour
+                match self.chain_id() {
+                    DEVNET_ID => KNOWNS_ACCOUNTS
+                        .iter()
+                        .map(get_account_id_from_seed::<sr25519::Public>)
+                        .collect(),
+                    _ => (0..n_members)
+                        .into_iter()
+                        .map(|id| account_key(id, self.base_path(), self.account_key_file.clone()))
+                        .collect(),
+                }
             }
+        }
+    }
+
+    pub fn sudo_account_id(&self) -> AccountId {
+        match &self.sudo_account_id {
+            // account is passed explicitely as a CLI argument
+            Some(id) => AccountId::from_string(id)
+                .expect("Passed string is not a hex encoding of a public key"),
+            // provide some sensible defaults
+            None => match self.chain_id() {
+                // defaults to the first account if chain is "dev", this is the same as substarte default behaviour
+                DEVNET_ID => get_account_id_from_seed::<sr25519::Public>(&KNOWNS_ACCOUNTS[0]),
+                // hardcoded account for any other chain
+                _ => hex![
+                    // 5F4SvwaUEQubiqkPF8YnRfcN77cLsT2DfG4vFeQmSXNjR7hD
+                    "848274306fea52dc528eabc8e14e6ae78ea275bc4247a5d6e2882ac8e948fe68"
+                ]
+                .into(),
+            },
         }
     }
 }
@@ -215,45 +290,37 @@ fn system_properties(token_symbol: String) -> serde_json::map::Map<String, Value
     .collect()
 }
 
-pub fn development_config(
+pub fn config(
     chain_params: ChainParams,
     authorities: Vec<AuthorityKeys>,
+    chain_id: &str,
 ) -> Result<ChainSpec, String> {
     let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
-
-    let rich_accounts: Vec<_> = [
-        "Alice",
-        "Alice//stash",
-        "Bob",
-        "Bob//stash",
-        "Charlie",
-        "Dave",
-        "Eve",
-    ]
-    .iter()
-    .map(get_account_id_from_seed::<sr25519::Public>)
-    // Also give money to the faucet account.
-    .chain(std::iter::once(FAUCET_HASH.into()))
-    .collect();
-
-    let sudo_account = rich_accounts[0].clone();
     let token_symbol = String::from(chain_params.token_symbol());
     let chain_name = String::from(chain_params.chain_name());
+    let sudo_account = chain_params.sudo_account_id();
+
+    // NOTE: this could be passed as a CLI argument similar to the sudo-account id
+    let faucet_account: AccountId =
+        hex!["eaefd9d9b42915bda608154f17bb03e407cbf244318a0499912c2fb1cd879b74"].into();
+
+    let chain_type = match chain_id {
+        DEVNET_ID => ChainType::Development,
+        _ => ChainType::Live,
+    };
 
     Ok(ChainSpec::from_genesis(
         // Name
         &chain_name,
         // ID
-        DEVNET_ID,
-        ChainType::Development,
+        chain_id,
+        chain_type,
         move || {
             genesis(
                 wasm_binary,
-                // Initial PoA authorities
-                authorities.clone(),
-                // Pre-funded accounts
-                sudo_account.clone(),
-                rich_accounts.clone(),
+                authorities.clone(), // Initial PoA authorities, will receive funds
+                sudo_account.clone(), // Root account, will also be pre funded
+                faucet_account.clone(), // Pre-funded faucet account
                 chain_params.clone(),
             )
         },
@@ -270,54 +337,11 @@ pub fn development_config(
     ))
 }
 
-pub fn config(
-    chain_params: ChainParams,
-    authorities: Vec<AuthorityKeys>,
-    chain_id: &str,
-) -> Result<ChainSpec, String> {
-    let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
-
-    let sudo_account: AccountId = hex![
-        // 5F4SvwaUEQubiqkPF8YnRfcN77cLsT2DfG4vFeQmSXNjR7hD
-        "848274306fea52dc528eabc8e14e6ae78ea275bc4247a5d6e2882ac8e948fe68"
-    ]
-    .into();
-
-    let token_symbol = String::from(chain_params.token_symbol());
-    let chain_name = String::from(chain_params.chain_name());
-
-    // Give money to the faucet account
-    let faucet: AccountId = FAUCET_HASH.into();
-    let rich_accounts = vec![faucet, sudo_account.clone()];
-
-    Ok(ChainSpec::from_genesis(
-        // Name
-        &chain_name,
-        // ID
-        chain_id,
-        ChainType::Live,
-        move || {
-            genesis(
-                wasm_binary,
-                // Initial PoA authorities
-                authorities.clone(),
-                sudo_account.clone(),
-                // Pre-funded accounts
-                rich_accounts.clone(),
-                chain_params.clone(),
-            )
-        },
-        // Bootnodes
-        vec![],
-        // Telemetry
-        None,
-        // Protocol ID
-        None,
-        // Properties
-        Some(system_properties(token_symbol)),
-        // Extensions
-        None,
-    ))
+/// Given a Vec<AccountIds> returns a unique collection
+fn de_duplicate(mut accounts: Vec<AccountId>) -> Vec<AccountId> {
+    let set: HashSet<_> = accounts.drain(..).collect();
+    accounts.extend(set.into_iter());
+    accounts
 }
 
 /// Configure initial storage state for FRAME modules
@@ -325,14 +349,24 @@ fn genesis(
     wasm_binary: &[u8],
     authorities: Vec<AuthorityKeys>,
     root_key: AccountId,
-    rich_accounts: Vec<AccountId>,
+    faucet_account: AccountId,
     chain_params: ChainParams,
 ) -> GenesisConfig {
     let session_period = chain_params.session_period();
     let millisecs_per_block = chain_params.millisecs_per_block();
     let unit_creation_delay = chain_params.unit_creation_delay();
 
-    log::debug!("chain parameters {:?}", &chain_params);
+    // NOTE: some combinations of bootstrap chain arguments can potentially
+    // lead to duplicated rich accounts, e.g. if a root account is also an authority
+    // which is why we remove the duplicates if any here
+    let unique_accounts: Vec<AccountId> = de_duplicate(
+        authorities
+            .iter()
+            .map(|auth| &auth.account_id)
+            .cloned()
+            .chain(vec![root_key.clone(), faucet_account])
+            .collect(),
+    );
 
     GenesisConfig {
         system: SystemConfig {
@@ -342,13 +376,7 @@ fn genesis(
         },
         balances: BalancesConfig {
             // Configure endowed accounts with initial balance of 1 << 60.
-            balances: authorities
-                .iter()
-                .map(|auth| &auth.account_id)
-                .cloned()
-                .chain(rich_accounts.into_iter())
-                .map(|k| (k, 1 << 60))
-                .collect(),
+            balances: unique_accounts.into_iter().map(|k| (k, 1 << 60)).collect(),
         },
         aura: AuraConfig {
             authorities: vec![],
