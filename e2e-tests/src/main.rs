e1f3b8c662aec20f00c184fd12c766a4f9827390
pmikolajczyk <piotr.mikolajczyk@cardinals.cc>
2021-11-15 09:51:06 +0100

    checkpoint

--- e2e-tests/src/main.rs
+++ e2e-tests/src/main.rs
@@ -1,13 +1,13 @@
 mod config;
 
 use clap::Parser;
-use codec::Decode;
+use codec::{Compact, Decode};
 use common::create_connection;
 use config::Config;
 use log::{debug, error, info};
 use sp_core::crypto::Ss58Codec;
 use sp_core::{sr25519, Pair};
-use sp_runtime::{generic, traits::BlakeTwo256};
+use sp_runtime::{generic, traits::BlakeTwo256, AccountId32, MultiAddress, OpaqueExtrinsic};
 use std::convert::TryFrom;
 use std::env;
 use std::iter;
@@ -16,11 +16,15 @@ use substrate_api_client::rpc::ws_client::{EventsDecoder, RuntimeEvent};
 use substrate_api_client::rpc::WsRpcClient;
 use substrate_api_client::utils::FromHexString;
 use substrate_api_client::{
-    compose_call, compose_extrinsic, AccountId, Api, UncheckedExtrinsicV4, XtStatus,
+    compose_call, compose_extrinsic, AccountId, Api, Balance, UncheckedExtrinsicV4, XtStatus,
 };
 
 type BlockNumber = u32;
 type Header = generic::Header<BlockNumber, BlakeTwo256>;
+type Block = generic::Block<Header, OpaqueExtrinsic>;
+type TransferTransaction =
+    UncheckedExtrinsicV4<([u8; 2], MultiAddress<AccountId, ()>, codec::Compact<u128>)>;
+type Connection = Api<sr25519::Pair, WsRpcClient>;
 
 fn main() -> anyhow::Result<()> {
     if env::var(env_logger::DEFAULT_FILTER_ENV).is_err() {
@@ -31,6 +35,7 @@ fn main() -> anyhow::Result<()> {
     let config: Config = Config::parse();
 
     test_finalization(config.clone())?;
+    test_fee_calculation(config.clone())?;
     test_token_transfer(config.clone())?;
     test_change_validators(config)?;
 
@@ -42,6 +47,117 @@ fn test_finalization(config: Config) -> anyhow::Result<u32> {
     let connection = create_connection(config.node);
     // wait till at least one block is finalized
     wait_for_finalized_block(connection, 1)
+}
+
+#[derive(Debug)]
+struct FeeInfo {
+    base_fee: Balance,
+    len_fee: Balance,
+    unadjusted_weight: Balance,
+    adjusted_weight: Balance,
+    tip: Balance,
+}
+
+fn get_tx_fee_info(connection: &Connection, tx: &TransferTransaction) -> FeeInfo {
+    let block = connection.get_block::<Block>(None).unwrap().unwrap();
+    let block_hash = block.header.hash();
+
+    let unadjusted_weight = connection
+        .get_payment_info(&tx.hex_encode(), Some(block_hash))
+        .unwrap()
+        .unwrap()
+        .weight as Balance;
+
+    let fee = connection
+        .get_fee_details(&tx.hex_encode(), Some(block_hash))
+        .unwrap()
+        .unwrap();
+    let inclusion_fee = fee.inclusion_fee.unwrap();
+
+    FeeInfo {
+        base_fee: inclusion_fee.base_fee,
+        len_fee: inclusion_fee.len_fee,
+        unadjusted_weight,
+        adjusted_weight: inclusion_fee.adjusted_weight_fee,
+        tip: fee.tip,
+    }
+}
+
+fn balance_of(account: &AccountId32, connection: &Connection) -> Balance {
+    connection.get_account_data(&account).unwrap().unwrap().free
+}
+
+fn transfer(
+    target: &AccountId32,
+    value: u128,
+    connection: &Connection,
+) -> UncheckedExtrinsicV4<([u8; 2], MultiAddress<AccountId32, ()>, Compact<u128>)> {
+    let tx: UncheckedExtrinsicV4<_> = compose_extrinsic!(
+        connection,
+        "Balances",
+        "transfer",
+        GenericAddress::Id(target.clone()),
+        Compact(value)
+    );
+
+    let tx_hash = connection
+        .send_extrinsic(tx.hex_encode(), XtStatus::InBlock)
+        .unwrap()
+        .expect("Could not get tx hash");
+    info!("[+] Transaction hash: {}", tx_hash);
+
+    tx
+}
+
+fn test_fee_calculation(config: Config) -> anyhow::Result<()> {
+    let Config { node, seeds, .. } = config;
+
+    let accounts: Vec<sr25519::Pair> = accounts(seeds);
+    let from: sr25519::Pair = accounts.get(0).expect("No accounts passed").to_owned();
+    let to = AccountId::from(
+        accounts
+            .get(1)
+            .expect("Pass at least two accounts")
+            .public(),
+    );
+    let from_account = AccountId::from(from.clone().public());
+
+    let connection = create_connection(node).set_signer(from);
+
+    let balance_before = balance_of(&from_account, &connection);
+    info!("[+] Account {} balance before tx: {}", to, balance_before);
+
+    let transfer_value = 1000u128;
+    let tx = transfer(&to, transfer_value, &connection);
+
+    let balance_after = balance_of(&from_account, &connection);
+    info!("[+] Account {} balance after tx: {}", to, balance_after);
+
+    let FeeInfo {
+        base_fee,
+        len_fee,
+        unadjusted_weight,
+        adjusted_weight,
+        tip,
+    } = get_tx_fee_info(&connection, &tx);
+    assert_eq!(
+        unadjusted_weight, adjusted_weight,
+        "Weight fee was adjusted incorrectly: raw fee = {}, adjusted fee = {}",
+        unadjusted_weight, adjusted_weight
+    );
+
+    let expected_fee = base_fee + len_fee + unadjusted_weight + tip;
+    assert_eq!(
+        balance_before - transfer_value - expected_fee,
+        balance_after,
+        "Incorrect balance: before = {}, after = {}, tx = {}, expected fee = {}",
+        balance_before,
+        balance_after,
+        transfer_value,
+        expected_fee
+    );
+
+    Ok(())
 }
 
 fn test_token_transfer(config: Config) -> anyhow::Result<()> {
@@ -58,11 +174,7 @@ fn test_token_transfer(config: Config) -> anyhow::Result<()> {
 
     let connection = create_connection(node).set_signer(from);
 
-    let balance_before = connection
-        .get_account_data(&to)?
-        .expect("Could not get account data")
-        .free;
-
+    let balance_before = balance_of(&to, &connection);
     info!("[+] Account {} balance before tx: {}", to, balance_before);
 
     let transfer_value = 1000u128;
@@ -82,11 +194,7 @@ fn test_token_transfer(config: Config) -> anyhow::Result<()> {
 
     info!("[+] Transaction hash: {}", tx_hash);
 
-    let balance_after = connection
-        .get_account_data(&to)?
-        .expect("Could not get account data")
-        .free;
-
+    let balance_after = balance_of(&to, &connection);
     info!("[+] Account {} balance after tx: {}", to, balance_after);
 
     assert!(
@@ -171,10 +279,7 @@ fn test_change_validators(config: Config) -> anyhow::Result<()> {
 }
 
 /// blocking wait, if ongoing session index is >= new_session_index returns the current
-fn wait_for_session(
-    connection: Api<sr25519::Pair, WsRpcClient>,
-    new_session_index: u32,
-) -> anyhow::Result<u32> {
+fn wait_for_session(connection: Connection, new_session_index: u32) -> anyhow::Result<u32> {
     let module = "Session";
     let variant = "NewSession";
     info!("[+] Creating event subscription {}/{}", module, variant);
@@ -212,10 +317,7 @@ fn wait_for_session(
 }
 
 /// blocks the main thread waiting for a block with a number at least `block_number`
-fn wait_for_finalized_block(
-    connection: Api<sr25519::Pair, WsRpcClient>,
-    block_number: u32,
-) -> anyhow::Result<u32> {
+fn wait_for_finalized_block(connection: Connection, block_number: u32) -> anyhow::Result<u32> {
     let (sender, receiver) = channel();
     connection.subscribe_finalized_heads(sender)?;
 
