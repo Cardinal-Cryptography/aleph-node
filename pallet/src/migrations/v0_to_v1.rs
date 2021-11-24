472137a28b3210de66737f9526beefa47a3b88f3
filip <fbielejec@gmail.com>
2021-11-24 18:55:45 +0100

    clippy

--- pallet/src/migrations/v0_to_v1.rs
+++ pallet/src/migrations/v0_to_v1.rs
@@ -1,20 +1,9 @@
 use crate::Config;
 use frame_support::log;
-use frame_support::storage;
 use frame_support::{
-    storage::migration,
     traits::{Get, GetStorageVersion, PalletInfoAccess, StorageVersion},
     weights::Weight,
 };
-use sp_std::prelude::*;
-
-// frame_support::generate_storage_alias!(
-//     Aleph, SessionForValidatorsChange => Value<Option<u32>>
-// );
-
-// frame_support::generate_storage_alias!(
-//     Aleph, Validators<T: Config> => Value<Option<Vec<T::AccountId>>>
-// );
 
 pub fn migrate<T: Config, P: GetStorageVersion + PalletInfoAccess>() -> Weight {
     let on_chain_storage_version = <P as GetStorageVersion>::on_chain_storage_version();
@@ -48,14 +37,3 @@ pub fn migrate<T: Config, P: GetStorageVersion + PalletInfoAccess>() -> Weight {
         T::DbWeight::get().reads(1)
     }
 }
-
-// fn storage_key(module: &str, version: &str) -> [u8; 32] {
-//     let pallet_name = sp_io::hashing::twox_128(module.as_bytes());
-//     let postfix = sp_io::hashing::twox_128(version.as_bytes());
-
-//     let mut final_key = [0u8; 32];
-//     final_key[..16].copy_from_slice(&pallet_name);
-//     final_key[16..].copy_from_slice(&postfix);
-
-//     final_key
-// }
