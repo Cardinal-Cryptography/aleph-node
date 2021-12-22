9db74c2ee7650cbb95e146ffc467383294516adc
Michal Swietek <michal.swietek@cardinals.cc>
2021-12-22 17:57:06 +0100

    read and write generated txs

--- flooder/src/main.rs
+++ flooder/src/main.rs
@@ -1,7 +1,7 @@
 mod config;
 
 use clap::Parser;
-use codec::{Compact, Encode};
+use codec::{Compact, Decode, Encode};
 use common::create_connection;
 use config::Config;
 use hdrhistogram::Histogram as HdrHistogram;
@@ -10,6 +10,7 @@ use rayon::prelude::*;
 use sp_core::{sr25519, Pair};
 use sp_runtime::{generic, traits::BlakeTwo256, MultiAddress, OpaqueExtrinsic};
 use std::{
+    io::{Read, Write},
     iter::{once, repeat, IntoIterator},
     sync::{Arc, Mutex},
     time::Instant,
@@ -78,134 +79,160 @@ fn main() -> Result<(), anyhow::Error> {
         time_stats.elapsed().as_millis()
     );
 
-    thread_pool.install(|| {
-        info!(
-            "deriving accounts: {}ms",
-            time_stats.elapsed().as_millis()
-        );
+    let txs = match config.generate_txs{
+        true => {
+            if config.tx_store_path.is_none() {
+                panic!("tx_store_path is not set");
+            }
+            let path = config.tx_store_path.unwrap();
+            let zipfile = std::fs::File::open(&path).expect("Missing file with txs");
+            let mut archive =
+                zip::ZipArchive::new(zipfile).expect("Zipfile is not properly created");
+            assert!(archive.len() == 1, "There should be one file with txs");
 
-        let accounts = (first_account_in_range..first_account_in_range + total_users)
-            .into_par_iter()
-            .map(derive_user_account)
-            .collect();
+            let mut file = archive.by_index(0).unwrap();
+            let mut bytes = Vec::with_capacity(file.size() as usize);
+            file.read_to_end(&mut bytes).expect("buffer overflow");
+            if bytes.len() > 0 {
+                info!("bytes {:?}", bytes.len());
+                panic!("done");
+            }
 
-        info!(
-            "accounts derived: {}ms",
-            time_stats.elapsed().as_millis()
-        );
-
-        if initialize_accounts_flag {
-            let account = account();
-            let source_account_id = AccountId::from(account.public());
-
-            info!(
-                "downloading source-nonce: {}ms",
-                time_stats.elapsed().as_millis()
-            );
-
-            let source_account_nonce = get_nonce(&connection, &source_account_id);
-
-            info!(
-                "source-nonce downloaded: {}ms",
-                time_stats.elapsed().as_millis()
-            );
-
-            info!(
-                "estimating required amount: {}ms",
-                time_stats.elapsed().as_millis()
-            );
-
-            let total_amount =
-                estimate_amount(&connection, &account, source_account_nonce, transfer_amount);
-
-            info!(
-                "amount estimated: {}ms",
-                time_stats.elapsed().as_millis()
-            );
-
-            assert!(
-                get_funds(&connection, &source_account_id)
-                    .ge(&(total_amount * total_users as u128)),
-                "Account is too poor"
-            );
-
-            info!(
-                "initializing accounts: {}ms",
-                time_stats.elapsed().as_millis()
-            );
-
-            initialize_accounts(
-                &connection,
-                &account,
-                source_account_nonce,
-                &accounts,
-                total_amount,
-            );
-
-            info!(
-                "accounts initialized: {}ms",
-                time_stats.elapsed().as_millis()
-            );
-
-            debug!("all accounts have received funds");
+            Vec::<TransferTransaction>::decode(&mut &bytes[..]).expect("Error while decoding txs")
         }
+        false => {
+            let store_txs = config.store_txs;
+            if store_txs && config.tx_store_path.is_none() {
+                panic!("tx_store_path is not set");
+            }
+            thread_pool.install(|| {
+                info!("deriving accounts: {}ms", time_stats.elapsed().as_millis());
 
-        info!(
-            "initializing nonces: {}ms",
-            time_stats.elapsed().as_millis()
-        );
+                let accounts = (first_account_in_range..first_account_in_range + total_users)
+                    .into_par_iter()
+                    .map(|seed| {if seed%10000 == 0 {info!("{:?}", seed);};derive_user_account(seed)})
+                    .collect();
 
-        let nonces: Vec<_> = match config.download_nonces {
-            false => repeat(0).take(accounts.len()).collect(),
-            true => accounts
-                .iter()
-                .map(|account| get_nonce(&connection, &AccountId::from(account.public())))
-                .collect(),
-        };
+                info!("accounts derived: {}ms", time_stats.elapsed().as_millis());
 
-        info!(
-            "nonces initialized: {}ms",
-            time_stats.elapsed().as_millis()
-        );
+                if initialize_accounts_flag {
+                    let account = account();
+                    let source_account_id = AccountId::from(account.public());
 
-        let receiver = accounts
-            .first()
-            .expect("we should be some accounts available for this test, but the list is empty")
-            .clone();
+                    info!(
+                        "downloading source-nonce: {}ms",
+                        time_stats.elapsed().as_millis()
+                    );
 
-        info!(
-            "signing transactions: {}ms",
-            time_stats.elapsed().as_millis()
-        );
+                    let source_account_nonce = get_nonce(&connection, &source_account_id);
 
-        let txs: Vec<_> = sign_transactions(
-            connection.clone(),
-            receiver,
-            accounts.into_par_iter().zip(nonces),
-            transfer_amount,
-        ).collect();
+                    info!(
+                        "source-nonce downloaded: {}ms",
+                        time_stats.elapsed().as_millis()
+                    );
 
-        info!(
-            "transactions signed: {}ms",
-            time_stats.elapsed().as_millis()
-        );
+                    info!(
+                        "estimating required amount: {}ms",
+                        time_stats.elapsed().as_millis()
+                    );
 
-        let histogram = Arc::new(Mutex::new(
-            HdrHistogram::<u64>::new_with_bounds(1, u64::MAX, 3).unwrap(),
-        ));
+                    let total_amount = estimate_amount(
+                        &connection,
+                        &account,
+                        source_account_nonce,
+                        transfer_amount,
+                    );
 
-        info!(
-            "flooding: {}ms",
-            time_stats.elapsed().as_millis()
-        );
-        let tick = Instant::now();
+                    info!("amount estimated: {}ms", time_stats.elapsed().as_millis());
 
-        flood(&pool, txs.into_par_iter(), tx_status, &histogram);
+                    assert!(
+                        get_funds(&connection, &source_account_id)
+                            .ge(&(total_amount * total_users as u128)),
+                        "Account is too poor"
+                    );
 
-        let tock = tick.elapsed().as_millis();
-        let histogram = histogram.lock().unwrap();
+                    info!(
+                        "initializing accounts: {}ms",
+                        time_stats.elapsed().as_millis()
+                    );
 
-        println!("Summary:\n TransferTransactions sent: {}\n Total time:        {} ms\n Slowest tx:        {} ms\n Fastest tx:        {} ms\n Average:           {:.1} ms\n Throughput:        {:.1} tx/s",
+                    initialize_accounts(
+                        &connection,
+                        &account,
+                        source_account_nonce,
+                        &accounts,
+                        total_amount,
+                    );
+
+                    info!(
+                        "accounts initialized: {}ms",
+                        time_stats.elapsed().as_millis()
+                    );
+
+                    debug!("all accounts have received funds");
+                }
+
+                info!(
+                    "initializing nonces: {}ms",
+                    time_stats.elapsed().as_millis()
+                );
+                debug!("all accounts have received funds");
+                let nonces: Vec<_> = match config.download_nonces {
+                    false => repeat(0).take(accounts.len()).collect(),
+                    true => accounts
+                        .par_iter()
+                        .map(|account| get_nonce(&connection, &AccountId::from(account.public())))
+                        .collect(),
+                };
+                info!("nonces initialized: {}ms", time_stats.elapsed().as_millis());
+                let receiver = accounts
+                    .first()
+                    .expect(
+                        "we should be some accounts available for this test, but the list is empty",
+                    )
+                    .clone();
+
+                info!(
+                    "signing transactions: {}ms",
+                    time_stats.elapsed().as_millis()
+                );
+
+                let txs: Vec<_> = sign_transactions(
+                    connection.clone(),
+                    receiver,
+                    accounts.into_par_iter().zip(nonces),
+                    transfer_amount,
+                )
+                .collect();
+
+                info!(
+                    "transactions signed: {}ms",
+                    time_stats.elapsed().as_millis()
+                );
+
+
+                if store_txs {
+                    zip_and_store_txs(txs);
+                }
+
+            });
+            panic!("done");
+        }
+    };
+
+    let histogram = Arc::new(Mutex::new(
+        HdrHistogram::<u64>::new_with_bounds(1, u64::MAX, 3).unwrap(),
+    ));
+
+    info!("flooding: {}ms", time_stats.elapsed().as_millis());
+    let tick = Instant::now();
+
+    flood(&pool, txs.into_par_iter(), tx_status, &histogram);
+
+    let tock = tick.elapsed().as_millis();
+    let histogram = histogram.lock().unwrap();
+
+    println!("Summary:\n TransferTransactions sent: {}\n Total time:        {} ms\n Slowest tx:        {} ms\n Fastest tx:        {} ms\n Average:           {:.1} ms\n Throughput:        {:.1} tx/s",
                  histogram.len (),
                  tock,
                  histogram.max (),
@@ -213,7 +240,6 @@ fn main() -> Result<(), anyhow::Error> {
                  histogram.mean (),
                  1000.0 * histogram.len () as f64 / tock as f64
         );
-    });
 
     Ok(())
 }
@@ -405,4 +431,17 @@ fn get_funds(connection: &Api<sr25519::Pair, WsRpcClient>, account: &AccountId) 
         Some(data) => data.free,
         None => 0,
     }
+}
+
+fn zip_and_store_txs(txs: Vec<TransferTransaction> ) {
+                let path = std::path::Path::new("tx_store.par.zip");
+                let file = std::fs::File::create(&path).unwrap();
+                let mut zip = zip::ZipWriter::new(file);
+                let options = zip::write::FileOptions::default()
+                    .compression_method(zip::CompressionMethod::Deflated);
+                zip.start_file("tx_store", options)
+                    .expect("Failed to initialize accounts");
+                zip.write_all(&txs.encode())
+                    .expect("Failed to store encoded bytes");
+                zip.finish().expect("Failed to zip the encoded txs");
 }
