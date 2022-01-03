d6ea3c7e95c7944495632770135ceb3f289dcd1b
filip <fbielejec@gmail.com>
2022-01-03 13:17:56 +0100

    wip : dump state

--- fork-off/src/main.rs
+++ fork-off/src/main.rs
@@ -1,6 +1,32 @@
-// build genesis spec
+// build raw genesis spec
 //
 
-fn main() {
-    println!("Hello, world!");
+// dump state
+// curl http://localhost:9933 -H "Content-type: application/json" -d '{"jsonrpc":"2.0","id":1,"method":"state_getPairs","params":["0x"]}' > /tmp/storage.json
+
+use common::create_connection;
+use reqwest;
+use serde_json::Value;
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let client = reqwest::Client::new();
+
+    let body: serde_json::Value = client
+        .post("http://127.0.0.1:9933")
+        .json(&serde_json::json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "state_getPairs",
+            "params": ["0x"]
+        }))
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    println!("{:#?}", body);
+
+    Ok(())
 }
