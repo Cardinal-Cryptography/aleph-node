5ef154e2542cfd96c7d44eae2c39d52793eb855a
filip <fbielejec@gmail.com>
2022-01-28 14:54:07 +0100

    uint notation

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -554,7 +554,7 @@ impl pallet_utility::Config for Runtime {
 
 parameter_types! {
     pub const DepositPerItem: Balance = deposit(1, 0);
-    pub const DepositPerByte: Balance = 1000; // 1 nano-saleph
+    pub const DepositPerByte: Balance = 1_000; // 1 nano-aleph
     // The lazy deletion runs inside on_initialize.
     pub DeletionWeightLimit: Weight = AVERAGE_ON_INITIALIZE_RATIO *
         RuntimeBlockWeights::get().max_block;
