a1b08134d946b417aa31b3f0c6c705a6ff076900
Michal Swietek <michal.swietek@cardinals.cc>
2022-02-03 21:13:34 +0100

    remove reward curve

--- pallet/src/lib.rs
+++ pallet/src/lib.rs
@@ -179,6 +179,7 @@ pub mod pallet {
                 total: 0,
                 voters: Vec::new(),
             };
+
             Ok(Pallet::<T>::validators()
                 .into_iter()
                 .flatten()
