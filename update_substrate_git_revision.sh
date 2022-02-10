b1c0fe8c2c6ad5f44a657bc980cc9a5c314b9f88
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-02-10 15:15:16 +0100

    Revert "my branch"
    This reverts commit b0ad54ac8fed7c271677d5eda9506eae73fc162b.

--- update_substrate_git_revision.sh
+++ update_substrate_git_revision.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rev=$(git ls-remote https://github.com/pmikolajczyk41/substrate.git | grep refs/heads/master | cut -c 1-7)
+rev=$(git ls-remote https://github.com/paritytech/substrate.git | grep refs/heads/master | cut -c 1-7)
 
 
 paths=(bin/node/Cargo.toml bin/runtime/Cargo.toml finality-aleph/Cargo.toml
