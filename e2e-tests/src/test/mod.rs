ab0eea1c5f80ea3983c57dca361d0fe01ca1f280
Michal Swietek <michal.swietek@cardinals.cc>
2022-02-11 16:00:45 +0100

    Add e2e test for elections

--- e2e-tests/src/test/mod.rs
+++ e2e-tests/src/test/mod.rs
@@ -3,11 +3,11 @@ pub use transfer::fee_calculation;
 pub use transfer::token_transfer;
 pub use treasury::channeling_fee;
 pub use treasury::treasury_access;
-pub use validators_change::change_validators;
 pub use utility::batch_transactions;
+pub use validators_change::change_validators;
 
 mod finalization;
 mod transfer;
 mod treasury;
-mod validators_change;
 mod utility;
+mod validators_change;
