65a89ceef5e0fdd6fa30d99cb1c76fb4426e9e6f
Michal Swietek <michal.swietek@cardinals.cc>
2022-02-15 09:44:36 +0100

    final refactor

--- e2e-tests/src/test/staking.rs
+++ e2e-tests/src/test/staking.rs
@@ -1,15 +1,17 @@
 use codec::Compact;
 use log::info;
 
-use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
+use rayon::iter::{
+    IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator, ParallelIterator,
+};
 use sp_core::Pair;
 use substrate_api_client::compose_call;
 use substrate_api_client::{compose_extrinsic, GenericAddress, XtStatus};
 
 use crate::accounts::accounts_from_seeds;
 use crate::config::Config;
-use crate::Header;
 use crate::KeyPair;
+use crate::{BlockNumber, Connection, Header};
 use anyhow::anyhow;
 use common::create_connection;
 use pallet_staking::ValidatorPrefs;
@@ -18,158 +20,118 @@ use std::sync::mpsc::channel;
 use substrate_api_client::extrinsic::staking::RewardDestination;
 use substrate_api_client::AccountId;
 
-fn endow_stash_balances(
-    address: &str,
-    signer_account: KeyPair,
-    stashes_accounts_key_pairs: &[KeyPair],
-    endowment: u128,
-) {
-    let connection = create_connection(address).set_signer(signer_account);
-    let batch_endow: Vec<_> = stashes_accounts_key_pairs
+fn send_xt(connection: &Connection, xt: String, xt_name: &'static str) {
+    let block_hash = connection
+        .send_extrinsic(xt, XtStatus::InBlock)
+        .expect("Could not send extrinsc")
+        .expect("Could not get tx hash");
+    let block_number = connection
+        .get_header::<Header>(Some(block_hash))
+        .expect("Could not fetch header")
+        .expect("Block exists; qed")
+        .number;
+    info!(
+        "Transaction {} was included in block {}.",
+        xt_name, block_number
+    );
+}
+
+fn endow_stash_balances(connection: &Connection, keys: &[KeyPair], endowment: u128) {
+    let batch_endow: Vec<_> = keys
         .iter()
-        .map(|stash| {
+        .map(|key| {
             compose_call!(
                 connection.metadata,
                 "Balances",
                 "transfer",
-                GenericAddress::Id(AccountId::from(stash.public())),
+                GenericAddress::Id(AccountId::from(key.public())),
                 Compact(endowment)
             )
         })
         .collect();
 
-    let extrinsic = compose_extrinsic!(connection, "Utility", "batch", batch_endow);
-
-    let block_hash = connection
-        .send_extrinsic(extrinsic.hex_encode(), XtStatus::InBlock)
-        .expect("Could not send extrinsc")
-        .expect("Could not get tx hash");
-    info!(
-        "[+] A batch of endow account transactions was included in {} block.",
-        block_hash
-    );
+    let xt = compose_extrinsic!(connection, "Utility", "batch", batch_endow);
+    send_xt(connection, xt.hex_encode(), "batch of endow balances");
 }
+
 fn bond(address: &str, initial_stake: u128, controller: &KeyPair) {
     let connection = create_connection(address).set_signer(controller.clone());
-    let account_id = AccountId::from(controller.public());
-    let bond_extrinsic = connection.staking_bond(
-        GenericAddress::Id(account_id.clone()),
-        initial_stake,
-        RewardDestination::Staked,
-    );
-    let block_hash = connection
-        .send_extrinsic(bond_extrinsic.hex_encode(), XtStatus::InBlock)
-        .expect("Could not send extrinsic")
-        .expect("Could not get tx hash");
-    info!(
-        "[+] Controller account {} was bond to stash account {} in block {}.",
-        account_id, account_id, block_hash
-    );
+    let account_id = GenericAddress::Id(AccountId::from(controller.public()));
+
+    let xt = connection.staking_bond(account_id, initial_stake, RewardDestination::Staked);
+    send_xt(&connection, xt.hex_encode(), "bond");
 }
+
 fn validate(address: &str, controller: &KeyPair) {
-    let controller_account_id = AccountId::from(controller.public());
     let connection = create_connection(address).set_signer(controller.clone());
-    crate::send_extrinsic!(
-        connection,
-        "Staking",
-        "validate",
-        XtStatus::InBlock,
-        |tx_hash| info!(
-            "[+] Controller {} intended itself to be elected in the next era, tx: {}",
-            controller_account_id, tx_hash
-        ),
-        ValidatorPrefs {
-            blocked: false,
-            commission: Perbill::from_percent(10),
-        }
-    );
+    let prefs = ValidatorPrefs {
+        blocked: false,
+        commission: Perbill::from_percent(10),
+    };
+
+    let xt = compose_extrinsic!(connection, "Staking", "validate", prefs);
+    send_xt(&connection, xt.hex_encode(), "validate");
 }
 
 fn nominate(address: &str, nominator_key_pair: &KeyPair, nominee_key_pair: &KeyPair) {
-    let nominator_account_id = AccountId::from(nominator_key_pair.public());
     let nominee_account_id = AccountId::from(nominee_key_pair.public());
     let connection = create_connection(address).set_signer(nominator_key_pair.clone());
 
-    let nominate_extrinsic =
-        connection.staking_nominate(vec![GenericAddress::Id(nominee_account_id.clone())]);
-    let block_hash = connection
-        .send_extrinsic(nominate_extrinsic.hex_encode(), XtStatus::InBlock)
-        .expect("Could not send extrinsic")
-        .expect("Could not get tx hash");
-    info!(
-        "[+] Nominator account {} nominated account {} in block {}.",
-        nominator_account_id, nominee_account_id, block_hash
-    );
+    let xt = connection.staking_nominate(vec![GenericAddress::Id(nominee_account_id)]);
+    send_xt(&connection, xt.hex_encode(), "nominate");
 }
 
-fn get_top_finalized_block<F>(
-    address: &str,
-    sender_account_key_pair: KeyPair,
+fn payout_stakers(address: &str, validator: KeyPair, era_number: BlockNumber) {
+    let account = AccountId::from(validator.public());
+    let connection = create_connection(address).set_signer(validator);
+    let xt = compose_extrinsic!(connection, "Staking", "payout_stakers", account, era_number);
+
+    send_xt(&connection, xt.hex_encode(), "payout_stakers");
+}
+
+fn top_finalized_block_number<F>(
+    connection: &Connection,
     predicate: F,
-) -> anyhow::Result<Header>
+) -> anyhow::Result<BlockNumber>
 where
-    F: Fn(&Header) -> bool,
+    F: Fn(BlockNumber) -> bool,
 {
-    let connection = create_connection(address).set_signer(sender_account_key_pair);
     let (sender, receiver) = channel();
     connection.subscribe_finalized_heads(sender)?;
 
-    while let Ok(header) = receiver
+    while let Ok(number) = receiver
         .recv()
-        .map(|h| serde_json::from_str::<Header>(&h).unwrap())
+        .map(|h| serde_json::from_str::<Header>(&h).unwrap().number)
     {
-        if predicate(&header) {
-            return Ok(header);
+        if predicate(number) {
+            return Ok(number);
         }
     }
     Err(anyhow!("Failed to receive finalized head block"))
 }
 
-fn wait_for_full_reward_era(
-    address: &str,
-    sender_account_key_pair: KeyPair,
-) -> anyhow::Result<u32> {
-    let current_finalized_block =
-        get_top_finalized_block(address, sender_account_key_pair.clone(), |_| true)?;
-    // TODO: this should be expressed in terms of runtime chain configuration, not hardcoded values
-    // 30 = block_in_session(10) * sessions_in_era (3)
-    const BLOCKS_PER_ERA: u32 = 30;
+fn wait_for_full_reward_era(connection: &Connection) -> anyhow::Result<BlockNumber> {
+    let current_finalized_block_number = top_finalized_block_number(connection, |_| true)?;
+    let session_period: u32 = connection
+        .get_storage_value("Elections", "SessionPeriod", None)
+        .unwrap()
+        .unwrap();
+    let sessions_per_era: u32 = connection
+        .get_storage_value("Elections", "SessionsPerEra", None)
+        .unwrap()
+        .unwrap();
+    let blocks_per_era = session_period * sessions_per_era;
 
     let next_full_era_block_number =
-        ((current_finalized_block.number + BLOCKS_PER_ERA) / BLOCKS_PER_ERA + 1) * BLOCKS_PER_ERA;
+        ((current_finalized_block_number + blocks_per_era) / blocks_per_era + 1) * blocks_per_era;
     info!(
         "[+] Top finalized block is {}, waiting for block {}",
-        current_finalized_block.number, next_full_era_block_number
+        current_finalized_block_number, next_full_era_block_number
     );
-    let _ = get_top_finalized_block(address, sender_account_key_pair, |header| {
-        header.number >= next_full_era_block_number
-    })?;
+    let _ = top_finalized_block_number(connection, |number| number >= next_full_era_block_number)?;
 
-    let current_era = next_full_era_block_number / BLOCKS_PER_ERA;
+    let current_era = next_full_era_block_number / blocks_per_era;
     Ok(current_era)
-}
-
-fn payout_staker(
-    address: &str,
-    sender_account_key_pair: KeyPair,
-    stash_account_key_pair: &KeyPair,
-    era_number: u32,
-) {
-    let stash_account_id = AccountId::from(stash_account_key_pair.public());
-    let connection = create_connection(address).set_signer(sender_account_key_pair);
-
-    crate::send_extrinsic!(
-        connection,
-        "Staking",
-        "payout_stakers",
-        XtStatus::Finalized,
-        |tx_hash| info!(
-            "[+] Payout for staker {} done in era {}, tx: {}",
-            stash_account_id, era_number, tx_hash
-        ),
-        stash_account_id.clone(),
-        era_number
-    );
 }
 
 fn get_key_pairs() -> (Vec<KeyPair>, Vec<KeyPair>) {
@@ -199,15 +161,13 @@ pub fn staking_test(config: &Config) -> anyhow::Result<()> {
     const VALIDATOR_STAKE: u128 = 25_000 * TOKEN;
     const NOMINATOR_STAKE: u128 = 1_000 * TOKEN;
 
-    let node = &config.node;
     let (stashes_accounts, validator_accounts) = get_key_pairs();
 
-    endow_stash_balances(
-        node,
-        validator_accounts[0].clone(),
-        &stashes_accounts,
-        VALIDATOR_STAKE,
-    );
+    let node = &config.node;
+    let sender = validator_accounts[0].clone();
+    let connection = create_connection(node).set_signer(sender);
+
+    endow_stash_balances(&connection, &stashes_accounts, VALIDATOR_STAKE);
 
     validator_accounts.par_iter().for_each(|account| {
         bond(node, VALIDATOR_STAKE, account);
@@ -226,17 +186,16 @@ pub fn staking_test(config: &Config) -> anyhow::Result<()> {
         .zip(validator_accounts.par_iter())
         .for_each(|(nominator, nominee)| nominate(node, nominator, nominee));
 
-    let sender = validator_accounts[0].clone();
-    let current_era = wait_for_full_reward_era(node, sender.clone())?;
+    let current_era = wait_for_full_reward_era(&connection)?;
     info!(
         "[+] Full era {} passed, claiming rewards for era {}",
         current_era,
         current_era - 1
     );
 
-    for validator_account in validator_accounts {
-        payout_staker(node, sender.clone(), &validator_account, current_era - 1);
-    }
+    validator_accounts
+        .into_par_iter()
+        .for_each(|account| payout_stakers(node, account, current_era - 1));
 
     Ok(())
 }
