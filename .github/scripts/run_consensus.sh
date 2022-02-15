74db64f706c3540fa5ea60f74cf50bbd8262f5b8
Michal Swietek <michal.swietek@cardinals.cc>
2022-02-15 13:25:55 +0100

    Fix sessions per era

--- .github/scripts/run_consensus.sh
+++ .github/scripts/run_consensus.sh
@@ -11,7 +11,7 @@ source docker/env
 # hidden assumption: --chain-id  must be different from a0dnet1, otherwise default chainspec would generate some
 # non-empty validators, and we want to start from the scratch
 docker run -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e DAMIAN -e TOMASZ -e ZBYSZKO -e HANSU -e RUST_LOG=info \
- aleph-node:latest -c "aleph-node bootstrap-chain --base-path /data --chain-id a0dnet1-e2e --millisecs-per-block 2000 --session-period 10 --sessions-per-era 2 --account-ids $DAMIAN,$TOMASZ,$ZBYSZKO,$HANSU --sudo-account-id $DAMIAN > /data/chainspec.json"
+ aleph-node:latest -c "aleph-node bootstrap-chain --base-path /data --chain-id a0dnet1-e2e --millisecs-per-block 2000 --session-period 10 --sessions-per-era 3 --account-ids $DAMIAN,$TOMASZ,$ZBYSZKO,$HANSU --sudo-account-id $DAMIAN > /data/chainspec.json"
 
 # get bootnote peer id
 export BOOTNODE_PEER_ID=$(docker run -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e DAMIAN -e RUST_LOG=info aleph-node:latest -c "aleph-node key inspect-node-key --file /data/$DAMIAN/p2p_secret")
@@ -22,4 +22,3 @@ echo "BOOTNODE_PEER_ID : $BOOTNODE_PEER_ID"
 docker-compose -f docker/docker-compose.yml up -d
 
 exit $?
-
