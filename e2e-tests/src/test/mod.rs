ad7a306da14d360077ee74d57176bfd89dabee48
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-02-18 16:06:12 +0100

    Ready

--- e2e-tests/src/test/mod.rs
+++ e2e-tests/src/test/mod.rs
@@ -1,11 +1,12 @@
+pub use fee::fee_calculation;
 pub use finalization::finalization;
-pub use transfer::fee_calculation;
 pub use transfer::token_transfer;
 pub use treasury::channeling_fee;
 pub use treasury::treasury_access;
-pub use validators_change::change_validators;
 pub use utility::batch_transactions;
+pub use validators_change::change_validators;
 
+mod fee;
 mod finalization;
 mod transfer;
 mod treasury;
