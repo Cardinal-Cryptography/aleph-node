d3299746ac61b7a669a6b32719ef13e3deee10d8
filip <fbielejec@gmail.com>
2022-02-22 14:05:09 +0100

    add release and test builds for node
    update edition adn fix futures version

--- fork-off/src/main.rs
+++ fork-off/src/main.rs
@@ -4,6 +4,7 @@ use std::fs::{self, File};
 use std::io::{ErrorKind, Write};
 use substrate_api_client::extrinsic::log::info;
 
+
 #[derive(Debug, Parser)]
 #[clap(version = "1.0")]
 pub struct Config {
