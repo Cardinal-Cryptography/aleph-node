e89c4d47db36e6db8fb4977bf86d3a31760f859a
filip <fbielejec@gmail.com>
2022-03-04 14:17:38 +0100

    wip : submit headers

--- pallets/tendermint-light-client/src/lib.rs
+++ pallets/tendermint-light-client/src/lib.rs
@@ -3,15 +3,6 @@
 //! It is a part of the Aleph0 <-> Terra bridge
 
 #![cfg_attr(not(feature = "std"), no_std)]
-// use sp_std::prelude::*;
-
-// use frame_support::{
-//     log,
-//     sp_runtime::BoundToRuntimeAppPublic,
-//     traits::{OneSessionHandler, StorageVersion},
-//     Parameter,
-// };
-// pub use pallet::*;
 
 use frame_support::traits::StorageVersion;
 pub use pallet::*;
@@ -22,7 +13,15 @@ const STORAGE_VERSION: StorageVersion = StorageVersion::new(0);
 #[frame_support::pallet]
 pub mod pallet {
     use super::*;
-    use frame_support::pallet_prelude::{IsType, StorageValue, ValueQuery};
+    use frame_support::{
+        log,
+        pallet_prelude::{DispatchClass, DispatchResult, IsType, StorageValue, ValueQuery},
+        traits::Get,
+    };
+    use frame_system::{
+        ensure_root,
+        pallet_prelude::{BlockNumberFor, OriginFor},
+    };
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
@@ -38,8 +37,23 @@ pub mod pallet {
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
+        /// Pallet is halted
+        LightClientHalted,
+        /// Pallet operations are resumed        
+        LightClientResumed,
         /// light client is initialized
         ClientInitialized(u32),
+    }
+
+    // TODO : errors
+
+    #[pallet::error]
+    pub enum Error<T> {
+        NotInitialized,
+        /// light client has already been initialized
+        AlreadyInitialized,
+        /// light client is halted
+        Halted,
     }
 
     // TODO : storage
@@ -50,4 +64,55 @@ pub mod pallet {
     pub type IsHalted<T: Config> = StorageValue<_, bool, ValueQuery>;
 
     // TODO : calls
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        // TODO : adjust weight
+        #[pallet::weight((T::DbWeight::get().reads_writes(1, 1), DispatchClass::Operational))]
+        pub fn initialize_client(origin: OriginFor<T>) -> DispatchResult {
+            ensure_root(origin)?;
+
+            // TODO
+
+            Ok(())
+        }
+
+        // TODO : adjust weight
+        #[pallet::weight((T::DbWeight::get().reads_writes(1, 1), DispatchClass::Operational))]
+        pub fn submit_finality_proof(origin: OriginFor<T>) -> DispatchResult {
+            ensure_not_halted::<T>()?;
+
+            // TODO
+
+            Ok(())
+        }
+
+        /// Halt or resume all operations.
+        ///
+        /// Can only be called by root
+        #[pallet::weight((T::DbWeight::get().reads_writes(1, 1), DispatchClass::Operational))]
+        pub fn set_halted(origin: OriginFor<T>, halted: bool) -> DispatchResult {
+            ensure_root(origin)?;
+            <IsHalted<T>>::put(halted);
+
+            if halted {
+                log::info!(target: "runtime::tendermint-lc", "Halting light client operations");
+                Self::deposit_event(Event::LightClientHalted);
+            } else {
+                log::warn!(target: "runtime::tendermint-lc", "Resuming light client operations.");
+                Self::deposit_event(Event::LightClientResumed);
+            }
+
+            Ok(())
+        }
+    }
+
+    /// Ensure that the bridge is not in a halted state
+    fn ensure_not_halted<T: Config>() -> Result<(), Error<T>> {
+        if <IsHalted<T>>::get() {
+            Err(<Error<T>>::Halted)
+        } else {
+            Ok(())
+        }
+    }
 }
