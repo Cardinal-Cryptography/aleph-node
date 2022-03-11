928398658fde1abb4787861fbd2e5d1fd2c82b31
timorl <tymorl@gmail.com>
2022-03-11 11:49:40 +0100

    I really should compile before committing...

--- bin/cliain/src/validators.rs
+++ bin/cliain/src/validators.rs
@@ -6,7 +6,7 @@ use sp_core::crypto::Ss58Codec;
 /// Change validators to the provided list by calling the provided node.
 /// The keypair has to be capable of signing sudo calls.
 pub fn change(validators: Vec<String>, node: String, sudo_key: KeyPair) {
-    let connection = create_connection(&node).set_signer(key);
+    let connection = create_connection(&node).set_signer(sudo_key);
     let validators: Vec<_> = validators.into_iter().map(|validator|
         AccountId::from_ss58check(&validator).expect("Address is valid")
     ).collect();
