9bca5643598cb4a8f165b055cf2cb62890d88d07
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-18 18:10:39 +0100

    AZ-596 added support for running tests in nix derivation

--- default.nix
+++ default.nix
@@ -1,5 +1,17 @@
-{ rocksDBVersion ? "6.29.3" }:
+{ nixpkgs ? (import ./nix/versions.nix).nixpkgs
+, rocksDBVersion ? "6.29.3"
+, runTests ? false
+, allCrates ? false
+}:
 let
-  aleph-node = ( import ./nix/aleph-node.nix { inherit rocksDBVersion; } ).project;
+  alephNode = (import ./nix/aleph-node.nix { inherit rocksDBVersion; }).project;
+  workspaceMembers = builtins.mapAttrs (_: crate: crate.build.override { inherit runTests; }) alephNode.workspaceMembers;
+  allWorkspaceMembers = nixpkgs.symlinkJoin {
+      name = "all-workspace-members";
+      paths = builtins.attrValues workspaceMembers;
+  };
 in
-aleph-node.workspaceMembers."aleph-node".build
+if allCrates then
+  allWorkspaceMembers
+else
+  workspaceMembers."aleph-node".build
