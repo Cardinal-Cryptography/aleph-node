84896b52c0cfdeb7ed76b9091287aad24ef4d02e
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-03-18 15:55:50 +0100

    remove unnecessary ascription

--- bin/cliain/src/validators.rs
+++ bin/cliain/src/validators.rs
@@ -6,7 +6,7 @@ use substrate_api_client::{AccountId, XtStatus};
 /// Change validators to the provided list by calling the provided node.
 /// The keypair has to be capable of signing sudo calls.
 pub fn change(connection: Connection, validators: Vec<String>) {
-    let validators: Vec<_> = validators
+    let validators = validators
         .into_iter()
         .map(|validator| AccountId::from_ss58check(&validator).expect("Address is valid"))
         .collect();
