7863344a70c5650d36c2f448c4213a5797f32635
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-18 17:09:00 +0100

    AZ-596 building aleph-node using crate2nix

---
+++ nix/aleph-node.nix
@@ -0,0 +1,92 @@
+{ rocksDBVersion ? "6.29.3"
+, versions ? import ./versions.nix
+, nixpkgs ? versions.nixpkgs
+, gitignoreSource ? versions.gitignoreSource
+, customRocksdb ? versions.customRocksdb rocksDBVersion
+}:
+let
+  # declares a build environment where C and C++ compilers are delivered by the llvm/clang project
+  # in this version build process should rely only on clang, without access to gcc
+  llvm = nixpkgs.llvmPackages_11;
+  env = llvm.stdenv;
+  llvmVersionString = "${nixpkgs.lib.getVersion env.cc.cc}";
+
+  # allows to skip files listed by .gitignore
+  # otherwise `nix-build` copies everything, including the target directory
+  src = gitignoreSource ../.;
+
+  # we need this to generate nix-based build plan
+  crate2nix = nixpkgs.crate2nix;
+  inherit (import ./tools.nix { pkgs = nixpkgs; lib = nixpkgs.lib; stdenv = env; inherit crate2nix; }) generatedCargoNix vendoredCargoLock;
+
+  # some of our dependencies requires external libraries like protobuf, etc.
+  customBuildRustCrateForPkgs = pkgs: pkgs.buildRustCrate.override {
+    stdenv = env;
+    defaultCrateOverrides = pkgs.defaultCrateOverrides // (
+      let protobufFix = _: {
+            # provides env variables necessary to use protobuf during compilation
+            buildInputs = [ pkgs.protobuf ];
+            PROTOC="${pkgs.protobuf}/bin/protoc";
+          };
+      in rec {
+        librocksdb-sys = _: {
+          buildInputs = [ customRocksdb ];
+          LIBCLANG_PATH="${llvm.libclang.lib}/lib";
+          ROCKSDB_LIB_DIR="${customRocksdb}/lib";
+          # forces librocksdb-sys to statically compile with our customRocksdb
+          ROCKSDB_STATIC=1;
+        };
+        libp2p-core = protobufFix;
+        libp2p-plaintext = protobufFix;
+        libp2p-floodsub = protobufFix;
+        libp2p-gossipsub = protobufFix;
+        libp2p-identify = protobufFix;
+        libp2p-kad = protobufFix;
+        libp2p-relay = protobufFix;
+        libp2p-rendezvous = protobufFix;
+        libp2p-noise = protobufFix;
+        sc-network = protobufFix;
+        prost-build = protobufFix;
+        aleph-runtime = _:
+          # this is a bit tricky - aleph-runtime's build.rs calls Cargo, so we need to provide it a populated
+          # CARGO_HOME, otherwise it tries to download crates (it doesn't work with sandboxed nix-build)
+          let
+            vendoredCargo = vendoredCargoLock "${src}" "Cargo.lock";
+            CARGO_HOME="$out/.cargo";
+            # this way Cargo called by build.rs can see our vendored CARGO_HOME
+            wrappedCargo = pkgs.writeShellScriptBin "cargo" ''
+               export CARGO_HOME="${CARGO_HOME}"
+               exec ${pkgs.cargo}/bin/cargo "$@"
+            '';
+          in
+          rec {
+            inherit src CARGO_HOME;
+            # otherwise it has no access to other dependencies in our workspace
+            workspace_member = "bin/runtime";
+            buildInputs = [pkgs.git pkgs.cacert];
+            CARGO = "${wrappedCargo}/bin/cargo";
+            # build.rs is called during `configure` phase, so we need to setup during `preConfigure`
+            preConfigure = ''
+              # populate vendored CARGO_HOME
+              mkdir -p $out
+              ln -s ${vendoredCargo}/.cargo ${CARGO_HOME}
+              ln -s ${vendoredCargo} $out/cargo-vendor-dir
+              ln -s ${vendoredCargo}/Cargo.lock $out/Cargo.lock
+            '';
+            postBuild = ''
+              # we need to clean after ourselves
+              # buildRustCrate derivation will populate it with necessary artifacts
+              rm -rf $out
+            '';
+          };
+    }
+    );
+  };
+
+  generated = generatedCargoNix {
+    name = "aleph-node";
+    inherit src;
+  };
+  project = import generated { pkgs = nixpkgs; buildRustCrateForPkgs = customBuildRustCrateForPkgs; };
+in
+{ inherit project src; }
---
+++ nix/versions.nix
@@ -0,0 +1,112 @@
+# declares all pinned versions of packages we are using during the build
+rec {
+  fetchImportCargoLock = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/be872a7453a176df625c12190b8a6c10f6b21647.tar.gz";
+    sha256 = "1hnwh2w5rhxgbp6c8illcrzh85ky81pyqx9309bkgpivyzjf2nba";
+  };
+
+  importCargoLock = (import fetchImportCargoLock {}).rustPlatform.importCargoLock;
+
+  fetchCrate2nix = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/c82b46413401efa740a0b994f52e9903a4f6dcd5.tar.gz";
+    sha256 = "13s8g6p0gzpa1q6mwc2fj2v451dsars67m4mwciimgfwhdlxx0bk";
+  };
+
+  crate2nix = (import fetchCrate2nix {}).crate2nix;
+
+  # pinned version of nix packages
+  # main reason for not using here the newest available version at the time or writing is that this way we depend on glibc version 2.31 (Ubuntu 20.04 LTS)
+  fetchNixpkgs = (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/2c162d49cd5b979eb66ff1653aecaeaa01690fcc.tar.gz";
+    sha256 = "08k7jy14rlpbb885x8dyds5pxr2h64mggfgil23vgyw6f1cn9kz6";
+  });
+
+  # this overlay allows us to use a specified version of the rust toolchain
+  fetchRustOverlay = builtins.fetchTarball {
+    url = "https://github.com/mozilla/nixpkgs-mozilla/archive/15b7a05f20aab51c4ffbefddb1b448e862dccb7d.tar.gz";
+    sha256 = "0admybxrjan9a04wq54c3zykpw81sc1z1nqclm74a7pgjdp7iqv1";
+  };
+
+  nixpkgs =
+    let
+      # this overlay allows us to use a specified version of the rust toolchain
+      rustOverlay =
+        import fetchRustOverlay;
+
+      overrideRustTarget = rustChannel: rustChannel // {
+        rust = rustChannel.rust.override {
+          targets = [ "x86_64-unknown-linux-gnu" "wasm32-unknown-unknown" ];
+        };
+      };
+      rustToolchain = with nixpkgs; overrideRustTarget ( rustChannelOf { rustToolchain = ../rust-toolchain; } );
+
+      inherit crate2nix;
+
+      # pinned version of nix packages
+      nixpkgs = import fetchNixpkgs { overlays = [
+            rustOverlay
+            (self: super: {
+              inherit (rustToolchain) cargo rust-src rust-std;
+              rustc = rustToolchain.rust;
+
+              inherit crate2nix;
+            })
+          ];
+        };
+    in
+    nixpkgs;
+
+  fetchDockerNixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/be872a7453a176df625c12190b8a6c10f6b21647.tar.gz";
+    sha256 = "1hnwh2w5rhxgbp6c8illcrzh85ky81pyqx9309bkgpivyzjf2nba";
+  };
+
+  dockerNixpkgs = import fetchDockerNixpkgs {};
+
+  fetchGitignoreSource = nixpkgs.fetchFromGitHub {
+    owner = "hercules-ci";
+    repo = "gitignore.nix";
+    rev = "5b9e0ff9d3b551234b4f3eb3983744fa354b17f1";
+    sha256 = "o/BdVjNwcB6jOmzZjOH703BesSkkS5O7ej3xhyO8hAY=";
+  };
+
+  gitignoreSource = (import fetchGitignoreSource { inherit (nixpkgs) lib; }).gitignoreSource;
+
+  fetchRocksdb = rocksDBVersion: builtins.fetchGit {
+    url = "https://github.com/facebook/rocksdb.git";
+    ref = "refs/tags/v${rocksDBVersion}";
+  };
+
+  # we use a newer version of rocksdb than the one provided by nixpkgs
+  # we disable all compression algorithms and force it to use SSE 4.2 cpu instruction set
+  customRocksdb = rocksDBVersion: nixpkgs.rocksdb.overrideAttrs (_: {
+
+    src = fetchRocksdb rocksDBVersion;
+
+    version = "${rocksDBVersion}";
+
+    patches = [];
+
+    cmakeFlags = [
+       "-DPORTABLE=0"
+       "-DWITH_JNI=0"
+       "-DWITH_BENCHMARK_TOOLS=0"
+       "-DWITH_TESTS=0"
+       "-DWITH_TOOLS=0"
+       "-DWITH_BZ2=0"
+       "-DWITH_LZ4=0"
+       "-DWITH_SNAPPY=0"
+       "-DWITH_ZLIB=0"
+       "-DWITH_ZSTD=0"
+       "-DWITH_GFLAGS=0"
+       "-DUSE_RTTI=0"
+       "-DFORCE_SSE42=1"
+       "-DROCKSDB_BUILD_SHARED=0"
+    ];
+
+    propagatedBuildInputs = [];
+
+    buildInputs = [ nixpkgs.git ];
+  });
+
+}
