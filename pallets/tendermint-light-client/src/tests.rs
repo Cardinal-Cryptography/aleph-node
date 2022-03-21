5bde89f907103b115dac98398ba943c3a64ac970
filip <fbielejec@gmail.com>
2022-03-21 15:08:10 +0100

    set block no

--- pallets/tendermint-light-client/src/tests.rs
+++ pallets/tendermint-light-client/src/tests.rs
@@ -1,14 +1,15 @@
-use frame_support::assert_ok;
-
 use super::*;
 use crate::{
     mock::*,
     types::{LightBlockStorage, LightClientOptionsStorage},
 };
+use frame_support::assert_ok;
 
 #[test]
 fn successful_verification() {
     new_test_ext(|| {
+        System::set_block_number(1);
+
         let origin = Origin::root();
         let options = LightClientOptionsStorage::default();
         let initial_block: LightBlockStorage = serde_json::from_str(mock::TRUSTED_BLOCK).unwrap();
@@ -24,6 +25,10 @@ fn successful_verification() {
         //  assert storage updated
         assert_eq!(Pallet::<TestRuntime>::get_options(), options.clone());
 
-        // assert!(false);
+        assert_eq!(Pallet::<TestRuntime>::is_halted(), false);
+
+        System::assert_last_event(mock::Event::TendermintLightClient(
+            super::Event::LightClientInitialized,
+        ));
     });
 }
