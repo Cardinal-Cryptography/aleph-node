6aaa0c205228137bea893970a23c4d6f370e47fe
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-03-25 13:28:18 +0100

    AZ-596 `crates ? [ "aleph-node" ]` instead of `allCrates ? false` in default.nix - it allows to filter crates that we to build

--- default.nix
+++ default.nix
@@ -1,18 +1,34 @@
 { nixpkgs ? (import ./nix/versions.nix {}).nixpkgs
 , rocksDBVersion ? "6.29.3"
 , runTests ? false
-, allCrates ? false
+, crates ? [ "aleph-node" ]
 }:
 let
   versions = import ./nix/versions.nix { inherit rocksDBVersion; };
   alephNode = (import ./nix/aleph-node.nix { inherit versions; }).project;
-  workspaceMembers = builtins.mapAttrs (_: crate: crate.build.override { inherit runTests; }) alephNode.workspaceMembers;
-  allWorkspaceMembers = nixpkgs.symlinkJoin {
-      name = "all-workspace-members";
-      paths = builtins.attrValues workspaceMembers;
-  };
+  workspaceMembers = builtins.mapAttrs (_: crate: crate.build.override { inherit runTests; extraRustcOpts = "--target x86-64-v3"; }) alephNode.workspaceMembers;
+  cratesFilter =
+    let
+      cratesAttrs = builtins.listToAttrs (builtins.map (member: { name = member; value = null; }) workspaceMembers);
+    in
+    n: builtins.hasAttr n cratesAttrs;
+  filteredWorkspaceMembers = nixpkgs.lib.filterAttrs (n: _: cratesFilter n) workspaceMembers;
+  workspaceMembersToBuild =
+    if crates == [] then
+      nixpkgs.symlinkJoin {
+        name = "all-workspace-members";
+        paths = builtins.attrValues workspaceMembers;
+      }
+    else
+      if builtins.length crates == 1 then
+        let
+          crateName = builtins.head crates;
+        in
+        workspaceMembers."${crateName}"
+      else
+        nixpkgs.symlinkJoin {
+          name = "filtered-workspace-members";
+          paths = builtins.attrValues filteredWorkspaceMembers;
+        };
 in
-if allCrates then
-  allWorkspaceMembers
-else
-  workspaceMembers."aleph-node"
+workspaceMembersToBuild
