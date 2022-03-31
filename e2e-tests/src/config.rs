53eef5587b39a7853f388231b16a789feb4870f5
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-03-31 17:47:35 +0200

    :see-no-evil:

--- e2e-tests/src/config.rs
+++ e2e-tests/src/config.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 #[clap(version = "1.0")]
 pub struct Config {
     /// WS endpoint address of the node to connect to
-    #[clap(long, default_value = "127.0.0.1:9944")]
+    #[clap(long, default_value = "127.0.0.1:9943")]
     pub node: String,
 
     /// seed values to create accounts
