72bae0aa81a9cfd12897605e3e2fb97f1a39ca9a
Marcin <marcin.radecki@cardinals.cc>
2022-04-04 15:48:29 +0200

    AZ-733 Forgot to remove some debug code

--- benches/payout-stakers/src/main.rs
+++ benches/payout-stakers/src/main.rs
@@ -6,7 +6,7 @@ use std::iter;
 use substrate_api_client::{extrinsic::staking::RewardDestination, AccountId, XtStatus};
 
 use aleph_client::{
-    balances_batch_transfer, create_connection, keypair_from_string, locks,
+    balances_batch_transfer, create_connection, keypair_from_string,
     payout_stakers_and_assert_locked_balance, staking_batch_bond, staking_batch_nominate,
     staking_bond, staking_validate, wait_for_next_era, Connection,
 };
@@ -36,14 +36,6 @@ fn main() -> Result<(), anyhow::Error> {
     let validators = set_validators(address);
     let validators_and_its_nominators =
         create_test_validators_and_its_nominators(&connection, validators);
-    locks(
-        &connection,
-        &[
-            validators_and_its_nominators[0].1[0].clone(),
-            validators_and_its_nominators[0].1[1].clone(),
-        ],
-    );
-
     wait_for_10_eras(address, &connection, validators_and_its_nominators)?;
 
     Ok(())
