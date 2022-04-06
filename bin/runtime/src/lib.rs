b2fd30a0dd67672413b66014ff816e9aa40e3983
Michal Swietek <michal.swietek@cardinals.cc>
2022-04-06 08:27:56 +0200

    linter

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -13,8 +13,8 @@ use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
 use sp_runtime::{
     create_runtime_str, generic, impl_opaque_keys,
     traits::{
-        AccountIdLookup, BlakeTwo256, Block as BlockT, CheckedSub, ConvertInto, IdentifyAccount,
-        OpaqueKeys, Verify,
+        AccountIdLookup, BlakeTwo256, Block as BlockT, ConvertInto, IdentifyAccount, OpaqueKeys,
+        Verify,
     },
     transaction_validity::{TransactionSource, TransactionValidity},
     ApplyExtrinsicResult, MultiSignature, RuntimeAppPublic,
