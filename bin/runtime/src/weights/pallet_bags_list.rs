2a2d3f031c43aa25a7a1489598e027660f704a19
Michal Swietek <michal.swietek@cardinals.cc>
2022-04-11 13:35:25 +0200

    fix linter

--- bin/runtime/src/weights/pallet_bags_list.rs
+++ bin/runtime/src/weights/pallet_bags_list.rs
@@ -9,17 +9,17 @@ impl<T: frame_system::Config> pallet_bags_list::WeightInfo for WeightInfo<T> {
     // Storage: BagsList ListNodes (r:4 w:4)
     // Storage: BagsList ListBags (r:1 w:1)
     fn rebag_non_terminal() -> Weight {
-        (64_669_000 as Weight)
-            .saturating_add(T::DbWeight::get().reads(7 as Weight))
-            .saturating_add(T::DbWeight::get().writes(5 as Weight))
+        64_669_000_u64
+            .saturating_add(T::DbWeight::get().reads(7))
+            .saturating_add(T::DbWeight::get().writes(5))
     }
     // Storage: Staking Bonded (r:1 w:0)
     // Storage: Staking Ledger (r:1 w:0)
     // Storage: BagsList ListNodes (r:3 w:3)
     // Storage: BagsList ListBags (r:2 w:2)
     fn rebag_terminal() -> Weight {
-        (62_211_000 as Weight)
-            .saturating_add(T::DbWeight::get().reads(7 as Weight))
-            .saturating_add(T::DbWeight::get().writes(5 as Weight))
+        62_211_000_u64
+            .saturating_add(T::DbWeight::get().reads(7))
+            .saturating_add(T::DbWeight::get().writes(5))
     }
 }
