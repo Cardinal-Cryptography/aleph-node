ce266aa4fbd1e2825a97ea08792cd6dfa190b36f
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-04-18 23:51:51 +0200

    added aleph-runtime nix derivation to github.nix

--- nix/github.nix
+++ nix/github.nix
@@ -2,6 +2,7 @@
 let
   alephNodeDrv = import ../default.nix;
   alephNode = alephNodeDrv { crates = { "aleph-node" = []; "aleph-runtime" = []; }; };
+  alephRuntime = alephNodeDrv { crates = { "aleph-runtime" = []; }; };
   alephNodeShortSession = alephNodeDrv { crates = { "aleph-node" = ["short_session"]; "aleph-runtime" = ["short_session"]; }; };
 in
-[ nixpkgs.patchelf alephNode alephNodeShortSession ]
+[ nixpkgs.patchelf alephNode alephRuntime alephNodeShortSession ]
