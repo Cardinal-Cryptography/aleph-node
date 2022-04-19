82c5972e57364c4701bc971bffa51eafc8eddf99
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-04-19 11:26:30 +0200

    aleph-node derivation in github.nix (cache for pipelines) builds only aleph-node without aleph-runtime (removed explicite arg)

--- nix/github.nix
+++ nix/github.nix
@@ -1,7 +1,7 @@
 { nixpkgs ? (import ./versions.nix).nixpkgs }:
 let
   alephNodeDrv = import ../default.nix;
-  alephNode = alephNodeDrv { crates = { "aleph-node" = []; "aleph-runtime" = []; }; };
+  alephNode = alephNodeDrv { crates = { "aleph-node" = []; }; };
   alephRuntime = alephNodeDrv { crates = { "aleph-runtime" = []; }; };
   alephNodeShortSession = alephNodeDrv { crates = { "aleph-node" = ["short_session"]; "aleph-runtime" = ["short_session"]; }; };
 in
