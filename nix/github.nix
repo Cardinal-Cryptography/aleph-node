006ab74d11ade76300413820ff17f822d27c73fd
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-04-20 20:31:20 +0200

    using setPrio for github.nix packages

--- nix/github.nix
+++ nix/github.nix
@@ -1,22 +1,23 @@
 let
   nixpkgs = (import ./versions.nix).nixpkgs;
   alephNodeDrv = import ../default.nix;
-  alephNode = alephNodeDrv { crates = { "aleph-node" = []; }; name = "aleph-node"; };
-  alephRuntime = (alephNodeDrv {
-    crates = { "aleph-runtime" = []; };
-    name = "aleph-runtime";
-  }).overrideAttrs (
-    attrs: {
-      meta.priority = alephNode.meta.priority + 1;
+  alephNode = nixpkgs.lib.hiPrio (
+    alephNodeDrv {
+      crates = { "aleph-node" = []; };
+      name = "aleph-node";
     }
   );
-  alephNodeShortSession = (alephNodeDrv {
-    crates = { "aleph-node" = ["short_session"]; "aleph-runtime" = ["short_session"]; };
-    name = "short_session";
-  }).overrideAttrs (
-      attrs: {
-        meta.priority = alephRuntime.meta.priority + 1;
-      }
-    );
+  alephRuntime = nixpkgs.lib.setPrio (alephNode.meta.priority + 1) (
+    alephNodeDrv {
+      crates = { "aleph-runtime" = []; };
+      name = "aleph-runtime";
+    }
+  );
+  alephNodeShortSession = nixpkgs.lib.setPrio (alephRuntime.meta.priority + 1) (
+    alephNodeDrv {
+      crates = { "aleph-node" = ["short_session"]; "aleph-runtime" = ["short_session"]; };
+      name = "short_session";
+    }
+  );
 in
 [ nixpkgs.patchelf alephNode alephRuntime alephNodeShortSession ]
