50386540762714e0783e9f19c6103cf19fa20c90
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-04-20 17:29:49 +0200

    each package in github.nix has now a different meta.priority - there were conflicts (providing same files for PATH etc.)

--- nix/github.nix
+++ nix/github.nix
@@ -2,7 +2,21 @@ let
   nixpkgs = (import ./versions.nix).nixpkgs;
   alephNodeDrv = import ../default.nix;
   alephNode = alephNodeDrv { crates = { "aleph-node" = []; }; name = "aleph-node"; };
-  alephRuntime = alephNodeDrv { crates = { "aleph-runtime" = []; }; name = "aleph-runtime"; };
-  alephNodeShortSession = alephNodeDrv { crates = { "aleph-node" = ["short_session"]; "aleph-runtime" = ["short_session"]; }; name = "short_session"; };
+  alephRuntime = (alephNodeDrv {
+    crates = { "aleph-runtime" = []; };
+    name = "aleph-runtime";
+  }).overrideAttrs (
+    attrs: {
+      meta.priority = alephNode.meta.priority + 1;
+    }
+  );
+  alephNodeShortSession = (alephNodeDrv {
+    crates = { "aleph-node" = ["short_session"]; "aleph-runtime" = ["short_session"]; };
+    name = "short_session";
+  }).overrideAttrs (
+      attrs: {
+        meta.priority = alephRuntime.meta.priority + 1;
+      }
+    );
 in
 [ nixpkgs.patchelf alephNode alephRuntime alephNodeShortSession ]
