74ebbce73eecc0aba5906c9ddbca84888907fdca
Maciej Żelaszczyk <maciek.zelaszczyk@gmail.com>
2022-04-21 13:50:44 +0200

    Changed chain spec generation to handle distinct controller and stash accounts for local runs.

--- bin/node/src/chain_spec.rs
+++ bin/node/src/chain_spec.rs
@@ -1,6 +1,6 @@
 use aleph_primitives::{
     staking::{MIN_NOMINATOR_BOND, MIN_VALIDATOR_BOND},
-    AuthorityId as AlephId, ADDRESSES_ENCODING, TOKEN_DECIMALS,
+    AuthorityId as AlephId, ADDRESSES_ENCODING, TOKEN, TOKEN_DECIMALS,
 };
 use aleph_runtime::{
     AccountId, AuraConfig, BalancesConfig, ElectionsConfig, GenesisConfig, Perbill, SessionConfig,
@@ -13,7 +13,7 @@ use serde::{de::Error, Deserialize, Deserializer, Serialize, Serializer};
 use serde_json::{Number, Value};
 use sp_application_crypto::Ss58Codec;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
-use sp_core::{Pair, Public};
+use sp_core::{sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
 use std::{collections::HashSet, path::PathBuf, str::FromStr};
 use structopt::StructOpt;
@@ -201,28 +201,38 @@ fn system_properties(token_symbol: String) -> serde_json::map::Map<String, Value
     .collect()
 }
 
-pub fn devnet_config(
-    chain_params: ChainParams,
-    authorities: Vec<AuthorityKeys>,
-) -> Result<ChainSpec, String> {
-    // TODO fix it properly so there's a default configuration for distinct stash and controller
-    // accounts, and linked controller accounts with validators
-    // for now it's better to leave it as empty not to imply (not advised) initial staking configuration
-    // in which stash == controller == validator
-    generate_chain_spec_config(chain_params, authorities, vec![])
-}
-
+/// Generate chain spec.
 pub fn config(
     chain_params: ChainParams,
     authorities: Vec<AuthorityKeys>,
 ) -> Result<ChainSpec, String> {
-    generate_chain_spec_config(chain_params, authorities, vec![])
+    generate_chain_spec_config(chain_params, authorities, None, None)
+}
+
+/// Generate chain spec for local runs.
+/// Stash and controller accounts are generated for the specified authorities.
+pub fn local_config(
+    chain_params: ChainParams,
+    authorities: Vec<AuthorityKeys>,
+) -> Result<ChainSpec, String> {
+    let authority_accounts: Vec<AccountId> = to_account_ids(&authorities).collect();
+    let controller_accounts: Vec<AccountId> =
+        generate_staking_accounts(authority_accounts.clone(), StakingAccount::Controller);
+    let stash_accounts: Vec<AccountId> =
+        generate_staking_accounts(authority_accounts, StakingAccount::Stash);
+    generate_chain_spec_config(
+        chain_params,
+        authorities,
+        Some(controller_accounts),
+        Some(stash_accounts),
+    )
 }
 
 fn generate_chain_spec_config(
     chain_params: ChainParams,
     authorities: Vec<AuthorityKeys>,
-    stakers: Vec<AccountId>,
+    controller_accounts: Option<Vec<AccountId>>,
+    stash_accounts: Option<Vec<AccountId>>,
 ) -> Result<ChainSpec, String> {
     let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
     let token_symbol = String::from(chain_params.token_symbol());
@@ -244,7 +254,8 @@ fn generate_chain_spec_config(
                 authorities.clone(), // Initial PoA authorities, will receive funds
                 sudo_account.clone(), // Sudo account, will also be pre funded
                 faucet_account.clone(), // Pre-funded faucet account
-                stakers.clone(),
+                controller_accounts.clone(), // Controller accounts for staking.
+                stash_accounts.clone(), // Stash accounts for staking.
             )
         },
         // Bootnodes
@@ -266,13 +277,44 @@ fn deduplicate(accounts: Vec<AccountId>) -> Vec<AccountId> {
     set.into_iter().collect()
 }
 
-/// Configure initial storage state for FRAME modules
+/// Type of staking account: stash or controller.
+enum StakingAccount {
+    Controller,
+    Stash,
+}
+
+/// Given a set of accounts and an account type returns the corresponding staking accounts.
+fn generate_staking_accounts(
+    accounts: Vec<AccountId>,
+    account_type: StakingAccount,
+) -> Vec<AccountId> {
+    let account_suffix = match account_type {
+        StakingAccount::Controller => "Controller",
+        StakingAccount::Stash => "Stash",
+    };
+    accounts
+        .into_iter()
+        .enumerate()
+        .map(|(index, _account)| {
+            get_account_id_from_seed::<sr25519::Public>(
+                format!("//{}//{}", index, account_suffix).as_str(),
+            )
+        })
+        .collect()
+}
+
+// to have total issuance 300M (for devnet/tests/local runs only)
+const ENDOWMENT: u128 = 60_000_000u128 * 10u128.pow(TOKEN_DECIMALS);
+
+/// Configure initial storage state for FRAME modules.
+/// Possible to provide distinct controller and stash accounts.
 fn generate_genesis_config(
     wasm_binary: &[u8],
     authorities: Vec<AuthorityKeys>,
     sudo_account: AccountId,
     faucet_account: Option<AccountId>,
-    stakers: Vec<AccountId>,
+    controller_accounts: Option<Vec<AccountId>>,
+    stash_accounts: Option<Vec<AccountId>>,
 ) -> GenesisConfig {
     let special_accounts = match faucet_account {
         Some(faucet_id) => vec![sudo_account.clone(), faucet_id],
@@ -285,37 +327,62 @@ fn generate_genesis_config(
     let unique_accounts: Vec<AccountId> = deduplicate(
         to_account_ids(&authorities)
             .chain(special_accounts)
-            .chain(stakers.iter().cloned())
             .collect(),
     );
 
-    // to have total issuance 300M (for devnet/tests/local runs only)
-    const ENDOWMENT: u128 = 60_000_000u128 * 10u128.pow(TOKEN_DECIMALS);
+    let unique_accounts_balances = unique_accounts
+        .into_iter()
+        .map(|account| (account, ENDOWMENT));
 
-    GenesisConfig {
-        system: SystemConfig {
-            // Add Wasm runtime to storage.
-            code: wasm_binary.to_vec(),
-        },
-        balances: BalancesConfig {
-            // Configure endowed accounts with an initial, significant balance
-            balances: unique_accounts
+    let (balances, members, keys, stakers) = match (controller_accounts, stash_accounts) {
+        (Some(controllers), Some(stashes)) => {
+            let balances: Vec<(AccountId, u128)> = unique_accounts_balances
+                .chain(
+                    controllers
+                        .clone()
+                        .into_iter()
+                        .map(|account| (account, TOKEN)),
+                )
+                .chain(
+                    stashes
+                        .clone()
+                        .into_iter()
+                        .map(|account| (account, MIN_VALIDATOR_BOND + TOKEN)),
+                )
+                .collect();
+            let members = stashes.clone();
+            let keys = authorities
+                .iter()
+                .zip(stashes.clone())
+                .map(|(auth, stash)| {
+                    (
+                        stash.clone(),
+                        stash,
+                        SessionKeys {
+                            aura: auth.aura_key.clone(),
+                            aleph: auth.aleph_key.clone(),
+                        },
+                    )
+                })
+                .collect();
+            let stakers = stashes
                 .into_iter()
-                .map(|account| (account, ENDOWMENT))
-                .collect(),
-        },
-        aura: AuraConfig {
-            authorities: vec![],
-        },
-        sudo: SudoConfig {
-            // Assign network admin rights.
-            key: sudo_account,
-        },
-        elections: ElectionsConfig {
-            members: to_account_ids(&authorities).collect(),
-        },
-        session: SessionConfig {
-            keys: authorities
+                .zip(controllers)
+                .map(|(stash, controller)| {
+                    (
+                        stash,
+                        controller,
+                        MIN_VALIDATOR_BOND,
+                        StakerStatus::Validator,
+                    )
+                })
+                .collect();
+            (balances, members, keys, stakers)
+        }
+        (_, _) => {
+            let balances = unique_accounts_balances.collect();
+            let members = to_account_ids(&authorities).collect();
+            let keys = authorities
                 .iter()
                 .map(|auth| {
                     (
@@ -327,26 +394,40 @@ fn generate_genesis_config(
                         },
                     )
                 })
-                .collect(),
+                .collect();
+            let stakers = vec![];
+            (balances, members, keys, stakers)
+        }
+    };
+
+    GenesisConfig {
+        system: SystemConfig {
+            // Add Wasm runtime to storage.
+            code: wasm_binary.to_vec(),
         },
+        balances: BalancesConfig {
+            // Configure endowed accounts with an initial, significant balance
+            balances,
+        },
+        aura: AuraConfig {
+            authorities: vec![],
+        },
+        sudo: SudoConfig {
+            // Assign network admin rights.
+            key: sudo_account,
+        },
+        elections: ElectionsConfig {
+            members: members.clone(),
+        },
+        session: SessionConfig { keys },
         staking: StakingConfig {
             force_era: Forcing::NotForcing,
             validator_count: authorities.len() as u32,
             // to satisfy some e2e tests as this cannot be changed during runtime
             minimum_validator_count: 4,
-            invulnerables: to_account_ids(&authorities).collect(),
+            invulnerables: members,
             slash_reward_fraction: Perbill::from_percent(10),
-            stakers: stakers
-                .into_iter()
-                .map(|account_id| {
-                    (
-                        account_id.clone(),
-                        account_id,
-                        MIN_VALIDATOR_BOND,
-                        StakerStatus::Validator,
-                    )
-                })
-                .collect(),
+            stakers,
             min_validator_bond: MIN_VALIDATOR_BOND,
             min_nominator_bond: MIN_NOMINATOR_BOND,
             ..Default::default()
