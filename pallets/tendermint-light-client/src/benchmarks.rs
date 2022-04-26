cd5bf883a82f01ecd7d3ba71e9a4947e1d742b14
filip <fbielejec@gmail.com>
2022-04-26 16:48:39 +0200

    cleanup

--- pallets/tendermint-light-client/src/benchmarks.rs
+++ pallets/tendermint-light-client/src/benchmarks.rs
@@ -6,81 +6,89 @@ use crate::{
 use frame_benchmarking::{benchmarks, whitelisted_caller};
 use frame_support::{
     assert_ok, log,
-    traits::{Get, OriginTrait, UnixTime},
+    traits::{Get, UnixTime},
 };
-use frame_system::{Pallet as System, RawOrigin};
+use frame_system::RawOrigin;
 use generator::generate_consecutive_blocks;
 use pallet_timestamp::Pallet as Timestamp;
 use scale_info::prelude::string::String;
-use sp_runtime::traits::{AtLeast32Bit, Bounded, One, Zero};
+use sp_runtime::traits::{AtLeast32Bit, One, Zero};
 
 fn mul<M>(value: M, by: u64) -> M
 where
     M: AtLeast32Bit,
 {
-    let mut res = value;
-    for _ in 0..by {
-        res += M::one();
-    }
-    res
+    (0..by).into_iter().fold(value, |mut sum, _| {
+        sum += M::one();
+        sum
+    })
 }
 
 benchmarks! {
-    // benchmark_for_initialize_client {
+    benchmark_for_initialize_client {
 
-    //     let v in 1 .. 2;//T::MaxVotesCount::get();
-    //     let mut blocks = generate_consecutive_blocks (1, String::from ("test-chain"), v, 3, TimestampStorage::new (3, 0));
+        let v in 1 .. T::MaxVotesCount::get();
 
-    //     let initial_block = blocks.pop ().unwrap ();
-    //     let options = LightClientOptionsStorage::default();
+        // set `now` to 1 millis after epoch time 0 to avoid errors from Timestamp pallet
+        Timestamp::<T>::set_timestamp(T::Moment::one ());
 
-    // }: initialize_client(RawOrigin::Root, options, initial_block.clone ())
+        // initial block at 1970-01-01T00:00:03Z
+        let mut blocks = generate_consecutive_blocks (1, String::from ("test-chain"), v, 3, TimestampStorage::new (3, 0));
 
-    //     verify {
-    //         // check if persisted
-    //         let last_imported = TendermintLightClient::<T>::get_last_imported_block_hash();
-    //         assert_eq!(
-    //             initial_block.signed_header.commit.block_id.hash,
-    //             TendermintHashStorage::Some (last_imported)
-    //         );
-    //     }
+        let initial_block = blocks.pop ().unwrap ();
+        let options = LightClientOptionsStorage::default();
 
-    // benchmark_for_update_client {
+    }: initialize_client(RawOrigin::Root, options, initial_block.clone ())
 
-    //     let v in 1 .. 2;//T::MaxVotesCount::get();
-    //     let mut blocks = generate_consecutive_blocks (2, String::from ("test-chain"), v, 3, TimestampStorage::new (3, 0));
+        verify {
+            // check if persisted
+            let last_imported = TendermintLightClient::<T>::get_last_imported_block_hash();
+            assert_eq!(
+                initial_block.signed_header.commit.block_id.hash,
+                TendermintHashStorage::Some (last_imported)
+            );
+        }
 
-    //     let options = LightClientOptionsStorage::default();
-    //     let initial_block = blocks.pop ().unwrap ();
+    benchmark_for_update_client {
 
-    //     assert_ok!(TendermintLightClient::<T>::initialize_client(
-    //         RawOrigin::Root.into() ,
-    //         options,
-    //         initial_block
-    //     ));
+        let v in 1 .. T::MaxVotesCount::get();
 
-    //     let caller: T::AccountId = whitelisted_caller();
-    //     let untrusted_block = blocks.pop ().unwrap ();
+        // set `now` to 1 millis after epoch time 0 to avoid errors from Timestamp pallet
+        Timestamp::<T>::set_timestamp(T::Moment::one ());
+        // initial block at 1970-01-01T00:00:03Z
+        let mut blocks = generate_consecutive_blocks (2, String::from ("test-chain"), v, 3, TimestampStorage::new (3, 0));
 
-    // }: update_client(RawOrigin::Signed(caller.clone()), untrusted_block.clone ())
+        let options = LightClientOptionsStorage::default();
+        let initial_block = blocks.pop ().unwrap ();
 
-    //     verify {
-    //         // check if persisted
-    //         let last_imported = TendermintLightClient::<T>::get_last_imported_block_hash();
-    //         assert_eq!(
-    //             untrusted_block.signed_header.commit.block_id.hash,
-    //             TendermintHashStorage::Some (last_imported)
-    //         );
-    //     }
+        assert_ok!(TendermintLightClient::<T>::initialize_client(
+            RawOrigin::Root.into() ,
+            options,
+            initial_block
+        ));
+
+        let caller: T::AccountId = whitelisted_caller();
+        let untrusted_block = blocks.pop ().unwrap ();
+
+    }: update_client(RawOrigin::Signed(caller.clone()), untrusted_block.clone ())
+
+        verify {
+            // check if persisted
+            let last_imported = TendermintLightClient::<T>::get_last_imported_block_hash();
+            assert_eq!(
+                untrusted_block.signed_header.commit.block_id.hash,
+                TendermintHashStorage::Some (last_imported)
+            );
+        }
 
     // this benchmarks update_client call which causes pruning of the oldest block
     // mock runtime keeps 3 headers, therefore rollover happens after inserting a fourth consecutive block
     benchmark_for_update_client_with_pruning {
 
-        let v in 1 .. 1; // T::MaxVotesCount::get();
+        let v in 1 .. T::MaxVotesCount::get();
 
-        let mut block_number = System::<T>::block_number();
-        System::<T>::set_block_number(block_number);
+        // let mut block_number = System::<T>::block_number();
+        // System::<T>::set_block_number(block_number);
 
         let mut now = T::Moment::zero ();
         Timestamp::<T>::set_timestamp(now);
@@ -101,10 +109,8 @@ benchmarks! {
 
         for _ in 1..T::HeadersToKeep::get() {
 
-            now = now + mul(T::Moment::one (), 1000) ;
-            block_number = block_number + One::one();
-
-            System::<T>::set_block_number(block_number);
+            // advance timestamp by 1 second
+            now = now + mul(T::Moment::one (), 1000);
             Timestamp::<T>::set_timestamp(now);
 
             log::info!(target: "runtime-benchmarks::tendermint-lc", "now {:?}", T::TimeProvider::now ());
