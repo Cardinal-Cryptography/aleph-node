d92691161d8bd25589f717ba289b39293a8d264a
Marcin <marcin.radecki@cardinals.cc>
2022-05-10 14:06:47 +0200

    A0-613 Resolved conflicts part1

--- bin/runtime/src/elections/mod.rs
+++ bin/runtime/src/elections/mod.rs
@@ -1,6 +1,6 @@
 use crate::{
-    AccountId, BlockNumber, EraIndex, Perbill, Runtime, Session, SessionPeriod,
-    SessionsPerEra, Staking, Vec,
+    AccountId, BlockNumber, EraIndex, Perbill, Runtime, Session, SessionPeriod, SessionsPerEra,
+    Staking, Vec,
 };
 use pallet_elections::MembersPerSession;
 use primitives::{SessionIndex, TOKEN};
@@ -48,7 +48,8 @@ impl pallet_authorship::EventHandler<AccountId, BlockNumber> for StakeReward {
 
         let total = total_exposure(active_era, &validator);
         let sessions_per_era = SessionsPerEra::get();
-        let blocks_to_produce_per_session = SessionPeriod::get() / MembersPerSession::<Runtime>::get().unwrap();
+        let blocks_to_produce_per_session =
+            SessionPeriod::get() / MembersPerSession::<Runtime>::get();
         let points_per_block =
             points_per_block(total, sessions_per_era, blocks_to_produce_per_session);
 
@@ -60,37 +61,57 @@ impl pallet_authorship::EventHandler<AccountId, BlockNumber> for StakeReward {
     fn note_uncle(_author: AccountId, _age: BlockNumber) {}
 }
 
-// Choose a subset of all the validators for current era that contains all the
-// reserved nodes. Non reserved ones are chosen in consecutive batches for every session
-fn rotate() -> Option<Vec<AccountId>> {
-    let current_era = match Staking::active_era() {
-        Some(ae) if ae.index > 0 => ae.index,
-        _ => return None,
-    };
-    let mut all_validators: Vec<AccountId> =
-        pallet_staking::ErasStakers::<Runtime>::iter_key_prefix(current_era).collect();
+fn rotate<T: Clone + PartialEq>(
+    current_era: EraIndex,
+    current_session: SessionIndex,
+    n_validators: usize,
+    mut all_validators: Vec<T>,
+    mut reserved: Vec<T>,
+) -> Option<Vec<T>> {
+    if current_era == 0 {
+        return None;
+    }
 
-    let mut validators = pallet_elections::ErasReserved::<Runtime>::get();
-    all_validators.retain(|v| !validators.contains(v));
+    all_validators.retain(|v| !reserved.contains(v));
     let n_all_validators = all_validators.len();
 
-    let n_validators = MembersPerSession::<Runtime>::get().unwrap() as usize;
-    let free_seats = n_validators.checked_sub(validators.len()).unwrap();
+    let free_seats = n_validators.checked_sub(reserved.len()).unwrap();
 
     // The validators for the committee at the session `n` are chosen as follow:
     // 1. Reserved validators are always chosen.
     // 2. Given non-reserved list of validators the chosen ones are from the range:
     // `n * free_seats` to `(n + 1) * free_seats` where free_seats is equal to free number of free
     // seats in the committee after reserved nodes are added.
-    let current_session = Session::current_index() as usize;
-    let first_validator = current_session * free_seats;
+    let first_validator = current_session as usize * free_seats;
 
-    validators.extend(
+    reserved.extend(
         (first_validator..first_validator + free_seats)
             .map(|i| all_validators[i % n_all_validators].clone()),
     );
 
-    Some(validators)
+    Some(reserved)
+}
+
+// Choose a subset of all the validators for current era that contains all the
+// reserved nodes. Non reserved ones are chosen in consecutive batches for every session
+fn rotate_committee() -> Option<Vec<AccountId>> {
+    let current_era = match Staking::active_era() {
+        Some(ae) if ae.index > 0 => ae.index,
+        _ => return None,
+    };
+    let all_validators: Vec<AccountId> =
+        pallet_staking::ErasStakers::<Runtime>::iter_key_prefix(current_era).collect();
+    let reserved = pallet_elections::ErasReserved::<Runtime>::get();
+    let n_validators = pallet_elections::MembersPerSession::<Runtime>::get() as usize;
+    let current_session = Session::current_index();
+
+    rotate(
+        current_era,
+        current_session,
+        n_validators,
+        all_validators,
+        reserved,
+    )
 }
 
 fn populate_reserved_on_next_era_start(start_index: SessionIndex) {
@@ -102,35 +123,22 @@ fn populate_reserved_on_next_era_start(start_index: SessionIndex) {
     // `n+1` starts a new era.
     if let Some(era_index) = Staking::eras_start_session_index(current_era + 1) {
         if era_index == start_index {
-            let reserved = pallet_staking::Invulnerables::<Runtime>::get();
-            pallet_elections::ErasReserved::<Runtime>::put(reserved);
+            let reserved_validators = pallet_staking::Invulnerables::<Runtime>::get();
+            pallet_elections::ErasReserved::<Runtime>::put(reserved_validators);
         }
     }
 }
 
-fn mark_participation(committee: &Option<Vec<AccountId>>, session: SessionIndex) {
-    let committee = match committee {
-        Some(c) => c,
-        None => return,
-    };
-
-    for validator in committee {
-        pallet_rewards::SessionParticipated::<Runtime>::insert(&session, &validator, true);
-    }
-}
-
 type SM = pallet_session::historical::NoteHistoricalRoot<Runtime, Staking>;
-pub struct SampleSessionManager;
+pub struct ComiteeRotationSessionManager;
 
-impl pallet_session::SessionManager<AccountId> for SampleSessionManager {
+impl pallet_session::SessionManager<AccountId> for ComiteeRotationSessionManager {
     fn new_session(new_index: SessionIndex) -> Option<Vec<AccountId>> {
         SM::new_session(new_index);
-
-        let committee = rotate();
         // new session is always called before the end_session of the previous session
         // so we need to populate reserved set here not on start_session nor end_session
+        let committee = rotate_committee();
         populate_reserved_on_next_era_start(new_index);
-        mark_participation(&committee, new_index);
 
         committee
     }
@@ -144,8 +152,18 @@ impl pallet_session::SessionManager<AccountId> for SampleSessionManager {
     }
 
     fn start_session(start_index: SessionIndex) {
-        reward_for_session_non_committee(start_index);
         SM::start_session(start_index)
+    }
+}
+
+fn mark_participation(committee: &Option<Vec<AccountId>>, session: SessionIndex) {
+    let committee = match committee {
+        Some(c) => c,
+        None => return,
+    };
+
+    for validator in committee {
+        pallet_rewards::SessionParticipated::<Runtime>::insert(&session, &validator, true);
     }
 }
 
