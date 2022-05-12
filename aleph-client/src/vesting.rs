fd16dc256af4d3f41e98ec0ba3d2269e59e1173d
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-05-12 11:27:18 +0200

    review

--- aleph-client/src/vesting.rs
+++ aleph-client/src/vesting.rs
@@ -1,4 +1,4 @@
-use anyhow::Result as AnyResult;
+use anyhow::{ensure, Result as AnyResult};
 use log::info;
 pub use pallet_vesting::VestingInfo;
 use sp_core::Pair;
@@ -14,45 +14,49 @@ use crate::{account_from_keypair, try_send_xt, AccountId, BlockNumber, Connectio
 pub enum VestingError {
     #[error("🦺❌ Account has no active vesting schedules.")]
     NotVesting,
+    #[error("🦺❌ The connection should be signed.")]
+    UnsignedConnection,
 }
 
 pub type VestingSchedule = VestingInfo<Balance, BlockNumber>;
 
 const PALLET: &str = "Vesting";
 
-/// Calls `pallet_vesting::vest` for `who`, i.e. makes all unlocked balances transferable.
+/// Checks whether `connection` is signed. If so, returns the signer keypair.
+fn ensure_signed(connection: &Connection) -> AnyResult<KeyPair> {
+    let maybe_signer = connection.signer.clone();
+    ensure!(maybe_signer.is_some(), VestingError::UnsignedConnection);
+    Ok(maybe_signer.expect("Must be `Some(_)`: just checked."))
+}
+
+/// Calls `pallet_vesting::vest` for the signer of `connection`, i.e. makes all unlocked balances
+/// transferable.
 ///
-/// Does not expect `connection` to be signed. Fails if transaction could not have been sent.
+/// Fails if `connection` is unsigned or transaction could not have been sent.
 ///
 /// *Note*: This function returns `Ok(_)` even if the account has no active vesting schedules
 /// and thus the extrinsic was not successful. However, semantically it is still correct.
-pub fn vest(connection: Connection, who: KeyPair) -> AnyResult<()> {
-    // Ensure that we make a call as `account`.
-    let connection = connection.set_signer(who.clone());
+pub fn vest(connection: Connection) -> AnyResult<()> {
+    let vester = ensure_signed(&connection)?;
     let xt = compose_extrinsic!(connection, PALLET, "vest");
     let block_hash = try_send_xt(&connection, xt, Some("Vesting"), Finalized)?
         .expect("For `Finalized` status a block hash should be returned");
     info!(
         target: "aleph-client", "Vesting for the account {:?}. Finalized in block {:?}",
-        account_from_keypair(&who), block_hash
+        account_from_keypair(&vester), block_hash
     );
     Ok(())
 }
 
-/// Calls `pallet_vesting::vest_other` by `caller` on behalf of `vest_account`, i.e. makes all
-/// unlocked balances of `vest_account` transferable.
+/// Calls `pallet_vesting::vest_other` by the signer of `connection` on behalf of `vest_account`,
+/// i.e. makes all unlocked balances of `vest_account` transferable.
 ///
-/// Does not expect `connection` to be signed. Fails if transaction could not have been sent.
+/// Fails if `connection` is not signed or transaction could not have been sent.
 ///
 /// *Note*: This function returns `Ok(_)` even if the account has no active vesting schedules
 /// and thus the extrinsic was not successful. However, semantically it is still correct.
-pub fn vest_other(
-    connection: Connection,
-    caller: KeyPair,
-    vest_account: AccountId,
-) -> AnyResult<()> {
-    // Ensure that we make a call as `caller`.
-    let connection = connection.set_signer(caller);
+pub fn vest_other(connection: Connection, vest_account: AccountId) -> AnyResult<()> {
+    ensure_signed(&connection)?;
     let xt = compose_extrinsic!(
         connection,
         PALLET,
@@ -65,17 +69,16 @@ pub fn vest_other(
     Ok(())
 }
 
-/// Performs a vested transfer from `receiver` to `sender` according to `schedule`.
+/// Performs a vested transfer from the signer of `connection` to `receiver` according to
+/// `schedule`.
 ///
-/// Does not expect `connection` to be signed. Fails if transaction could not have been sent.
+/// Fails if `connection` is unsigned or transaction could not have been sent.
 pub fn vested_transfer(
     connection: Connection,
-    sender: KeyPair,
     receiver: AccountId,
     schedule: VestingSchedule,
 ) -> AnyResult<()> {
-    // Ensure that we make a call as `sender`.
-    let connection = connection.set_signer(sender);
+    ensure_signed(&connection)?;
     let xt = compose_extrinsic!(
         connection,
         PALLET,
@@ -99,20 +102,14 @@ pub fn get_schedules(connection: &Connection, who: AccountId) -> AnyResult<Vec<V
         .ok_or_else(|| VestingError::NotVesting.into())
 }
 
-/// Merges two vesting schedules (at indices `idx1` and `idx2`) of `who`.
+/// Merges two vesting schedules (at indices `idx1` and `idx2`) of the signer of `connection`.
 ///
-/// Does not expect `connection` to be signed. Fails if transaction could not have been sent.
+/// Fails if `connection` is unsigned or transaction could not have been sent.
 ///
 /// *Note*: This function returns `Ok(_)` even if the account has no active vesting schedules, or
 /// it has fewer schedules than `max(idx1, idx2) - 1` and thus the extrinsic was not successful.
-pub fn merge_schedules(
-    connection: Connection,
-    who: KeyPair,
-    idx1: u32,
-    idx2: u32,
-) -> AnyResult<()> {
-    // Ensure that we make a call as `who`.
-    let connection = connection.set_signer(who.clone());
+pub fn merge_schedules(connection: Connection, idx1: u32, idx2: u32) -> AnyResult<()> {
+    let who = ensure_signed(&connection)?;
     let xt = compose_extrinsic!(connection, PALLET, "merge_schedules", idx1, idx2);
     let block_hash = try_send_xt(&connection, xt, Some("Merge vesting schedules"), Finalized)?
         .expect("For `Finalized` status a block hash should be returned");
--- bin/cliain/src/vesting.rs
+++ bin/cliain/src/vesting.rs
@@ -1,25 +1,14 @@
 use aleph_client::{
-    account_from_keypair, keypair_from_string, BlockNumber, Connection, KeyPair, VestingSchedule,
+    account_from_keypair, keypair_from_string, BlockNumber, Connection, VestingSchedule,
 };
 use log::{error, info};
 use primitives::{Balance, TOKEN};
-
-/// Retrieves signer from `connection`.
-///
-/// Panics if `connection` is not signed.
-fn get_caller(connection: &Connection) -> KeyPair {
-    connection
-        .signer
-        .clone()
-        .expect("Connection should be signed")
-}
 
 /// Delegates to `aleph_client::vest`.
 ///
 /// `connection` should be signed: the vesting is performed for the signer.
 pub fn vest(connection: Connection) {
-    let caller = get_caller(&connection);
-    match aleph_client::vest(connection, caller) {
+    match aleph_client::vest(connection) {
         Ok(_) => info!("Vesting has succeeded"),
         Err(e) => error!("Vesting has failed with:\n {:?}", e),
     }
@@ -30,9 +19,8 @@ pub fn vest(connection: Connection) {
 /// `connection` should be signed: the vesting is performed by the signer for
 /// `vesting_account_seed`.
 pub fn vest_other(connection: Connection, vesting_account_seed: String) {
-    let caller = get_caller(&connection);
     let vester = account_from_keypair(&keypair_from_string(vesting_account_seed.as_str()));
-    match aleph_client::vest_other(connection, caller, vester) {
+    match aleph_client::vest_other(connection, vester) {
         Ok(_) => info!("Vesting on behalf has succeeded"),
         Err(e) => error!("Vesting on behalf has failed with:\n {:?}", e),
     }
@@ -50,11 +38,10 @@ pub fn vested_transfer(
     per_block: Balance,
     starting_block: BlockNumber,
 ) {
-    let sender = get_caller(&connection);
     let receiver = account_from_keypair(&keypair_from_string(target_seed.as_str()));
     let schedule =
         VestingSchedule::new(amount_in_tokens as u128 * TOKEN, per_block, starting_block);
-    match aleph_client::vested_transfer(connection, sender, receiver, schedule) {
+    match aleph_client::vested_transfer(connection, receiver, schedule) {
         Ok(_) => info!("Vested transfer has succeeded"),
         Err(e) => error!("Vested transfer has failed with:\n {:?}", e),
     }
