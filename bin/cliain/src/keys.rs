bb64ecc85ef7635188ef5e1da5084999dcd5784d
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-05-16 12:31:59 +0200

    rotating does not require sudo

--- bin/cliain/src/keys.rs
+++ bin/cliain/src/keys.rs
@@ -1,6 +1,6 @@
 use aleph_client::{
-    rotate_keys as rotate, rotate_keys_raw_result, set_keys as set, staking_bond, RootConnection,
-    SessionKeys, SignedConnection,
+    rotate_keys as rotate, rotate_keys_raw_result, set_keys as set, staking_bond, AnyConnection,
+    RootConnection, SessionKeys, SignedConnection,
 };
 use log::info;
 use primitives::staking::MIN_VALIDATOR_BOND;
@@ -25,7 +25,7 @@ pub fn set_keys(connection: SignedConnection, new_keys: String) {
     );
 }
 
-pub fn rotate_keys(connection: RootConnection) {
+pub fn rotate_keys<C: AnyConnection>(connection: C) {
     let new_keys = rotate_keys_raw_result(&connection).expect("Failed to retrieve keys");
     info!("Rotated keys: {:?}", new_keys);
 }
