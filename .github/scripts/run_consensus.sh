c85b958e12031a393156f85b68d3602d0c1fdf9a
Marcin <marcin.radecki@cardinals.cc>
2022-05-25 07:00:52 +0200

    A0-984 Refactor e2e test to used unified validators accounts with local runs.

--- .github/scripts/run_consensus.sh
+++ .github/scripts/run_consensus.sh
@@ -2,21 +2,30 @@
 
 set -e
 
+# change it when increasing number of node containers
+export NODE_COUNT=5
+
 mkdir -p docker/data/
 
-# source account ids
-source docker/env
+echo "Generating accounts ids..."
+declare -a account_ids
+for node_index in $(seq 0 $((NODE_COUNT - 1))); do
+  account_ids+=($(docker run -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e node_index aleph-node:latest -c "aleph-node key inspect //$node_index | grep \"SS58 Address:\" | awk \"{print \\\$3;}\""))
+done
+# space separated ids
+validator_ids="${account_ids[*]}"
+# comma separated ids
+validator_ids="${validator_ids//${IFS:0:1}/,}"
 
-# generate chain spec and keystores based on authorites passed via --account-ids
-docker run -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e DAMIAN -e TOMASZ -e ZBYSZKO -e HANSU -e JULIA -e RUST_LOG=info \
- aleph-node:latest -c "aleph-node bootstrap-chain --base-path /data --account-ids $DAMIAN,$TOMASZ,$ZBYSZKO,$HANSU,$JULIA --sudo-account-id $DAMIAN > /data/chainspec.json"
+echo "Generate chainspec and keystores..."
+docker run -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e RUST_LOG=info -e validator_ids aleph-node:latest -c \
+"aleph-node bootstrap-chain --base-path /data --account-ids $validator_ids  > /data/chainspec.json"
 
-# get bootnote peer id
-export BOOTNODE_PEER_ID=$(docker run -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e DAMIAN -e RUST_LOG=info aleph-node:latest -c "aleph-node key inspect-node-key --file /data/$DAMIAN/p2p_secret")
+echo "Genereting bootnode peer id..."
+bootnote_account=${account_ids[0]}
+export BOOTNODE_PEER_ID=$(docker run -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e bootnote_account -e RUST_LOG=info aleph-node:latest -c "aleph-node key inspect-node-key --file /data/$bootnote_account/p2p_secret")
 
-echo "BOOTNODE_PEER_ID : $BOOTNODE_PEER_ID"
-
-# Run consensus party
+echo "Running ${NODE_COUNT} containers..."
 docker-compose -f docker/docker-compose.yml up -d
 
 exit $?
--- scripts/run_e2e.sh
+++ scripts/run_e2e.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+# change it when increasing number of node containers
+export NODE_COUNT=5
+
 cd e2e-tests/
 
-RUST_LOG=aleph_e2e_client=info,aleph-client=info cargo run -- --node 127.0.0.1:9943
+RUST_LOG=aleph_e2e_client=info,aleph-client=info cargo run -- --node 127.0.0.1:9943 --validators_count $NODE_COUNT
 
 exit $?
