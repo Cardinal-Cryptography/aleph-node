071eb024d70710106926d7b8700c465ec3afc99a
Marcin <marcin.radecki@cardinals.cc>
2022-05-27 16:09:54 +0200

    A0-984 updated run_consensus.sh to use variable for docker image

--- .github/scripts/run_consensus.sh
+++ .github/scripts/run_consensus.sh
@@ -5,6 +5,8 @@ set -e
 # change it when increasing number of node containers
 export NODE_COUNT=5
 
+export NODE_IMAGE=aleph-node:latest
+
 mkdir -p docker/data/
 
 function generate_authorities {
@@ -14,7 +16,7 @@ function generate_authorities {
   declare -a account_ids
   for node_index in $(seq 0 $((authorities_count - 1))); do
     echo "Generating authority ${node_index} from key //${node_index}" >&2
-    account_ids+=($(docker run --rm -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" aleph-node:latest -c "aleph-node key inspect //$node_index | grep \"SS58 Address:\" | awk \"{print \\\$3;}\""))
+    account_ids+=($(docker run --rm -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" "${NODE_IMAGE}" -c "aleph-node key inspect //$node_index | grep \"SS58 Address:\" | awk \"{print \\\$3;}\""))
   done
   echo "${account_ids[*]}"
 }
@@ -26,7 +28,7 @@ function generate_chainspec {
   validator_ids="${authorities//${IFS:0:1}/,}"
 
   echo "Generate chainspec and keystores with sudo account //Alice ..."
-  docker run --rm -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e RUST_LOG=debug aleph-node:latest -c \
+  docker run --rm -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e RUST_LOG=debug "${NODE_IMAGE}" -c \
   "aleph-node bootstrap-chain --base-path /data --account-ids $validator_ids > /data/chainspec.json"
 }
 
@@ -34,7 +36,7 @@ function generete_bootnode_peer_id {
   local bootnode_account="$1"
 
   echo "Generate bootnode peer id..."
-  export BOOTNODE_PEER_ID=$(docker run --rm -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e RUST_LOG=info aleph-node:latest -c "aleph-node key inspect-node-key --file /data/$bootnode_account/p2p_secret")
+  export BOOTNODE_PEER_ID=$(docker run --rm -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e RUST_LOG=info "${NODE_IMAGE}" -c "aleph-node key inspect-node-key --file /data/$bootnode_account/p2p_secret")
 }
 
 function run_containers {
