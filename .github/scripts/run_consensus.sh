513da8501aa545064c69a0b71da3a483348699a2
Marcin <marcin.radecki@cardinals.cc>
2022-05-27 09:36:23 +0200

    A9-984 Review remarks

--- .github/scripts/run_consensus.sh
+++ .github/scripts/run_consensus.sh
@@ -7,25 +7,46 @@ export NODE_COUNT=5
 
 mkdir -p docker/data/
 
-echo "Generating accounts ids..."
-declare -a account_ids
-for node_index in $(seq 0 $((NODE_COUNT - 1))); do
-  account_ids+=($(docker run -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e node_index aleph-node:latest -c "aleph-node key inspect //$node_index | grep \"SS58 Address:\" | awk \"{print \\\$3;}\""))
-done
-# space separated ids
-validator_ids="${account_ids[*]}"
-# comma separated ids
-validator_ids="${validator_ids//${IFS:0:1}/,}"
+function generate_authorities {
+  local authorities_count="$1"
 
-echo "Generate chainspec and keystores..."
-docker run -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e RUST_LOG=info -e validator_ids aleph-node:latest -c \
-"aleph-node bootstrap-chain --base-path /data --account-ids $validator_ids  > /data/chainspec.json"
+  echo "Generating ${authorities_count} authorities accounts ids..." >&2
+  declare -a account_ids
+  for node_index in $(seq 0 $((authorities_count - 1))); do
+    echo "Generating authority ${node_index} from key //${node_index}" >&2
+    account_ids+=($(docker run --rm -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" aleph-node:latest -c "aleph-node key inspect //$node_index | grep \"SS58 Address:\" | awk \"{print \\\$3;}\""))
+  done
+  echo "${account_ids[*]}"
+}
 
-echo "Genereting bootnode peer id..."
-bootnote_account=${account_ids[0]}
-export BOOTNODE_PEER_ID=$(docker run -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e bootnote_account -e RUST_LOG=info aleph-node:latest -c "aleph-node key inspect-node-key --file /data/$bootnote_account/p2p_secret")
+function generate_chainspec {
+  local authorities="$1"
 
-echo "Running ${NODE_COUNT} containers..."
-docker-compose -f docker/docker-compose.yml up -d
+  # comma separated ids
+  validator_ids="${authorities//${IFS:0:1}/,}"
+
+  echo "Generate chainspec and keystores with sudo account //Alice ..."
+  docker run --rm -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e RUST_LOG=debug aleph-node:latest -c \
+  "aleph-node bootstrap-chain --base-path /data --account-ids $validator_ids > /data/chainspec.json"
+}
+
+function generete_bootnode_peer_id {
+  local bootnode_account="$1"
+
+  echo "Generate bootnode peer id..."
+  export BOOTNODE_PEER_ID=$(docker run --rm -v $(pwd)/docker/data:/data --entrypoint "/bin/sh" -e RUST_LOG=info aleph-node:latest -c "aleph-node key inspect-node-key --file /data/$bootnode_account/p2p_secret")
+}
+
+function run_containers {
+  local authorities_count="$1"
+
+  echo "Running ${authorities_count} containers..."
+  docker-compose -f docker/docker-compose.yml up -d
+}
+
+authorities=$(generate_authorities ${NODE_COUNT})
+generate_chainspec ${authorities[*]}
+generete_bootnode_peer_id ${authorities[0]}
+run_containers ${NODE_COUNT}
 
 exit $?
