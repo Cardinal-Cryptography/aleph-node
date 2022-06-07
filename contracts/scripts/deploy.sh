53fbdd0a5211e03e7243a5c87a8b5f652c77188c
filip <fbielejec@gmail.com>
2022-06-07 15:23:26 +0200

    tests

--- contracts/scripts/deploy.sh
+++ contracts/scripts/deploy.sh
@@ -5,7 +5,6 @@ NODE=ws://127.0.0.1:9943
 ALICE=5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
 ALICE_SEED=//Alice
 
-# NOTE: needs some balance
 NODE0=5D34dL5prEUaGNQtPPZ3yN5Y6BnkfXunKXXz6fo7ZJbLwRRH
 NODE0_SEED=//0
 
@@ -13,7 +12,6 @@ LIFETIME=5
 TOTAL_BALANCE=1000
 GAME_BALANCE=900
 
-# CONTRACTS_PATH=/home/filip/CloudStation/aleph/aleph-node/contracts
 CONTRACTS_PATH=$(pwd)/contracts
 
 ## --- DEPLOY TOKEN CONTRACT
@@ -23,11 +21,8 @@ cargo +nightly contract build --release
 
 CONTRACT=$(cargo contract instantiate --url $NODE --constructor new --args $TOTAL_BALANCE --suri $ALICE_SEED)
 BUTTON_TOKEN=$(echo "$CONTRACT" | grep Contract | tail -1 | cut -c 15-)
-# BUTTON_TOKEN=5C6tBqxFKoPVhGfAzgR8JGMrMZNJy7pDjB5gvrs1z5mPscKX
-# CODE_HASH=$(echo "$CONTRACT" | grep "Code hash" | tail -1 | cut -c 15-)
 
-# echo "code hash: " $CODE_HASH
-echo "contract address: " $BUTTON_TOKEN
+echo "button token contract address: " $BUTTON_TOKEN
 
 ## --- DEPLOY GAME CONTRACT
 
@@ -35,19 +30,13 @@ cd $CONTRACTS_PATH/yellow-button
 cargo +nightly contract build --release
 
 CONTRACT=$(cargo contract instantiate --url $NODE --constructor new --args $BUTTON_TOKEN $LIFETIME --suri $ALICE_SEED)
-# CONTRACT=$(cargo contract instantiate --url $NODE --constructor new --args $BUTTON_TOKEN $LIFETIME --suri $ALICE_SEED --code-hash )
-
 YELLOW_BUTTON=$(echo "$CONTRACT" | grep Contract | tail -1 | cut -c 15-)
-# YELLOW_BUTTON=5H9BynYoymMsCqqsX4f6iDi9MKfacL2mmEhVv6ByjnKfH1HM
-CODE_HASH=$(echo "$CONTRACT" | grep "Code hash" | tail -1 | cut -c 15-)
 
-echo "code hash: " $CODE_HASH
-echo "contract address: " $YELLOW_BUTTON
+echo "game contract address: " $YELLOW_BUTTON
 
 ## --- TRANSFER BALANCE TO THE GAME CONTRACT
 
 cd $CONTRACTS_PATH/button-token
-
 cargo contract call --url $NODE --contract $BUTTON_TOKEN --message transfer --args $YELLOW_BUTTON $GAME_BALANCE --suri $ALICE_SEED
 
 ## -- DEBUGGING CALLS
@@ -82,8 +71,7 @@ cargo contract call --url $NODE --contract $YELLOW_BUTTON --message press --suri
 
 ## --- TODO : assert rewards distribution
 
-# 
-
+#
 
 echo "Done"
 exit $?
