22d0d838a8505f66fb3c8a62611810c9ae630348
Artemis Silvestris <artemis.silvestris@cardinals.cc>
2022-07-06 14:12:31 -0700

    aleph-party: Only delay start when initial syncing
    Add a method to `RequestBlocks` to be able to detect this.

--- finality-aleph/src/party/mod.rs
+++ finality-aleph/src/party/mod.rs
@@ -439,7 +439,7 @@ where
             sleep(Duration::from_secs(1)).await;
             previous_finalized_number = finalized_number;
             finalized_number = self.client.info().finalized_number;
-            if finalized_number == previous_finalized_number {
+            if finalized_number == previous_finalized_number || !self.block_requester.is_major_syncing() {
                 starting_session =
                     session_id_from_block_num::<B>(finalized_number, self.session_period);
                 break;
