eaaef920088b14b7c564bb001d193710ad2d5249
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-07-11 17:44:33 +0200

    nix-build.sh - manually downloading cargo dependencies (using nix-shell) - it removes issues related with the naersk's vendored CARGO_HOME - using `sandbox` mode for nix-build - copying binaries to result folder (by default it was only a link)

--- nix/nix-build.sh
+++ nix/nix-build.sh
@@ -32,14 +32,30 @@ then
     nix-shell --pure $SHELL_NIX_FILE
 else
     ARGS=(--arg crates "${CRATES}" --arg singleStep "${SINGLE_STEP}" --arg rustflags "${RUSTFLAGS}")
-    nix-build --max-jobs auto $SHELL_NIX_FILE "${ARGS[@]}"
+
+    # first we download all dependencies
+    echo fetching depedencies...
+    CARGO_HOME="$(realpath ~/cargo-home)"
+    nix-shell --pure --run "CARGO_HOME=$CARGO_HOME cargo fetch --locked --offline || CARGO_HOME=$CARGO_HOME cargo fetch --locked"
+
+    echo building...
+    nix-build --max-jobs auto --option sandbox true --arg cargoHomePath "$CARGO_HOME" --show-trace $SHELL_NIX_FILE "${ARGS[@]}"
+    echo build finished
+
+    echo copying results...
+    cp -Lr result result.copied
+    mv result result.tmp
+    mv result.copied result
+    rm result.tmp
+    echo results copied
+
     # we need to change the dynamic linker
     # otherwise our binary references one that is specific for nix
     # we need it for aleph-node to be run outside nix-shell
-    if [ ! -z "$PATH_TO_FIX" ]; then
-        cp $PATH_TO_FIX ./
-        FILENAME=$(basename $PATH_TO_FIX)
-        chmod +w $FILENAME
-        patchelf --set-interpreter $DYNAMIC_LINKER_PATH $FILENAME
+    if [[ ! -z "$PATH_TO_FIX" && -f $PATH_TO_FIX ]]; then
+        echo patching...
+        chmod +w $PATH_TO_FIX
+        patchelf --set-interpreter $DYNAMIC_LINKER_PATH $PATH_TO_FIX
     fi
+    echo nix-build finished
 fi
