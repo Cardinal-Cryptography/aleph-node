2ac8bbacbfccbda3b73e6051752043f175b39639
Krzysztof Ziobro <krzysztof.ziobro@cardinals.cc>
2022-07-11 16:53:58 +0200

    Formatting

--- e2e-tests/src/cases.rs
+++ e2e-tests/src/cases.rs
@@ -1,12 +1,14 @@
 use crate::{
     config::Config,
     test::{
-        batch_transactions as test_batch_transactions, change_validators as test_change_validators,
+        batch_transactions as test_batch_transactions,
+        change_stake_and_force_new_era as test_change_stake_and_force_new_era,
+        change_validators as test_change_validators,
         channeling_fee_and_tip as test_channeling_fee_and_tip, disable_node as test_disable_node,
         era_payouts_calculated_correctly as test_era_payout, era_validators as test_era_validators,
         fee_calculation as test_fee_calculation, finalization as test_finalization,
         force_new_era as test_force_new_era, points_stake_change as test_points_stake_change,
-        staking_era_payouts as test_staking_era_payouts, change_stake_and_force_new_era as test_change_stake_and_force_new_era,
+        staking_era_payouts as test_staking_era_payouts,
         staking_new_validator as test_staking_new_validator, token_transfer as test_token_transfer,
         treasury_access as test_treasury_access, validators_rotate as test_validators_rotate,
     },
@@ -21,7 +23,6 @@ pub type PossibleTestCases = Vec<(&'static str, TestCase)>;
 /// This comes up in local tests.
 pub fn possible_test_cases() -> PossibleTestCases {
     vec![
-        ("rewards_change_stake_and_force_new_era", test_change_stake_and_force_new_era as TestCase),
         ("finalization", test_finalization as TestCase),
         ("rewards_disable_node", test_disable_node as TestCase),
         ("token_transfer", test_token_transfer as TestCase),
@@ -41,6 +42,10 @@ pub fn possible_test_cases() -> PossibleTestCases {
         ("fee_calculation", test_fee_calculation as TestCase),
         ("era_payout", test_era_payout as TestCase),
         ("era_validators", test_era_validators as TestCase),
+        (
+            "rewards_change_stake_and_force_new_era",
+            test_change_stake_and_force_new_era as TestCase,
+        ),
         ("rewards_force_new_era", test_force_new_era as TestCase),
         ("rewards_stake_change", test_points_stake_change as TestCase),
     ]
