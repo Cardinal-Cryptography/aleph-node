849ee274a89ab05e2f1d794361e9591cf27d784e
kostekIV <27210860+kostekIV@users.noreply.github.com>
2022-07-11 09:21:27 +0200

    fix change_validators call (#515) (#516)

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -112,7 +112,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("aleph-node"),
     impl_name: create_runtime_str!("aleph-node"),
     authoring_version: 1,
-    spec_version: 20,
+    spec_version: 21,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 8,
