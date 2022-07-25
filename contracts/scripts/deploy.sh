6e197178d06ed91877f2ab225affea3e755f97ad
filip <fbielejec@gmail.com>
2022-07-25 13:26:13 +0200

    double quote path var

--- contracts/scripts/deploy.sh
+++ contracts/scripts/deploy.sh
@@ -12,7 +12,7 @@ function instrument_game_token {
 
   # --- CREATE AN INSTANCE OF THE TOKEN CONTRACT
 
-  cd $CONTRACTS_PATH/$contract_name
+  cd "$CONTRACTS_PATH"/$contract_name
 
   local contract_address=$(cargo contract instantiate --url $NODE --constructor new --args $TOTAL_BALANCE --suri $AUTHORITY_SEED --salt $salt)
   local contract_address=$(echo "$contract_address" | grep Contract | tail -1 | cut -c 15-)
@@ -21,7 +21,7 @@ function instrument_game_token {
 
   # --- GRANT PRIVILEDGES ON THE TOKEN CONTRACT
 
-  cd $CONTRACTS_PATH/access_control
+  cd "$CONTRACTS_PATH"/access_control
 
   # set the admin and the owner of the contract instance
   cargo contract call --url $NODE --contract $ACCESS_CONTROL --message grant_role --args $AUTHORITY 'Admin('$contract_address')' --suri $AUTHORITY_SEED
@@ -38,7 +38,7 @@ function deploy_and_instrument_game {
 
   # --- UPLOAD CONTRACT CODE
 
-  cd $CONTRACTS_PATH/$contract_name
+  cd "$CONTRACTS_PATH"/$contract_name
   link_bytecode $contract_name 4465614444656144446561444465614444656144446561444465614444656144 $ACCESS_CONTROL_PUBKEY
   rm target/ink/$contract_name.wasm
   node ../scripts/hex-to-wasm.js target/ink/$contract_name.contract target/ink/$contract_name.wasm
@@ -48,13 +48,13 @@ function deploy_and_instrument_game {
 
   # --- GRANT INIT PRIVILEDGES ON THE CONTRACT CODE
 
-  cd $CONTRACTS_PATH/access_control
+  cd "$CONTRACTS_PATH"/access_control
 
   cargo contract call --url $NODE --contract $ACCESS_CONTROL --message grant_role --args $AUTHORITY 'Initializer('$code_hash')' --suri $AUTHORITY_SEED
 
   # --- CREATE AN INSTANCE OF THE CONTRACT
 
-  cd $CONTRACTS_PATH/$contract_name
+  cd "$CONTRACTS_PATH"/$contract_name
 
   local contract_address=$(cargo contract instantiate --url $NODE --constructor new --args $game_token $LIFETIME --suri $AUTHORITY_SEED)
   local contract_address=$(echo "$contract_address" | grep Contract | tail -1 | cut -c 15-)
@@ -70,13 +70,13 @@ function deploy_and_instrument_game {
 
   # --- TRANSFER TOKENS TO THE CONTRACT
 
-  cd $CONTRACTS_PATH/button_token
+  cd "$CONTRACTS_PATH"/button_token
 
   cargo contract call --url $NODE --contract $game_token --message transfer --args $contract_address $GAME_BALANCE --suri $AUTHORITY_SEED
 
   # --- WHITELIST ACCOUNTS FOR PLAYING
 
-  cd $CONTRACTS_PATH/$contract_name
+  cd "$CONTRACTS_PATH"/$contract_name
 
   cargo contract call --url $NODE --contract $contract_address --message IButtonGame::bulk_allow --args $WHITELIST --suri $AUTHORITY_SEED
 
@@ -99,25 +99,25 @@ NODE_IMAGE=public.ecr.aws/p6e8q1z1/aleph-node:latest
 TOTAL_BALANCE=1000
 GAME_BALANCE=$(echo "0.8 * $TOTAL_BALANCE" | bc)
 
-CONTRACTS_PATH=$(pwd)/contracts
+"CONTRACTS_PATH"=$(pwd)/contracts
 
 # --- COMPILE CONTRACTS
 
-cd $CONTRACTS_PATH/access_control
+cd "$CONTRACTS_PATH"/access_control
 cargo contract build --release
 
-cd $CONTRACTS_PATH/button_token
+cd "$CONTRACTS_PATH"/button_token
 cargo contract build --release
 
-cd $CONTRACTS_PATH/early_bird_special
+cd "$CONTRACTS_PATH"/early_bird_special
 cargo contract build --release
 
-cd $CONTRACTS_PATH/back_to_the_future
+cd "$CONTRACTS_PATH"/back_to_the_future
 cargo contract build --release
 
 # --- DEPLOY ACCESS CONTROL CONTRACT
 
-cd $CONTRACTS_PATH/access_control
+cd "$CONTRACTS_PATH"/access_control
 
 CONTRACT=$(cargo contract instantiate --url $NODE --constructor new --suri $AUTHORITY_SEED)
 ACCESS_CONTROL=$(echo "$CONTRACT" | grep Contract | tail -1 | cut -c 15-)
@@ -128,7 +128,7 @@ echo "access control contract public key (hex): " $ACCESS_CONTROL_PUBKEY
 
 # --- UPLOAD TOKEN CONTRACT CODE
 
-cd $CONTRACTS_PATH/button_token
+cd "$CONTRACTS_PATH"/button_token
 # replace address placeholder with the on-chain address of the AccessControl contract
 link_bytecode button_token 4465614444656144446561444465614444656144446561444465614444656144 $ACCESS_CONTROL_PUBKEY
 # remove just in case
@@ -144,7 +144,7 @@ echo "button token code hash" $BUTTON_TOKEN_CODE_HASH
 
 # --- GRANT INIT PRIVILEDGES ON THE TOKEN CONTRACT CODE
 
-cd $CONTRACTS_PATH/access_control
+cd "$CONTRACTS_PATH"/access_control
 
 # set the initializer of the button-token contract
 cargo contract call --url $NODE --contract $ACCESS_CONTROL --message grant_role --args $AUTHORITY 'Initializer('$BUTTON_TOKEN_CODE_HASH')' --suri $AUTHORITY_SEED
@@ -176,7 +176,7 @@ instrument_game_token BACK_TO_THE_FUTURE_TOKEN button_token 0x4261636B546F546865
 deploy_and_instrument_game BACK_TO_THE_FUTURE back_to_the_future $BACK_TO_THE_FUTURE_TOKEN
 
 # spit adresses to a JSON file
-cd $CONTRACTS_PATH
+cd "$CONTRACTS_PATH"
 
 jq -n --arg early_bird_special $EARLY_BIRD_SPECIAL \
    --arg back_to_the_future $BACK_TO_THE_FUTURE \
