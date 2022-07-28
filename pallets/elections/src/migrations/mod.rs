679a0b58409f027bf06c4bb81c2a00c9a243acc3
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-07-28 10:33:00 +0200

    fine grained checks

--- pallets/elections/src/migrations/mod.rs
+++ pallets/elections/src/migrations/mod.rs
@@ -1,3 +1,20 @@
+use frame_support::{traits::OnRuntimeUpgrade, weights::Weight};
+
 pub mod v0_to_v1;
 pub mod v1_to_v2;
 pub mod v2_to_v3;
+
+pub trait StorageMigration: OnRuntimeUpgrade {
+    #[allow(clippy::let_and_return)]
+    fn migrate() -> Weight {
+        #[cfg(feature = "try-runtime")]
+        Self::pre_upgrade().expect("Pre upgrade should succeed");
+
+        let weight = Self::on_runtime_upgrade();
+
+        #[cfg(feature = "try-runtime")]
+        Self::post_upgrade().expect("Post upgrade should succeed");
+
+        weight
+    }
+}
