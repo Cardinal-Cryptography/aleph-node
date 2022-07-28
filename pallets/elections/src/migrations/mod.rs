0341708964407b617ae5a3df1a82052b5162a403
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-07-28 11:34:59 +0200

    docs

--- pallets/elections/src/migrations/mod.rs
+++ pallets/elections/src/migrations/mod.rs
@@ -4,6 +4,10 @@ pub mod v0_to_v1;
 pub mod v1_to_v2;
 pub mod v2_to_v3;
 
+/// In order to run both pre- and post- checks around every migration, we entangle methods of
+/// `OnRuntimeUpgrade` into the desired flow and expose it with `migrate` method.
+///
+/// This way, `try-runtime` no longer triggers checks. We do it by hand.
 pub trait StorageMigration: OnRuntimeUpgrade {
     #[allow(clippy::let_and_return)]
     fn migrate() -> Weight {
