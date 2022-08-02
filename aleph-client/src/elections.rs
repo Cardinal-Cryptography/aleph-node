e8cebd7211bd4fbabdef433b5f800ccd83de54bd
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-08-02 19:16:06 +0200

    added missing file: aleph-client/elections.rs

---
+++ aleph-client/src/elections.rs
@@ -0,0 +1,50 @@
+use primitives::SessionIndex;
+pub use primitives::{CommitteeSeats, EraValidators};
+use sp_core::H256;
+use substrate_api_client::AccountId;
+
+use crate::{get_block_hash, get_session_period, AnyConnection};
+
+pub fn get_committee_seats<C: AnyConnection>(
+    connection: &C,
+    block_hash: Option<H256>,
+) -> CommitteeSeats {
+    connection
+        .as_connection()
+        .get_storage_value("Elections", "CommitteeSize", block_hash)
+        .expect("Failed to decode CommitteeSize extrinsic!")
+        .unwrap_or_else(|| {
+            panic!(
+                "Failed to obtain CommitteeSize for block hash: {:?}.",
+                block_hash
+            )
+        })
+}
+
+pub fn get_validator_block_count<C: AnyConnection>(
+    connection: &C,
+    account_id: &AccountId,
+    block_hash: Option<H256>,
+) -> Option<u32> {
+    connection
+        .as_connection()
+        .get_storage_map(
+            "Elections",
+            "SessionValidatorBlockCount",
+            account_id,
+            block_hash,
+        )
+        .expect("Failed to decode SessionValidatorBlockCount extrinsic!")
+}
+
+pub fn get_era_validators<C: AnyConnection>(
+    connection: &C,
+    session_index: SessionIndex,
+) -> EraValidators<AccountId> {
+    let session_period = get_session_period(connection);
+    let block_number = session_period * session_index;
+    let block_hash = get_block_hash(connection, block_number);
+    connection
+        .read_storage_value_from_block("Elections", "CurrentEraValidators", Some(block_hash))
+        .expect("Elections/CurrentEraValidators should be set to some value")
+}
