9b55ff89650acf8b53e1d2a88fb308b2d09aad22
Maciej Żelaszczyk <maciek.zelaszczyk@gmail.com>
2022-08-03 15:06:54 +0200

    Added description for docs.

--- e2e-tests/src/connection.rs
+++ e2e-tests/src/connection.rs
@@ -2,6 +2,7 @@ use aleph_client::SignedConnection;
 
 use crate::{accounts::get_validators_keys, Config};
 
+/// Get a `SignedConnection` where the signer is the first validator.
 pub fn get_signed_connection(config: &Config) -> SignedConnection {
     let node = &config.node;
     let accounts = get_validators_keys(config);
