bbf36f2f220098e671235d730eb1932b0934b100
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-08-19 10:46:12 +0200

    new version of task.rs - caching result in stopped()

--- finality-aleph/src/party/manager/task.rs
+++ finality-aleph/src/party/manager/task.rs
@@ -12,26 +12,43 @@ pub type Handle = Pin<Box<(dyn Future<Output = sc_service::Result<(), ()>> + Sen
 pub struct Task {
     handle: Handle,
     exit: oneshot::Sender<()>,
+    error_on_exit: Option<bool>,
 }
 
 impl Task {
     /// Create a new task.
     pub fn new(handle: Handle, exit: oneshot::Sender<()>) -> Self {
-        Task { handle, exit }
-    }
-
-    /// Cleanly stop the task.
-    pub async fn stop(self) {
-        if let Err(e) = self.exit.send(()) {
-            warn!(target: "aleph-party", "Failed to send exit signal to authority: {:?}", e);
-        } else {
-            let _ = self.handle.await;
+        Task {
+            handle,
+            exit,
+            error_on_exit: None,
         }
     }
 
+    /// Cleanly stop the task.
+    pub async fn stop(self) -> Result<(), ()> {
+        if let Some(res) = self.error_on_exit {
+            return if res { Err(()) } else { Ok(()) };
+        }
+        if let Err(e) = self.exit.send(()) {
+            warn!(target: "aleph-party", "Failed to send exit signal to authority: {:?}", e);
+            return if let Some(true) = self.error_on_exit {
+                Err(())
+            } else {
+                Ok(())
+            };
+        }
+        self.handle.await
+    }
+
     /// Await the task to stop by itself. Should usually just block forever, unless something went
-    /// wrong.
-    pub async fn stopped(&mut self) {
-        let _ = (&mut self.handle).await;
+    /// wrong. Can be called multiple times.
+    pub async fn stopped(&mut self) -> Result<(), ()> {
+        if let Some(res) = self.error_on_exit {
+            return if res { Err(()) } else { Ok(()) };
+        }
+        let result = (&mut self.handle).await;
+        self.error_on_exit = Some(result.is_err());
+        result
     }
 }
