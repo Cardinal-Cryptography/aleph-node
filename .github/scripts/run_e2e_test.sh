7bb89c041bfccdb651d3d6584c5b6bf9b3aa3c86
Maciej Żelaszczyk <maciek.zelaszczyk@gmail.com>
2022-08-25 12:55:49 +0200

    Changed env variable passing to docker

--- .github/scripts/run_e2e_test.sh
+++ .github/scripts/run_e2e_test.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 TEST_CASES=""
 RANDOMIZED="false"
 
-MIN_VALIDATOR_COUNT=""
+# This is required by the `Staking` pallet from Substrate.
+MIN_VALIDATOR_COUNT=4
 MAX_VALIDATOR_COUNT=""
 VALIDATOR_COUNT=""
 RESERVED_SEATS=""
@@ -33,25 +34,24 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-export TEST_CASES
-export RANDOMIZED
-
 if [[ "${RANDOMIZED}" == "true" ]]; then
-  # This is required by the `Staking` pallet from Substrate.
-  MIN_VALIDATOR_COUNT=4
-  # This is arbitrary.
-  MAX_VALIDATOR_COUNT=20
-
-  VALIDATOR_COUNT=$(shuf -i "${MIN_VALIDATOR_COUNT}"-"${MAX_VALIDATOR_COUNT}" -n 1)
-  # Assumes there is at least one reserved seat for validators.
-  RESERVED_SEATS=$(shuf -i 1-"${VALIDATOR_COUNT}" -n 1)
-  NON_RESERVED_SEATS=$((${VALIDATOR_COUNT} - ${RESERVED_SEATS}))
+  set_randomized_test_params
 fi
 
 # source docker/env
 
-docker run -v $(pwd)/docker/data:/data --network container:Node0 -e TEST_CASES -e MIN_VALIDATOR_COUNT \
-  -e RESERVED_SEATS -e NON_RESERVED_SEATS -e NODE_URL=127.0.0.1:9943 -e RUST_LOG=info aleph-e2e-client:latest
+docker run -v $(pwd)/docker/data:/data --network container:Node0 -e TEST_CASES="${TEST_CASES}" \
+  -e MIN_VALIDATOR_COUNT="${MIN_VALIDATOR_COUNT}" -e RESERVED_SEATS="${RESERVED_SEATS}" \
+  -e NON_RESERVED_SEATS="${NON_RESERVED_SEATS}" -e NODE_URL=127.0.0.1:9943 -e RUST_LOG=info aleph-e2e-client:latest
+
+function set_randomized_test_params() {
+  # This is arbitrary.
+  MAX_VALIDATOR_COUNT=20
+  VALIDATOR_COUNT=$(shuf -i "${MIN_VALIDATOR_COUNT}"-"${MAX_VALIDATOR_COUNT}" -n 1)
+  # Assumes there is at least one reserved seat for validators.
+  RESERVED_SEATS=$(shuf -i 1-"${VALIDATOR_COUNT}" -n 1)
+  NON_RESERVED_SEATS=$((${VALIDATOR_COUNT} - ${RESERVED_SEATS}))
+}
 
 function usage {
     cat << EOF
