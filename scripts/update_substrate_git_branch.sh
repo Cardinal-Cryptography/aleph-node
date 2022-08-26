ae11731021eba45e0a729f0c6763b65326384c29
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-08-26 08:53:40 +0200

    escape slashes

--- scripts/update_substrate_git_branch.sh
+++ scripts/update_substrate_git_branch.sh
@@ -28,7 +28,7 @@ for path in ${paths[@]}; do
     # 2. Find and capture whatever is after closing `"` and before `,` or `}`. It will be available as `\2`.
     # 3. Substitute new branch and concatenate it with `\1` and `\2`.
 
-    sed -e '/Cardinal-Cryptography\/substrate.git/s/\(branch\s*=\s*"\)[^"]*"\([^,}]*\)/\1'"${BRANCH}"'"\2/' < $path > x
+    sed -e '/Cardinal-Cryptography\/substrate.git/s/\(branch\s*=\s*"\)[^"]*"\([^,}]*\)/\1'"${BRANCH//\//\\/}"'"\2/' < $path > x
     mv x "${path}"
 done
 
