a94f28a5ad74e7ee2dc167cfd1c1f4d45d971ffa
Maciej Żelaszczyk <maciek.zelaszczyk@gmail.com>
2022-08-30 13:38:11 +0200

    Reworked scripts to use arrays

--- .github/scripts/run_e2e_test.sh
+++ .github/scripts/run_e2e_test.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 # This is required by Substrate: MinValidatorCount in pallet_Staking.
 MIN_VALIDATOR_COUNT=4
+# This is arbitrary.
+MAX_VALIDATOR_COUNT=20
 
 function set_randomized_test_params {
-  # This is arbitrary.
-  MAX_VALIDATOR_COUNT=20
   VALIDATOR_COUNT=$(shuf -i "${MIN_VALIDATOR_COUNT}"-"${MAX_VALIDATOR_COUNT}" -n 1)
   # Assumes there is at least one reserved seat for validators.
   RESERVED_SEATS=$(shuf -i 1-"${VALIDATOR_COUNT}" -n 1)
@@ -18,23 +18,18 @@ function usage {
     cat << EOF
 Usage:
   $0
-    --test-cases, -t
+    -t
       test cases to run
-    --randomized, -r
+    -r
       whether to randomize test case params, "true" and "false" values supported
       if randomization is performed, the `--reserved-seats` and `--non-reserved-seats` params are ignored
-    --reserved-seats, -f
+    -f
       number of reserved seats available to validators; ignored if empty or `--non-reserved-seats` is empty
-    --non-reserved-seats, -n
+    -n
       number of non-reserved seats available to validators; ignored if empty or `--reserved-seats` is empty
 EOF
   exit 0
 }
-
-TEST_CASES=""
-RANDOMIZED="false"
-RESERVED_SEATS=""
-NON_RESERVED_SEATS=""
 
 while getopts "h:t:r:f:n:" flag
 do
@@ -51,28 +46,40 @@ do
   esac
 done
 
-ARGS="--network container:Node0 -e NODE_URL=127.0.0.1:9943 -e RUST_LOG=info -e TEST_CASES="${TEST_CASES}""
+ARGS=(
+  --network "container:Node0"
+  -e NODE_URL="127.0.0.1:9943"
+  -e RUST_LOG=info
+)
+
+if [[ -n "${TEST_CASES:-}" ]]; then
+  ARGS+=(-e TEST_CASES="${TEST_CASES}")
+fi
+
+RANDOMIZED="${RANDOMIZED:-"false"}"
 
 # If randomization requested, generate random test params, ignore test params if provided.
-# Otherwise:
-#   a) in case of both non-empty params, pass them,
-#   b) in case either param is empty, do not pass them.
 if [[ "${RANDOMIZED}" == "true" ]]; then
   set_randomized_test_params
-  echo "Using randomized test case params: "${RESERVED_SEATS}" reserved and "${NON_RESERVED_SEATS}" non-reserved seats."
-  ARGS=""${ARGS}" -e RESERVED_SEATS="${RESERVED_SEATS}" -e NON_RESERVED_SEATS="${NON_RESERVED_SEATS}""
+  echo "Test case params have been randomized."
 elif [[ "${RANDOMIZED}" == "false" ]]; then
-  if [[ -n "${RESERVED_SEATS}" && -n "${NON_RESERVED_SEATS}" ]]; then
-    echo "Using provided test case params: "${RESERVED_SEATS}" reserved and "${NON_RESERVED_SEATS}" non-reserved seats."
-    ARGS=""${ARGS}" -e RESERVED_SEATS="${RESERVED_SEATS}" -e NON_RESERVED_SEATS="${NON_RESERVED_SEATS}""
-  else
-    echo "Falling back on default test case param values."
-  fi
+  echo "Test case params have not been randomized."
 else
   echo "Only 'true' and 'false' values supported, "${RANDOMIZED}" provided!"
   exit 1
 fi
 
-docker run -v $(pwd)/docker/data:/data ${ARGS} aleph-e2e-client:latest
+# If both test params are not empty, pass them. Otherwise, do not pass them.
+if [[ -n "${RESERVED_SEATS:-}" && -n "${NON_RESERVED_SEATS:-}" ]]; then
+  echo "Test case params: "${RESERVED_SEATS}" reserved and "${NON_RESERVED_SEATS}" non-reserved seats."
+  ARGS+=(
+    -e "${RESERVED_SEATS}"
+    -e "${NON_RESERVED_SEATS}"
+  )
+else
+  echo "Falling back on default test case param values."
+fi
+
+docker run -v $(pwd)/docker/data:/data "${ARGS[@]}" aleph-e2e-client:latest
 
 exit $?
--- e2e-tests/docker_entrypoint.sh
+++ e2e-tests/docker_entrypoint.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-RESERVED_SEATS=${RESERVED_SEATS:-}
-NON_RESERVED_SEATS=${NON_RESERVED_SEATS:-}
+ARGS=(
+  --node "${NODE_URL}"
+)
 
-ARGS="--node "${NODE_URL}" --test-cases "${TEST_CASES}""
-
-# If test case params are both not empty, run client with them. Otherwise, run without params.
-if [[ -n "${RESERVED_SEATS}" && -n "${NON_RESERVED_SEATS}" ]]; then
-  ARGS="${ARGS} --reserved-seats "${RESERVED_SEATS}" --non-reserved-seats "${NON_RESERVED_SEATS}""
+if [[ -n "${TEST_CASES:-}"]] ; then
+  ARGS+=(--test-cases "${TEST_CASES}")
 fi
 
-aleph-e2e-client "${ARGS}"
+# If test case params are both not empty, run client with them. Otherwise, run without params.
+if [[ -n "${RESERVED_SEATS:-}" && -n "${NON_RESERVED_SEATS:-}" ]]; then
+  ARGS+=(
+    --reserved-seats "${RESERVED_SEATS}"
+    --non-reserved-seats "${NON_RESERVED_SEATS}"
+  )
+fi
+
+aleph-e2e-client "${ARGS[@]}"
 
 echo "Done!"
