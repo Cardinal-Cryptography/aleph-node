3ed6fe6a690ff2e2e3eb71b58f7bd5dc05daba63
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-09-06 15:05:14 +0200

    using Perbill in aleph-client/system.rs

--- aleph-client/src/system.rs
+++ aleph-client/src/system.rs
@@ -1,4 +1,5 @@
 use sp_core::Pair;
+use sp_runtime::Perbill;
 use substrate_api_client::{compose_call, compose_extrinsic, ExtrinsicParams, XtStatus};
 
 use crate::{send_xt, try_send_xt, AnyConnection, CallSystem, RootConnection};
@@ -25,12 +26,12 @@ impl CallSystem for RootConnection {
 
     fn fill_block(&self, target_ratio_percent: u32, status: XtStatus) -> Result<(), Self::Error> {
         let connection = self.as_connection();
-        let target_ratio_perbill = target_ratio_percent * 10_000_000;
+        let target_ratio_perbill = Perbill::from_percent(target_ratio_percent);
         let call = compose_call!(
             connection.metadata,
             "System",
             "fill_block",
-            target_ratio_perbill
+            target_ratio_perbill.deconstruct()
         );
         let xt = compose_extrinsic!(connection, "Sudo", "sudo", call);
         try_send_xt(&connection, xt, Some("fill block"), status).map(|_| ())
