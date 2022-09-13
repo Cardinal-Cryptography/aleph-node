e0d9fb3ade5bae9f781841a2dc5de284c76a4b76
Maciej Żelaszczyk <maciek.zelaszczyk@gmail.com>
2022-09-13 12:00:38 +0200

    Changed behavior on session start and end

--- pallets/aleph/src/impls.rs
+++ pallets/aleph/src/impls.rs
@@ -5,16 +5,19 @@ where
     T: Config,
 {
     fn new_session(new_index: SessionIndex) -> Option<Vec<T::AccountId>> {
-        None
+        <T as Config>::SessionManager::new_session(new_index);
     }
 
     fn new_session_genesis(new_index: SessionIndex) -> Option<Vec<T::AccountId>> {
         Self::new_session()
     }
 
-    fn end_session(end_index: SessionIndex) { }
+    fn end_session(end_index: SessionIndex) {
+        <T as Config>::SessionManager::end_session(end_index);
+    }
 
     fn start_session(start_index: SessionIndex) {
+        <T as Config>::SessionManager::start_session(start_index);
         Self::update_version_change_history_and_schedule();
     }
 }
