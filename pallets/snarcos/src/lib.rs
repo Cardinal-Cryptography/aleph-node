d189a4e4ae743be6f92ef6625f38997d6d7d720c
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-09-14 15:38:02 +0200

    pallet ~ready

--- pallets/snarcos/src/lib.rs
+++ pallets/snarcos/src/lib.rs
@@ -14,6 +14,9 @@ pub type VerificationKeyIdentifier = [u8; 4];
 #[frame_support::pallet]
 pub mod pallet {
     use ark_ec::PairingEngine;
+    use ark_groth16::{Groth16, Proof, VerifyingKey};
+    use ark_serialize::CanonicalDeserialize;
+    use ark_snark::SNARK;
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::OriginFor;
     use sp_std::prelude::Vec;
@@ -31,14 +34,29 @@ pub mod pallet {
 
     #[pallet::error]
     pub enum Error<T> {
+        /// This verification key identifier is already taken.
         IdentifierAlreadyInUse,
+        /// There is no verification key available under this identifier.
+        UnknownVerificationKeyIdentifier,
+        /// Provided verification key is longer than `MaximumVerificationKeyLength` limit.
         VerificationKeyTooLong,
+        /// Couldn't deserialize proof.
+        DeserializingProofFailed,
+        /// Couldn't deserialize verification key from storage.
+        DeserializingVerificationKeyFailed,
+        /// Verification procedure has failed. Proof still can be correct.
+        VerificationFailed,
+        /// Proof has been found as incorrect.
+        IncorrectProof,
     }
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
+        /// Verification key has been successfully stored.
         VerificationKeyStored,
+        /// Proof has been successfully verified.
+        VerificationSucceeded,
     }
 
     #[pallet::pallet]
@@ -53,8 +71,6 @@ pub mod pallet {
         VerificationKeyIdentifier,
         BoundedVec<u8, T::MaximumVerificationKeyLength>,
     >;
-
-    impl<T: Config> Pallet<T> {}
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
@@ -79,6 +95,32 @@ pub mod pallet {
             );
 
             Self::deposit_event(Event::VerificationKeyStored);
+            Ok(())
+        }
+
+        #[pallet::weight(4141)]
+        pub fn verify(
+            _origin: OriginFor<T>,
+            verification_key_identifier: VerificationKeyIdentifier,
+            proof: Vec<u8>,
+            public_input: (),
+        ) -> DispatchResult {
+            let proof = Proof::<T::Field>::deserialize(&*proof)
+                .map_err(|_| Error::<T>::DeserializingProofFailed)?;
+
+            let verification_key = VerificationKeys::<T>::get(verification_key_identifier)
+                .ok_or(Error::<T>::UnknownVerificationKeyIdentifier)?;
+            let verification_key = VerifyingKey::<T::Field>::deserialize(&**verification_key)
+                .map_err(|_| Error::<T>::DeserializingVerificationKeyFailed)?;
+
+            let public_input = Vec::new();
+
+            let valid_proof = Groth16::verify(&verification_key, &public_input, &proof)
+                .map_err(|_| Error::<T>::VerificationFailed)?;
+
+            ensure!(valid_proof, Error::<T>::IncorrectProof);
+
+            Self::deposit_event(Event::VerificationSucceeded);
             Ok(())
         }
     }
