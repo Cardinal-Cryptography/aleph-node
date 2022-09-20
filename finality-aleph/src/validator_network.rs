dddbd54a46fbb47f17a78848a133d9f077f4c75d
Damian Leśniak <damian.lesniak@cardinals.cc>
2022-09-20 13:19:52 +0200

    fix3

--- finality-aleph/src/validator_network.rs
+++ finality-aleph/src/validator_network.rs
@@ -1,4 +1,5 @@
 use aleph_primitives::AuthorityId;
+use futures::future::pending;
 
 /// Network represents an interface for opening and closing connections with other Validators,
 /// and sending direct messages between them.
@@ -13,7 +14,7 @@ pub trait Network<A, D>: Send {
     fn add_connection(&mut self, peer: AuthorityId, addresses: Vec<A>);
 
     /// Remove the peer from the set of connected peers and close the connection.
-    fn del_connection(&mut self, peer: AuthorityId);
+    fn remove_connection(&mut self, peer: AuthorityId);
 
     /// Send a message to a single peer.
     /// Note on the implementation: this function should be implemented in a non-blocking manner.
@@ -29,9 +30,12 @@ pub struct MockNetwork;
 impl Network<(), ()> for MockNetwork {
     fn add_connection(&mut self, _peer: AuthorityId, _addresses: Vec<()>) {}
 
-    fn del_connection(&mut self, _peer: AuthorityId) {}
+    fn remove_connection(&mut self, _peer: AuthorityId) {}
 
     fn send(&self, _data: (), _recipient: AuthorityId) {}
 
-    async fn next(&mut self) {}
+    async fn next(&mut self) {
+        // MockNetwork never receives any messages
+        pending::<()>().await
+    }
 }
