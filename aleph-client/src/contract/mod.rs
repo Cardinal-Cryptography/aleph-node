3786d5d4f55eec0743a6bfba9b4e9d1c73481447
Paweł Obrok <pawel.obrok@cardinals.cc>
2022-10-14 18:08:35 +0200

    Add utils for contracts to aleph-client

---
+++ aleph-client/src/contract/mod.rs
@@ -0,0 +1,144 @@
+/// Utilities for writing contract wrappers.
+///
+/// The functions in this module simplify parsing the data types returned by contract calls.
+pub mod util;
+
+use std::{
+    fmt::{Debug, Formatter},
+    fs::File,
+};
+
+use ac_primitives::ExtrinsicParams;
+use anyhow::{anyhow, Context, Result};
+use contract_metadata::ContractMetadata;
+use contract_transcode::{ContractMessageTranscoder, Value};
+use ink_metadata::{InkProject, MetadataVersioned};
+use serde_json::{from_reader, from_str, from_value, json};
+use sp_core::{crypto::AccountId32, Pair};
+use substrate_api_client::{compose_extrinsic, GenericAddress, XtStatus};
+
+use crate::{try_send_xt, AnyConnection, Connection, SignedConnection};
+
+/// Represents a contract instantiated on the chain.
+pub struct ContractInstance {
+    address: AccountId32,
+    ink_project: InkProject,
+}
+
+impl ContractInstance {
+    const MAX_READ_GAS: u64 = 500000000000u64;
+    const MAX_GAS: u64 = 10000000000u64;
+
+    /// Creates a new contract instance under `address` with metadata read from `metadata_path`.
+    pub fn new(address: AccountId32, metadata_path: &str) -> Result<Self> {
+        Ok(Self {
+            address,
+            ink_project: load_metadata(metadata_path)?,
+        })
+    }
+
+    /// The address of this contract instance.
+    pub fn address(&self) -> &AccountId32 {
+        &self.address
+    }
+
+    /// The metadata of this contract instance.
+    pub fn ink_project(&self) -> &InkProject {
+        &self.ink_project
+    }
+
+    /// Reads the value of a read-only, 0-argument call via RPC.
+    pub fn contract_read0(&self, conn: &Connection, message: &str) -> Result<Value> {
+        self.contract_read(conn, message, &[])
+    }
+
+    /// Reads the value of a read-only call via RPC.
+    pub fn contract_read(&self, conn: &Connection, message: &str, args: &[&str]) -> Result<Value> {
+        let payload = self.encode(message, args)?;
+        let request = self.contract_read_request(&payload);
+        let response = conn
+            .get_request(request)?
+            .context("RPC request error - there may be more info in node logs.")?;
+        let response_data = from_str::<serde_json::Value>(&response)?;
+        let hex_data = response_data["result"]["Ok"]["data"]
+            .as_str()
+            .context("Contract response data not found - the contract address might be invalid.")?;
+        self.decode_response(message, hex_data)
+    }
+
+    /// Executes a contract call.
+    pub fn contract_exec(
+        &self,
+        conn: SignedConnection,
+        message: &str,
+        args: &[&str],
+    ) -> Result<()> {
+        let data = self.encode(message, args)?;
+        let xt = compose_extrinsic!(
+            conn.as_connection(),
+            "Contracts",
+            "call",
+            GenericAddress::Id(self.address.clone()),
+            Compact(0u64),
+            Compact(Self::MAX_GAS),
+            None::<Option<u128>>,
+            data
+        );
+
+        try_send_xt(&conn, xt, Some("Contracts call"), XtStatus::InBlock)
+            .context("Failed to exec contract message")?;
+        Ok(())
+    }
+
+    fn contract_read_request(&self, payload: &[u8]) -> serde_json::Value {
+        let payload = hex::encode(payload);
+        json!({
+            "jsonrpc": "2.0",
+            "method": "contracts_call",
+            "params": [{
+                "origin": self.address,
+                "dest": self.address,
+                "value": 0,
+                "gasLimit": Self::MAX_READ_GAS,
+                "inputData": payload
+            }],
+            "id": 1
+        })
+    }
+
+    fn encode(&self, message: &str, args: &[&str]) -> Result<Vec<u8>> {
+        ContractMessageTranscoder::new(&self.ink_project).encode(message, args)
+    }
+
+    fn decode_response(&self, from: &str, contract_response: &str) -> Result<Value> {
+        let contract_response = contract_response.replacen("0x", "", 1);
+        let bytes = hex::decode(contract_response)?;
+        ContractMessageTranscoder::new(&self.ink_project).decode_return(from, &mut bytes.as_slice())
+    }
+}
+
+impl Debug for ContractInstance {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ContractInstance")
+            .field("address", &self.address)
+            .field("ink_project", &self.ink_project)
+            .finish()
+    }
+}
+
+/// Helper for loading contract metadata from a file.
+///
+/// The contract-metadata lib contains a similar function starting with version 0.2. It seems that
+/// version conflicts with some of our other dependencies, however, if we upgrade in the future we
+/// can drop this function in favour of their implementation.
+fn load_metadata(path: &str) -> Result<InkProject> {
+    let file = File::open(path)?;
+    let metadata: ContractMetadata = from_reader(file)?;
+    let ink_metadata = from_value(serde_json::Value::Object(metadata.abi))?;
+
+    if let MetadataVersioned::V3(ink_project) = ink_metadata {
+        Ok(ink_project)
+    } else {
+        Err(anyhow!("Unsupported ink metadata version. Expected V3"))
+    }
+}
