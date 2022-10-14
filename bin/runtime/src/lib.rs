f9c8b59854449d8b3c6ccfc757fc3c3c0af04d37
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-10-14 10:28:35 +0200

    xD

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -721,7 +721,7 @@ impl pallet_contracts::Config for Runtime {
     type DepositPerByte = DepositPerByte;
     type WeightPrice = pallet_transaction_payment::Pallet<Self>;
     type WeightInfo = pallet_contracts::weights::SubstrateWeight<Self>;
-    type ChainExtension = ();
+    type ChainExtension = AlephChainExtension;
     type DeletionQueueDepth = DeletionQueueDepth;
     type DeletionWeightLimit = DeletionWeightLimit;
     type Schedule = Schedule;
