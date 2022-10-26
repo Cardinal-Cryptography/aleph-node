11750431b2edb2a8c1f385e69f37ac4e43cd95ef
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-10-26 10:41:02 +0200

    simplify caller and identifier

--- pallets/snarcos/src/benchmarking/suite.rs
+++ pallets/snarcos/src/benchmarking/suite.rs
@@ -5,44 +5,41 @@ use frame_system::RawOrigin;
 use crate::{ProvingSystem::*, *};
 
 const SEED: u32 = 41;
+const IDENTIFIER: VerificationKeyIdentifier = [0; 4];
+
+fn caller<T: Config>() -> RawOrigin<<T as frame_system::Config>::AccountId> {
+    RawOrigin::Signed(account("caller", 0, SEED))
+}
 
 benchmarks! {
 
     store_key {
-        let caller = account("caller", 0, SEED);
-        let identifier = [0u8; 4];
         let l in 1 .. T::MaximumVerificationKeyLength::get();
         let key = vec![0u8; l as usize];
-    } : _(RawOrigin::Signed(caller), identifier, key.clone())
+    } : _(caller::<T>(), IDENTIFIER, key)
 
     verify_xor {
-        let caller = account("caller", 0, SEED);
-
         let key = get_artifact!(Groth16, Xor, VerifyingKey);
         let proof = get_artifact!(Groth16, Xor, Proof);
         let input = get_artifact!(Groth16, Xor, PublicInput);
 
-        let identifier = [0u8; 4];
         let _ = VerificationKeys::<T>::insert(
-            identifier.clone(),
+            IDENTIFIER,
             BoundedVec::try_from(key).unwrap()
         );
 
-    } : verify(RawOrigin::Signed(caller), identifier, proof, input, Groth16)
+    } : verify(caller::<T>(), IDENTIFIER, proof, input, Groth16)
 
     verify_linear_equation {
-        let caller = account("caller", 0, SEED);
-
         let key = get_artifact!(Groth16, LinearEquation, VerifyingKey);
         let proof = get_artifact!(Groth16, LinearEquation, Proof);
         let input = get_artifact!(Groth16, LinearEquation, PublicInput);
 
-        let identifier = [0u8; 4];
         let _ = VerificationKeys::<T>::insert(
-            identifier.clone(),
+            IDENTIFIER,
             BoundedVec::try_from(key).unwrap()
         );
 
-    } : verify(RawOrigin::Signed(caller), identifier, proof, input, Groth16)
+    } : verify(caller::<T>(), IDENTIFIER, proof, input, Groth16)
 
 }
