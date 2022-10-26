cd7ddc09afa650c3b5d41ed7fd44ab330fe62359
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-10-26 09:47:06 +0200

    checkpoint

--- pallets/snarcos/src/benchmarking/mod.rs
+++ pallets/snarcos/src/benchmarking/mod.rs
@@ -1,25 +1,16 @@
+mod import;
 mod suite;
 
-fn xor_vk() -> &'static [u8] {
-    include_bytes!("resources/xor.vk.bytes")
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+enum Relation {
+    Xor,
+    LinearEquation,
+    MerkleTree,
 }
 
-fn xor_proof() -> &'static [u8] {
-    include_bytes!("resources/xor.proof.bytes")
-}
-
-fn xor_input() -> &'static [u8] {
-    include_bytes!("resources/xor.public_input.bytes")
-}
-
-fn linear_vk() -> &'static [u8] {
-    include_bytes!("resources/linear-equation.vk.bytes")
-}
-
-fn linear_proof() -> &'static [u8] {
-    include_bytes!("resources/linear-equation.proof.bytes")
-}
-
-fn linear_input() -> &'static [u8] {
-    include_bytes!("resources/linear-equation.public_input.bytes")
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+enum Artifact {
+    VerifyingKey,
+    Proof,
+    PublicInput,
 }
--- pallets/snarcos/src/benchmarking/suite.rs
+++ pallets/snarcos/src/benchmarking/suite.rs
@@ -2,10 +2,7 @@ use frame_benchmarking::{account, benchmarks, vec};
 use frame_support::{traits::Get, BoundedVec};
 use frame_system::RawOrigin;
 
-use crate::{
-    benchmarking::{linear_input, linear_proof, linear_vk, xor_input, xor_proof, xor_vk},
-    *,
-};
+use crate::{ProvingSystem::*, *};
 
 const SEED: u32 = 41;
 
@@ -21,9 +18,9 @@ benchmarks! {
     verify_xor {
         let caller = account("caller", 0, SEED);
 
-        let key = xor_vk().to_vec();
-        let proof = xor_proof().to_vec();
-        let input = xor_input().to_vec();
+        let key = get_artifact!(Groth16, Xor, VerifyingKey);
+        let proof = get_artifact!(Groth16, Xor, Proof);
+        let input = get_artifact!(Groth16, Xor, PublicInput);
 
         let identifier = [0u8; 4];
         let _ = VerificationKeys::<T>::insert(
@@ -31,14 +28,14 @@ benchmarks! {
             BoundedVec::try_from(key).unwrap()
         );
 
-    } : verify(RawOrigin::Signed(caller), identifier, proof, input, ProvingSystem::Groth16)
+    } : verify(RawOrigin::Signed(caller), identifier, proof, input, Groth16)
 
     verify_linear_equation {
         let caller = account("caller", 0, SEED);
 
-        let key = linear_vk().to_vec();
-        let proof = linear_proof().to_vec();
-        let input = linear_input().to_vec();
+        let key = get_artifact!(Groth16, LinearEquation, VerifyingKey);
+        let proof = get_artifact!(Groth16, LinearEquation, Proof);
+        let input = get_artifact!(Groth16, LinearEquation, PublicInput);
 
         let identifier = [0u8; 4];
         let _ = VerificationKeys::<T>::insert(
@@ -46,6 +43,6 @@ benchmarks! {
             BoundedVec::try_from(key).unwrap()
         );
 
-    } : verify(RawOrigin::Signed(caller), identifier, proof, input, ProvingSystem::Groth16)
+    } : verify(RawOrigin::Signed(caller), identifier, proof, input, Groth16)
 
 }
