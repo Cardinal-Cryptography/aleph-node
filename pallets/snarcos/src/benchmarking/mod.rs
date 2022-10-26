f019d75f78fa910c2506fccdb1a104e1bdbe8868
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-10-26 11:33:38 +0200

    50 repeats

--- pallets/snarcos/src/benchmarking/mod.rs
+++ pallets/snarcos/src/benchmarking/mod.rs
@@ -1,16 +1,2 @@
 mod import;
 mod suite;
-
-#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
-enum Relation {
-    Xor,
-    LinearEquation,
-    MerkleTree,
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
-enum Artifact {
-    VerifyingKey,
-    Proof,
-    PublicInput,
-}
