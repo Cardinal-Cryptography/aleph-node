80805692c82c9bd6b82dc7149b0977e0e1807eea
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-10-26 11:56:12 +0200

    docstrings

--- pallets/snarcos/src/benchmarking/import.rs
+++ pallets/snarcos/src/benchmarking/import.rs
@@ -6,9 +6,20 @@ pub(super) struct Artifacts {
     pub input: Vec<u8>,
 }
 
+/// Returns `Artifacts` object built from the resources described with `system` and `relation`
+/// arguments.
+///
+/// Example of usage:
+/// ```rust
+/// # use pallet_snarcos::get_artifacts;
+///
+/// let Artifacts { key, proof, input } = get_artifacts!(Groth16, LinearEquation);
+/// ```
+///
+///
 #[macro_export]
 macro_rules! get_artifacts {
-    ($(ProvingSystem::)?$system:tt, $(Relation::)?$relation:tt $(,)?) => {{
+    ($system:tt, $relation:tt $(,)?) => {{
         let key = $crate::get_artifact!($system, $relation, VerifyingKey);
         let proof = $crate::get_artifact!($system, $relation, Proof);
         let input = $crate::get_artifact!($system, $relation, PublicInput);
@@ -17,9 +28,15 @@ macro_rules! get_artifacts {
     }};
 }
 
+/// Returns a resource as `Vec<u8>`. The retrieval is done in compilation time
+/// (with `include_bytes!`).
+///
+/// Since `include_bytes!` accepts only string literals, in order to achieve some kind of brevity,
+/// we had to wrap it with light macros (see `system!`, `relation!` and `artifact!` defined in this
+/// module).
 #[macro_export]
 macro_rules! get_artifact {
-    ($(ProvingSystem::)?$system:tt, $(Relation::)?$relation:tt, $(Artifact::)?$artifact:tt $(,)?) => {
+    ($system:tt, $relation:tt, $artifact:tt $(,)?) => {
         include_bytes!(concat!(
             "resources/",
             $crate::system!($system),
@@ -33,6 +50,7 @@ macro_rules! get_artifact {
     };
 }
 
+/// Converts system identifier to a `&static str` that describes corresponding resources directory.
 #[macro_export]
 macro_rules! system {
     (Groth16) => {
@@ -43,6 +61,7 @@ macro_rules! system {
     };
 }
 
+/// Converts relation identifier to a `&static str` that is used in the filename pattern.
 #[macro_export]
 macro_rules! relation {
     (Xor) => {
@@ -51,11 +70,18 @@ macro_rules! relation {
     (LinearEquation) => {
         "linear_equation"
     };
-    (MerkleTree) => {
-        "merkle_tree"
+    (MerkleTree8) => {
+        "merkle_tree_8"
+    };
+    (MerkleTree64) => {
+        "merkle_tree_64"
+    };
+    (MerkleTree1024) => {
+        "merkle_tree_1024"
     };
 }
 
+/// Converts artifact identifier to a `&static str` that is used in the filename pattern.
 #[macro_export]
 macro_rules! artifact {
     (VerifyingKey) => {
