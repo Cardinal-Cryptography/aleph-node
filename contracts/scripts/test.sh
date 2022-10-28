f9d267873d14fe3a977b315a961f1a93de2751ed
Paweł Obrok <pawel.obrok@cardinals.cc>
2022-10-28 15:14:41 +0200

    Run contract e2e tests on CI

--- contracts/scripts/test.sh
+++ contracts/scripts/test.sh
@@ -2,77 +2,26 @@
 
 set -euo pipefail
 
-# --- FUNCTIONS
-
-function play {
-
-  local contract_name=$1
-  local contract_address=$(cat "$CONTRACTS_PATH"/addresses.json | jq --raw-output ".$contract_name")
-  local ticket_address=$(cat "$CONTRACTS_PATH"/addresses.json | jq --raw-output ".${contract_name}_ticket")
-
-  # airdrop initial tickets
-
-  cd "$CONTRACTS_PATH"/ticket_token
-
-  echo "sending ticket token" ${contract_name}_ticket "["$ticket_address"]" "to " $PLAYER1
-
-  cargo contract call --url $NODE --contract $ticket_address --message PSP22::transfer --args $PLAYER1 1 "[0]" --suri $AUTHORITY_SEED --skip-confirm
-
-  echo "sending ticket token" ${contract_name}_ticket "["$ticket_address"]" "to " $PLAYER2
-
-  cargo contract call --url $NODE --contract $ticket_address --message PSP22::transfer --args $PLAYER2 1 "[0]" --suri $AUTHORITY_SEED --skip-confirm
-
-  # give allowance for spending tickets to the game contract
-
-  echo "allowing" $contract_name "["$contract_address"]" "to spend up to" $TICKET_BALANCE "of" ${contract_name}_ticket "["$ticket_address"]" "on behalf of" $PLAYER1
-
-  cargo contract call --url $NODE --contract $ticket_address --message PSP22::approve --args $contract_address $TICKET_BALANCE --suri $PLAYER1_SEED --skip-confirm
-
-  echo "allowing" $contract_name "["$contract_address"]" "to spend up to" $TICKET_BALANCE "of" ${contract_name}_ticket "["$ticket_address"]" "on behalf of" $PLAYER2
-
-  cargo contract call --url $NODE --contract $ticket_address --message PSP22::approve --args $contract_address $TICKET_BALANCE --suri $PLAYER2_SEED --skip-confirm
-
-# TODO: uncomment when cargo contract doesn't break on parsing "foreign" events
-#
-#  # play the game
-#
-#  cd "$CONTRACTS_PATH"/button
-#
-#  echo "calling press for" $contract_name "["$contract_address"]" "by" $PLAYER1_SEED
-#
-#  cargo contract call --url $NODE --contract $contract_address --message press --suri $PLAYER1_SEED --skip-confirm
-#
-#  sleep 1
-#
-#  echo "calling press for" $contract_name "["$contract_address "]" "by" $PLAYER2_SEED
-#
-#  cargo contract call --url $NODE --contract $contract_address --message press --suri $PLAYER2_SEED --skip-confirm
-#
-#  # ---  WAIT FOR THE BUTTON DEATH
-#
-#  sleep $(($LIFETIME + 1))
-#
-#  # --- TRIGGER GAME RESET
-#
-#  cargo contract call --url $NODE --contract $contract_address --message reset --suri $AUTHORITY_SEED --skip-confirm
-#
-#  echo "Done playing" $contract_name
-}
-
-# --- ARGUMENTS
-
+E2E_PATH=$(pwd)/e2e-tests
 CONTRACTS_PATH=$(pwd)/contracts
+EARLY_BIRD_SPECIAL=$(jq --raw-output ".early_bird_special" < "$CONTRACTS_PATH"/addresses.json)
+THE_PRESSIAH_COMETH=$(jq --raw-output ".the_pressiah_cometh" < "$CONTRACTS_PATH"/addresses.json)
+BACK_TO_THE_FUTURE=$(jq --raw-output ".back_to_the_future" < "$CONTRACTS_PATH"/addresses.json)
 
-PLAYER1=5D34dL5prEUaGNQtPPZ3yN5Y6BnkfXunKXXz6fo7ZJbLwRRH
-PLAYER1_SEED=//0
-PLAYER2=5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
-PLAYER2_SEED=//Alice
+pushd "$E2E_PATH"
 
-GAMES=(early_bird_special back_to_the_future the_pressiah_cometh)
-for GAME in "${GAMES[@]}"; do
-  (
-    play $GAME
-  )&
-done
+RUST_LOG="aleph_e2e_client=info" cargo run --release -- \
+  --test-cases marketplace \
+  --test-cases button_game_reset \
+  --test-cases early_bird_special \
+  --test-cases the_pressiah_cometh \
+  --test-cases back_to_the_future \
+  --early-bird-special "$EARLY_BIRD_SPECIAL" \
+  --the-pressiah-cometh "$THE_PRESSIAH_COMETH" \
+  --back-to-the-future "$BACK_TO_THE_FUTURE" \
+  --button-game-metadata ../contracts/button/target/ink/metadata.json \
+  --ticket-token-metadata ../contracts/ticket_token/target/ink/metadata.json \
+  --reward-token-metadata ../contracts/game_token/target/ink/metadata.json \
+  --marketplace-metadata ../contracts/marketplace/target/ink/metadata.json
 
 exit $?
