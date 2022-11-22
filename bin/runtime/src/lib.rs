6488f2708275255261b9cd7024522b8a0561d927
Michal Swietek <michal.swietek@cardinals.cc>
2022-11-22 14:25:16 +0100

    bump spec version

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -108,7 +108,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("aleph-node"),
     impl_name: create_runtime_str!("aleph-node"),
     authoring_version: 1,
-    spec_version: 38,
+    spec_version: 40,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 13,
