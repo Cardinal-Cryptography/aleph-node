ef37cc8cfda1ba7fa4835e6394cf9ce024ce9336
Jan Koscisz <koscis.j@gmail.com>
2022-11-24 14:30:05 +0100

    fix

--- pallets/elections/src/migrations/v2_to_v3.rs
+++ pallets/elections/src/migrations/v2_to_v3.rs
@@ -9,6 +9,8 @@ use frame_support::{
 use pallets_support::ensure_storage_version;
 use pallets_support::StorageMigration;
 use primitives::CommitteeSeats;
+#[cfg(feature = "try-runtime")]
+use sp_std::vec::Vec;
 
 use crate::{migrations::Validators, Config, EraValidators};
 
