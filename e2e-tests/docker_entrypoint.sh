2800f359521decd79bb052bdf6b016820e48dd24
Maciej Żelaszczyk <maciek.zelaszczyk@gmail.com>
2022-11-28 14:03:00 +0100

    Init CI attempt

--- e2e-tests/docker_entrypoint.sh
+++ e2e-tests/docker_entrypoint.sh
@@ -11,7 +11,9 @@ fi
 
 # If test case params are both not empty, run client with them. Otherwise, run without params.
 if [[ -n "${RESERVED_SEATS:-}" && -n "${NON_RESERVED_SEATS:-}" ]]; then
+  VALIDATOR_COUNT = $((${RESERVED_SEATS} + ${NON_RESERVED_SEATS}))
   ARGS+=(
+    --validator-count "${VALIDATOR_COUNT}"
     --reserved-seats "${RESERVED_SEATS}"
     --non-reserved-seats "${NON_RESERVED_SEATS}"
   )
