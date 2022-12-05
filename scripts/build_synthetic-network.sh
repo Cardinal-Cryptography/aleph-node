511e1212c78015f78d1eb9f95d6ae55bbfd9de2d
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-12-05 17:56:29 +0100

    build script for aleph-node:synthetic-network

---
+++ scripts/build_synthetic-network.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+set -euo pipefail
+
+source ./scripts/common.sh
+
+GIT_COMMIT=${GIT_COMMIT:-72bbb4fde915e4132c19cd7ce3605364abac58a5}
+
+TMPDIR=$(mktemp -d --tmpdir=.)
+log "created a temporary folder at $TMPDIR"
+
+pushd .
+
+cd $TMPDIR
+git clone https://github.com/daily-co/synthetic-network.git
+cd synthetic-network
+git checkout $GIT_COMMIT
+sed -i 's/FROM node:12.20.2/FROM node:19.2/' Dockerfile
+
+log "building base docker image for synthetic-network with support for synthetic-network"
+docker build -t syntheticnet .
+
+popd
+
+log "building docker image for aleph-node with support for synthetic-network"
+docker build -t aleph-node:syntheticnet -f docker/Dockerfile.synthetic_network .
