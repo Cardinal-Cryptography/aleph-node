75abb858212810aeed9c22306158f473254008dd
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-12-05 22:16:01 +0100

    added script for running scripts for synthetic-network

---
+++ scripts/run_script_for_synthetic-network.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+
+set -euo pipefail
+
+source ./scripts/common.sh
+
+GIT_COMMIT=${GIT_COMMIT:-72bbb4fde915e4132c19cd7ce3605364abac58a5}
+SCRIPT_PATH=${SCRIPT_PATH:-scripts/synthetic-network.js}
+SCRIPT_PATH=$(realpath $SCRIPT_PATH)
+
+TMPDIR="$(dirname $0)/vendor"
+mkdir -p $TMPDIR
+log "created a temporary folder at $TMPDIR"
+
+log "cloning synthetic-network's git repo"
+cd $TMPDIR
+if [[ -f ./synthetic-network ]]; then
+    git clone https://github.com/daily-co/synthetic-network.git
+fi
+cd synthetic-network
+git fetch origin
+git checkout $GIT_COMMIT
+cd frontend
+
+log "running .js script"
+cp $SCRIPT_PATH ./
+node $(basename $SCRIPT_PATH)
+
+exit 0
