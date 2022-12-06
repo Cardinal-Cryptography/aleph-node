f4c7e866f652545c0cc01f715d943d39c31baeae
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2022-12-06 10:28:36 +0100

    refactored run_script_for_synthetic-network.sh: added help and args

--- scripts/run_script_for_synthetic-network.sh
+++ scripts/run_script_for_synthetic-network.sh
@@ -4,6 +4,40 @@ set -euo pipefail
 
 source ./scripts/common.sh
 
+function usage(){
+    cat << EOF
+Usage:
+  $0
+  This script allows you to run a custom .js script using the synthetic-network network simulation tool.
+    --commit 72bbb4fde915e4132c19cd7ce3605364abac58a5
+        commit hash used to build synthetic-network, default is 72bbb4fde915e4132c19cd7ce3605364abac58a5
+    --script-path scripts/vendor/synthetic-network/frontend/udp_rate_sine_demo.js
+        path to a synthetic-network scrypt. Default is a demo scripts/vendor/synthetic-network/frontend/udp_rate_sine_demo.js
+        from the synthetic-network repo. Please consult synthetic-network repo for details: https://github.com/daily-co/synthetic-network
+EOF
+    exit 0
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --commit)
+            GIT_COMMIT="$2"
+            shift;shift
+            ;;
+        --script-path)
+            SCRIPT_PATH="$2"
+            shift;shift
+            ;;
+        --help)
+            usage
+            shift
+            ;;
+        *)
+            error "Unrecognized argument $1!"
+            ;;
+    esac
+done
+
 GIT_COMMIT=${GIT_COMMIT:-72bbb4fde915e4132c19cd7ce3605364abac58a5}
 SCRIPT_PATH=${SCRIPT_PATH:-scripts/vendor/synthetic-network/frontend/udp_rate_sine_demo.js}
 SCRIPT_PATH=$(realpath $SCRIPT_PATH)
