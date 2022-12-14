eb3cd1c7c8bd34595a66df10d00cfe96e106e562
filip <fbielejec@gmail.com>
2022-12-14 10:12:44 +0100

    docstring

--- contracts/simple_dex/lib.rs
+++ contracts/simple_dex/lib.rs
@@ -322,6 +322,7 @@ mod simple_dex {
             Ok(())
         }
 
+        /// Returns true if a pair of tokens is whitelisted for swapping between
         #[ink(message)]
         pub fn can_swap_pair(&self, from: AccountId, to: AccountId) -> bool {
             self.swap_pairs.contains(&SwapPair::new(from, to))
