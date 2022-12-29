b67cded32380e52ddfce833b3cf4d88b3b9bda17
filip <fbielejec@gmail.com>
2022-12-29 12:19:08 +0100

    generate params

--- poseidon/build.rs
+++ poseidon/build.rs
@@ -1,6 +1,15 @@
+use ark_bls12_381::{Fq, FqParameters};
+use ark_ff::FpParameters;
+use poseidon_paramgen::poseidon_build;
+
 fn main() {
     let security_level = 128;
-    let t_values = vec![2, 3, 4, 5, 6, 7, 8];
+    // t = arity + 1, so t=2 is a 1:1 hash
+    // see https://spec.filecoin.io/#section-algorithms.crypto.poseidon.filecoins-poseidon-instances for similar specification used by Filecoin
+    let t_values = vec![2, 3];
 
-    println!("@@@ {}", security_level);
+    let params_codegen =
+        poseidon_build::compile::<Fq>(security_level, t_values, FqParameters::MODULUS, true);
+
+    println!("@@@ {}", params_codegen);
 }
