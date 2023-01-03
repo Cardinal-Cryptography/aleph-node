c75df59f56992cf3a2b794cea0dd355504ee995b
filip <fbielejec@gmail.com>
2023-01-03 12:40:19 +0100

    wip : 1:1 hash

--- poseidon/build.rs
+++ poseidon/build.rs
@@ -10,7 +10,7 @@ use poseidon_paramgen::poseidon_build;
 
 fn main() {
     let security_level = 128;
-    // t = arity + 1, so t=2 is a 1:1 hash
+    // t = arity + 1, so t=2 is a 1:1 hash, t=3 is a 2:1 hash etc
     // see https://spec.filecoin.io/#section-algorithms.crypto.poseidon.filecoins-poseidon-instances for similar specification used by Filecoin
     let t_values = vec![2, 3];
 
--- relations/src/preimage.rs
+++ relations/src/preimage.rs
@@ -1,9 +1,23 @@
 // This relation showcases how to use Poseidon in r1cs circuits
 use ark_bls12_381::Fq;
+use ark_r1cs_std::{alloc::AllocVar, fields::fp::FpVar};
+use ark_relations::{
+    ns,
+    r1cs::{
+        ConstraintSynthesizer, ConstraintSystemRef, SynthesisError,
+        SynthesisError::AssignmentMissing,
+    },
+};
 use ark_std::{marker::PhantomData, vec::Vec};
 use once_cell::sync::Lazy;
 
-use crate::relation::state::State;
+use crate::{
+    relation::state::{FullInput, NoInput, OnlyPublicInput, State, WithPublicInput},
+    CircuitField, GetPublicInput,
+};
+
+pub type CircuitVar = FpVar<CircuitField>;
+// pub type CircuitField = ark_bls12_381::Fr;
 
 // Poseidon paper suggests using domain separation for this, concretely encoding the use case in the capacity element (which is fine as it is 256 bits large and has a lot of bits to fill)
 static DOMAIN_SEP: Lazy<Fq> = Lazy::new(|| Fq::from(2137));
@@ -15,9 +29,60 @@ static DOMAIN_SEP: Lazy<Fq> = Lazy::new(|| Fq::from(2137));
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub struct PreimageRelation<S: State> {
     // private witness
-    pub preimage: Option<Fq>,
+    pub preimage: Option<CircuitField>,
     // public input
-    pub hash: Option<Fq>,
-
+    pub hash: Option<CircuitField>,
     _phantom: PhantomData<S>,
+}
+
+impl PreimageRelation<NoInput> {
+    pub fn without_input() -> Self {
+        Self {
+            hash: None,
+            preimage: None,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl PreimageRelation<OnlyPublicInput> {
+    pub fn with_public_input(hash: CircuitField) -> Self {
+        Self {
+            preimage: None,
+            hash: Some(hash),
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl PreimageRelation<FullInput> {
+    pub fn with_full_input(preimage: CircuitField, hash: CircuitField) -> Self {
+        Self {
+            preimage: Some(preimage),
+            hash: Some(hash),
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<S: State> ConstraintSynthesizer<CircuitField> for PreimageRelation<S> {
+    fn generate_constraints(
+        self,
+        cs: ConstraintSystemRef<CircuitField>,
+    ) -> Result<(), SynthesisError> {
+        let preimage = CircuitVar::new_witness(ns!(cs, "preimage"), || {
+            self.preimage.ok_or(AssignmentMissing)
+        })?;
+        let hash = CircuitVar::new_witness(ns!(cs, "hash"), || self.hash.ok_or(AssignmentMissing))?;
+
+        // let hash_result = poseidon::
+
+        Ok(())
+    }
+}
+
+impl<S: WithPublicInput> GetPublicInput<CircuitField> for PreimageRelation<S> {
+    fn public_input(&self) -> Vec<CircuitField> {
+        todo!()
+    }
 }
