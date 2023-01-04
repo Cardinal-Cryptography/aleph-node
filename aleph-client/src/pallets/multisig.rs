6fd46f2c7cb1e4ffbc64f5a7de2f8110649bc6b6
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-01-04 15:30:06 +0100

    Full basic support

--- aleph-client/src/pallets/multisig.rs
+++ aleph-client/src/pallets/multisig.rs
@@ -1,17 +1,32 @@
 use primitives::{Balance, BlockNumber};
 
 use crate::{
-    api, api::runtime_types, sp_weights::weight_v2::Weight, AccountId, BlockHash,
-    SignedConnectionApi, TxStatus,
+    aleph_runtime::RuntimeCall, api, api::runtime_types, sp_weights::weight_v2::Weight, AccountId,
+    BlockHash, SignedConnectionApi, TxStatus,
 };
 
 pub type CallHash = [u8; 32];
-pub type Call = Vec<u8>;
-pub type Timepoint = api::runtime_types::pallet_multisig::Timepoint<BlockNumber>;
+pub type Call = RuntimeCall;
+pub type Timepoint = runtime_types::pallet_multisig::Timepoint<BlockNumber>;
 pub type Multisig = runtime_types::pallet_multisig::Multisig<BlockNumber, Balance, AccountId>;
 
 #[async_trait::async_trait]
 pub trait MultisigUserApi {
+    async fn as_multi_threshold_1(
+        &self,
+        other_signatories: Vec<AccountId>,
+        call: Call,
+        status: TxStatus,
+    ) -> anyhow::Result<BlockHash>;
+    async fn as_multi(
+        &self,
+        threshold: u16,
+        other_signatories: Vec<AccountId>,
+        timepoint: Option<Timepoint>,
+        max_weight: Weight,
+        call: Call,
+        status: TxStatus,
+    ) -> anyhow::Result<BlockHash>;
     async fn approve_as_multi(
         &self,
         threshold: u16,
@@ -33,6 +48,39 @@ pub trait MultisigUserApi {
 
 #[async_trait::async_trait]
 impl<S: SignedConnectionApi> MultisigUserApi for S {
+    async fn as_multi_threshold_1(
+        &self,
+        other_signatories: Vec<AccountId>,
+        call: Call,
+        status: TxStatus,
+    ) -> anyhow::Result<BlockHash> {
+        let tx = api::tx()
+            .multisig()
+            .as_multi_threshold_1(other_signatories, call);
+
+        self.send_tx(tx, status).await
+    }
+
+    async fn as_multi(
+        &self,
+        threshold: u16,
+        other_signatories: Vec<AccountId>,
+        timepoint: Option<Timepoint>,
+        max_weight: Weight,
+        call: Call,
+        status: TxStatus,
+    ) -> anyhow::Result<BlockHash> {
+        let tx = api::tx().multisig().as_multi(
+            threshold,
+            other_signatories,
+            timepoint,
+            call,
+            max_weight,
+        );
+
+        self.send_tx(tx, status).await
+    }
+
     async fn approve_as_multi(
         &self,
         threshold: u16,
