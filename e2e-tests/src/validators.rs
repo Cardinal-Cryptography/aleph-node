ba56d3ba016fb1a1757502486843f50b3ae1e17c
Maciej Żelaszczyk <maciek.zelaszczyk@gmail.com>
2023-01-10 14:43:29 +0100

    Ip address and ports from config

--- e2e-tests/src/validators.rs
+++ e2e-tests/src/validators.rs
@@ -138,9 +138,15 @@ pub async fn prepare_validators<S: SignedConnectionApi + AuthorRpc>(
         }));
     }
 
+    let address_tokens = node.split(":").collect::<Vec<_>>();
+    let ip_address = address_tokens[0];
+    let first_port = address_tokens[1];
+
+    // Assumes the same ip address and consecutive ports for nodes, e.g. ws://127.0.0.1:9943,
+    // ws://127.0.0.1:9944, etc.
     for (port, controller) in accounts.controller_raw_keys.iter().enumerate() {
         let connection = SignedConnection::new(
-            format!("ws://127.0.0.1:{}", 9943 + port).as_str(),
+            format!("{}:{}", ip_address, first_port + port).as_str(),
             KeyPair::new(controller.clone()),
         )
         .await;
