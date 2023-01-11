2e610cd65976d706f1ecdc281823e6a5dc689b72
Marcin <marcin.radecki@cardinals.cc>
2023-01-11 07:59:15 +0100

    Test if runtime check works again

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -104,7 +104,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("aleph-node"),
     impl_name: create_runtime_str!("aleph-node"),
     authoring_version: 1,
-    spec_version: 45,
+    spec_version: 46,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 14,
