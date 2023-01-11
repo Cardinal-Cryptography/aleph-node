7c7069fd8a9cbaf179f9f872cf3e260a4b8d46a2
filip <fbielejec@gmail.com>
2023-01-11 15:27:25 +0100

    env var for security level M

--- poseidon/build.rs
+++ poseidon/build.rs
@@ -11,7 +11,16 @@ use ark_std::vec;
 use poseidon_paramgen::poseidon_build;
 
 fn main() {
-    let security_level = 128;
+    let security_level = match env::var("SECURITY_LEVEL") {
+        Ok(level) => match level.as_str() {
+            "80" => 80,
+            "128" => 128,
+            "256" => 256,
+            _ => panic!("Unsupported security level"),
+        },
+        Err(_) => 128,
+    };
+
     // t = arity + 1, so t=2 is a 1:1 hash, t=3 is a 2:1 hash etc
     // see https://spec.filecoin.io/#section-algorithms.crypto.poseidon.filecoins-poseidon-instances for similar specification used by Filecoin
     let t_values = vec![2];
@@ -20,9 +29,10 @@ fn main() {
     let parameters =
         poseidon_build::compile::<Fr>(security_level, t_values, FrParameters::MODULUS, true);
 
-    let output_directory =
-        PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR environmental variable must be set"))
-            .join("parameters.rs");
+    let output_directory = PathBuf::from(
+        env::var("OUT_DIR").expect("OUT_DIR environmental variable should be always set"),
+    )
+    .join("parameters.rs");
 
     let mut file =
         BufWriter::new(File::create(output_directory).expect("can't create source file"));
