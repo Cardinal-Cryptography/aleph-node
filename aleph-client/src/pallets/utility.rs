f0775a820c44f0c624a3299289bf1e755534f78d
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-01-13 07:43:40 +0100

    Aleph client migrated

--- aleph-client/src/pallets/utility.rs
+++ aleph-client/src/pallets/utility.rs
@@ -1,15 +1,15 @@
-use crate::{api, BlockHash, Call, SignedConnectionApi, TxStatus};
+use crate::{api, connections::TxCoords, Call, SignedConnectionApi, TxStatus};
 
 /// Pallet utility api.
 #[async_trait::async_trait]
 pub trait UtilityApi {
     /// API for [`batch`](https://paritytech.github.io/substrate/master/pallet_utility/pallet/struct.Pallet.html#method.batch) call.
-    async fn batch_call(&self, calls: Vec<Call>, status: TxStatus) -> anyhow::Result<BlockHash>;
+    async fn batch_call(&self, calls: Vec<Call>, status: TxStatus) -> anyhow::Result<TxCoords>;
 }
 
 #[async_trait::async_trait]
 impl<S: SignedConnectionApi> UtilityApi for S {
-    async fn batch_call(&self, calls: Vec<Call>, status: TxStatus) -> anyhow::Result<BlockHash> {
+    async fn batch_call(&self, calls: Vec<Call>, status: TxStatus) -> anyhow::Result<TxCoords> {
         let tx = api::tx().utility().batch(calls);
 
         self.send_tx(tx, status).await
