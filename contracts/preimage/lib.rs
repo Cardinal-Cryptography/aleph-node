50dc5dd4e23a444c6d5626b7f870197124806ec1
filip <fbielejec@gmail.com>
2023-01-18 15:43:05 +0100

    wip reveal commitment

--- contracts/preimage/lib.rs
+++ contracts/preimage/lib.rs
@@ -1,69 +1,67 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-#[ink::contract]
+#[ink::contract(env = snarcos_extension::DefaultEnvironment)]
 mod preimage {
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    use ink::{
+        codegen::EmitEvent,
+        prelude::{format, string::String, vec::Vec},
+        reflect::ContractEventBase,
+        storage::Mapping,
+        ToAccountId,
+    };
+    use snarcos_extension::VerificationKeyIdentifier;
+
+    const PREIMAGE_VK_IDENTIFIER: VerificationKeyIdentifier = [b'p', b'i', b'm', b'g'];
+
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum PreimageContractError {
+        AlreadyCommited,
+        NotCommited, // InkEnvError(String),
+    }
+
     #[ink(storage)]
     pub struct Preimage {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        commitments: Mapping<AccountId, Vec<u8>>,
     }
 
     impl Preimage {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            Self {
+                commitments: Mapping::default(),
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn commit(&mut self, hash: Vec<u8>) -> Result<(), PreimageContractError> {
+            let caller = Self::env().caller();
+
+            if self.commitments.contains(caller) {
+                return Err(PreimageContractError::AlreadyCommited);
+            }
+
+            self.commitments.insert(caller, &hash);
+            Ok(())
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
+        pub fn reveal(
+            &mut self,
+            hash: Vec<u8>,
+            proof: Vec<u8>,
+        ) -> Result<(), PreimageContractError> {
+            let caller = Self::env().caller();
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
+            if !self.commitments.contains(caller) {
+                return Err(PreimageContractError::NotCommited);
+            }
 
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let preimage = Preimage::default();
-            assert_eq!(preimage.get(), false);
-        }
+            // TODO : verify
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut preimage = Preimage::new(false);
-            assert_eq!(preimage.get(), false);
-            preimage.flip();
-            assert_eq!(preimage.get(), true);
+            self.commitments.remove(caller);
+            Ok(())
         }
     }
 }
