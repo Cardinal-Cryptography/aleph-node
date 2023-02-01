90b2d0efc153e6344e7eac32c9f525ac32067438
Marcin <marcin.radecki@cardinals.cc>
2023-02-01 10:41:38 +0100

    A0-1910 Added custom transaction payment migration

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -783,6 +783,7 @@ pub type Executive = frame_executive::Executive<
         >,
         pallet_staking::migrations::v12::MigrateToV12<Runtime>,
         pallet_multisig::migrations::v1::MigrateToV1<Runtime>,
+        migrations::custom_transaction_payment_migration::BumpTransactionVersionToV2<Runtime>,
     ),
 >;
 
