2194dac9a2387e2c1d40344db8f270d1a75a39a7
KostekIV <koscis.j@gmail.com>
2023-02-07 11:48:42 +0100

    history

--- bin/runtime/src/migrations/mod.rs
+++ bin/runtime/src/migrations/mod.rs
@@ -1,5 +1,7 @@
+mod history;
 mod scheduler;
 mod staking;
 
 pub use scheduler::MigrateToV3 as SchedulerMigrateToV3;
 pub use staking::BumpStorageVersionFromV7ToV10;
+pub use history::MigrateToV1 as HistoryMigrateToV1;
