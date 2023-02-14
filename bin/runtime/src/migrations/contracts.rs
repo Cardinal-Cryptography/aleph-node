e2efa6fafd41fb588dbb1e03b056d69f24a23fb4
Damian Straszak <damian.straszak@gmail.com>
2023-02-14 14:40:10 +0100

    Clean up

--- bin/runtime/src/migrations/contracts.rs
+++ bin/runtime/src/migrations/contracts.rs
@@ -1,21 +1,4 @@
-// This file is part of Substrate.
-
-// Copyright (C) 2018-2022 Parity Technologies (UK) Ltd.
-// SPDX-License-Identifier: Apache-2.0
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// 	http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-use codec::{Decode, DecodeAll, Encode, Error, Input};
+use codec::{Decode, DecodeAll, Encode, Error};
 use frame_support::{
     codec, log,
     pallet_prelude::*,
@@ -27,6 +10,8 @@ use frame_support::{
 use pallet_contracts::{Config, Pallet};
 use sp_runtime::traits::Saturating;
 use sp_std::{marker::PhantomData, prelude::*};
+
+use crate::sp_api_hidden_includes_construct_runtime::hidden_include::StoragePrefixedMap;
 
 type CodeHash<T> = <T as frame_system::Config>::Hash;
 type TrieId = BoundedVec<u8, ConstU32<128>>;
@@ -48,8 +33,15 @@ impl<T: Config> OnRuntimeUpgrade for Migration<T> {
             version
         );
         if version < 8 {
-            weight = weight.saturating_add(v8::migrate::<T>());
-            StorageVersion::new(8).put::<Pallet<T>>();
+            let (used_weight, finished) = v8::migrate::<T>();
+            weight = weight.saturating_add(used_weight);
+            if finished {
+                StorageVersion::new(8).put::<Pallet<T>>();
+                log::info!(
+                    target: TARGET,
+                    "All contracts migrated. Writing new StorageVersion.",
+                );
+            }
         }
 
         weight
@@ -90,11 +82,12 @@ impl<T: Config> OnRuntimeUpgrade for Migration<T> {
 
 /// Update `ContractInfo` with new fields that track storage deposits.
 mod v8 {
+    use frame_support::storage::unhashed;
     use sp_io::default_child_storage as child;
 
     use super::*;
 
-    #[derive(Decode)]
+    #[derive(Encode, Decode)]
     struct RawContractInfo<CodeHash, Balance> {
         pub trie_id: TrieId,
         pub code_hash: CodeHash,
@@ -116,45 +109,42 @@ mod v8 {
 
     const OLD_INFO_ENCODING_LEN: usize = 33 + 32 + 16;
     const NEW_INFO_ENCODING_LEN: usize = 33 + 32 + 4 + 4 + 16 + 16 + 16;
+    const MAX_NUM_ITEMS_PROCESSED_PER_MIGRATION: u32 = 140000;
 
     enum EitherContractInto<T: Config> {
         Old(OldContractInfo<T>),
         New(ContractInfo<T>),
     }
 
-    impl<T: Config> Decode for EitherContractInto<T> {
-        // This is a generally incorrect and bad implementation of decode. This works only because we know for sure
-        // that the only place where we use this decoding is when applying `translate` in the migration, where the
-        // value is decoded from a vector whose length we can predict
-        fn decode<I: Input>(input: &mut I) -> Result<Self, Error> {
-            let len = input.remaining_len()?.unwrap_or_default();
-            let mut buffer = vec![0; len];
-            input.read(&mut buffer)?;
-            match len {
-                OLD_INFO_ENCODING_LEN => {
-                    if let Ok(c_info) = OldContractInfo::<T>::decode_all(&mut buffer.as_slice()) {
-                        Ok(EitherContractInto::Old(c_info))
-                    } else {
-                        Err(Error::from("Failed to decode ContractInfo to Old type"))
-                    }
-                }
-                NEW_INFO_ENCODING_LEN => {
-                    if let Ok(c_info) = ContractInfo::<T>::decode_all(&mut buffer.as_slice()) {
-                        Ok(EitherContractInto::New(c_info))
-                    } else {
-                        Err(Error::from("Failed to decode ContractInfo to New type"))
-                    }
-                }
-                _ => Err(Error::from("Wrong size of ContractInfo")),
-            }
-        }
-    }
-
     #[storage_alias]
     type ContractInfoOf<T: Config, V> =
         StorageMap<Pallet<T>, Twox64Concat, <T as frame_system::Config>::AccountId, V>;
 
-    pub fn migrate<T: Config>() -> Weight {
+    fn decode_to_either_contract_info<T: Config>(
+        input: &mut &[u8],
+    ) -> Result<EitherContractInto<T>, Error> {
+        let len = input.len();
+        match len {
+            OLD_INFO_ENCODING_LEN => {
+                if let Ok(c_info) = OldContractInfo::<T>::decode_all(input) {
+                    Ok(EitherContractInto::Old(c_info))
+                } else {
+                    Err(Error::from("Failed to decode ContractInfo to Old type"))
+                }
+            }
+            NEW_INFO_ENCODING_LEN => {
+                if let Ok(c_info) = ContractInfo::<T>::decode_all(input) {
+                    Ok(EitherContractInto::New(c_info))
+                } else {
+                    Err(Error::from("Failed to decode ContractInfo to New type"))
+                }
+            }
+            _ => Err(Error::from("Wrong size of ContractInfo")),
+        }
+    }
+
+	// Returns the used weight and whether this migration is the final one.
+    pub fn migrate<T: Config>() -> (Weight, bool) {
         log::info!(
             target: TARGET,
             "Running v7->v8 migration of pallet contracts"
@@ -162,19 +152,34 @@ mod v8 {
         let mut weight = Weight::zero();
         let mut total_bytes: u32 = 0;
         let mut total_items: u32 = 0;
-        let mut total_contracts_migrated: u32 = 0;
-		let mut total_contracts_skipped: u32 = 0;
+        let mut num_old_contracts_migrated: u32 = 0;
+        let mut num_old_contracts_skipped: u32 = 0;
+        let mut num_new_contracts: u32 = 0;
+        let mut num_contracts_erred: u32 = 0;
 
-        <ContractInfoOf<T, ContractInfo<T>>>::translate_values(|either: EitherContractInto<T>| {
-            match either {
-                EitherContractInto::Old(old) => {
+        // Note that we use V = ContractInfo<T> and not V = OldContractInfo<T>, but that does not matter for the prefix.
+        let prefix = ContractInfoOf::<T, ContractInfo<T>>::final_prefix();
+        let mut previous_key = prefix.clone().to_vec();
+        // Iterate over all elements of the ContractInfoOf map
+        while let Some(next) =
+            sp_io::storage::next_key(&previous_key).filter(|n| n.starts_with(&prefix))
+        {
+            previous_key = next;
+            let value = sp_io::storage::get(&previous_key)
+                .expect("The loop conditions checks for existence of the key; qed");
+            match decode_to_either_contract_info::<T>(&mut &value[..]) {
+                Ok(EitherContractInto::Old(old)) => {
+                    if total_items >= MAX_NUM_ITEMS_PROCESSED_PER_MIGRATION {
+                        num_old_contracts_skipped.saturating_accrue(1);
+                        continue;
+                    }
                     let mut storage_bytes = 0u32;
                     let mut storage_items = 0u32;
-                    let mut key = Vec::new();
-                    while let Some(next) = child::next_key(&old.trie_id, &key) {
-                        key = next;
+                    let mut child_key = Vec::new();
+                    while let Some(next_child_key) = child::next_key(&old.trie_id, &child_key) {
+                        child_key = next_child_key;
                         let mut val_out = [];
-                        let len = child::read(&old.trie_id, &key, &mut val_out, 0)
+                        let len = child::read(&old.trie_id, &child_key, &mut val_out, 0)
                             .expect("The loop conditions checks for existence of the key; qed");
                         storage_bytes.saturating_accrue(len);
                         storage_items.saturating_accrue(1);
@@ -182,7 +187,6 @@ mod v8 {
 
                     total_bytes.saturating_accrue(storage_bytes);
                     total_items.saturating_accrue(storage_items);
-                    total_contracts_migrated.saturating_accrue(1);
 
                     let storage_byte_deposit =
                         T::DepositPerByte::get().saturating_mul(storage_bytes.into());
@@ -193,13 +197,7 @@ mod v8 {
                         .saturating_sub(storage_byte_deposit)
                         .saturating_sub(storage_item_deposit);
 
-                    // Reads: One read for each storage item plus the contract info itself.
-                    // Writes: Only the new contract info.
-                    weight = weight.saturating_add(
-                        T::DbWeight::get().reads_writes(u64::from(storage_items) + 1, 1),
-                    );
-
-                    Some(ContractInfo {
+                    let new_contract_info = ContractInfo {
                         trie_id: old.trie_id,
                         code_hash: old.code_hash,
                         storage_bytes,
@@ -207,23 +205,41 @@ mod v8 {
                         storage_byte_deposit,
                         storage_item_deposit,
                         storage_base_deposit,
-                    })
-                },
-                EitherContractInto::New(new) => {
-					total_contracts_skipped.saturating_accrue(1);
-					Some(new)
-				},
+                    };
+                    unhashed::put::<ContractInfo<T>>(&previous_key, &new_contract_info);
+
+                    weight = weight.saturating_add(
+                        T::DbWeight::get().reads_writes(u64::from(storage_items) + 1, 1),
+                    );
+
+                    num_old_contracts_migrated.saturating_accrue(1);
+                }
+                Ok(EitherContractInto::New(_)) => {
+                    weight = weight.saturating_add(T::DbWeight::get().reads_writes(1, 0));
+                    num_new_contracts.saturating_accrue(1);
+                }
+                Err(e) => {
+                    log::info!(
+                        target: TARGET,
+                        "Error {:?} when decoding ContractInfo at key {:?}",
+                        previous_key,
+                        e
+                    );
+                    num_contracts_erred.saturating_accrue(1);
+                }
             }
-        });
+        }
         log::info!(
             target: TARGET,
-            "Migration ended. Migrated {}, skipped {}. Stats: total_bytes {:?}, total_items {:?}",
-            total_contracts_migrated,
-			total_contracts_skipped,
+            "Migration ended. old migrated {}, old skipped {}, new skipped {}, err {}. Stats: total_bytes {:?}, total_items {:?}",
+			num_old_contracts_migrated,
+			num_old_contracts_skipped,
+			num_new_contracts,
+            num_contracts_erred,
             total_bytes,
             total_items
         );
-        weight
+        (weight, num_old_contracts_skipped == 0)
     }
 
     #[cfg(feature = "try-runtime")]
@@ -244,6 +260,7 @@ mod v8 {
                 );
             }
         }
+        Ok(())
     }
 
     #[cfg(feature = "try-runtime")]
@@ -264,7 +281,6 @@ mod v8 {
                     reserved
                 );
             }
-
             let mut storage_bytes = 0u32;
             let mut storage_items = 0u32;
             let mut key = Vec::new();
--- bin/runtime/src/migrations/mod.rs
+++ bin/runtime/src/migrations/mod.rs
@@ -1,3 +1,2 @@
-pub use pallet_contracts::Migration as SubstrateContractMigration;
 mod contracts;
 pub use contracts::Migration as CustomContractMigration;
