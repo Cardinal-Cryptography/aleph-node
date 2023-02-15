130896a619d04f9371fea2f916609afe52bc49f5
maciejnems <macieknems@gmail.com>
2023-02-15 13:43:14 +0100

    fix build

--- e2e-tests/src/test/fee.rs
+++ e2e-tests/src/test/fee.rs
@@ -1,15 +1,18 @@
-// use aleph_client::{
-//     api::transaction_payment::events::TransactionFeePaid,
-//     pallets::{balances::BalanceUserApi, fee::TransactionPaymentApi, system::SystemSudoApi},
-//     utility::BlocksApi,
-//     AccountId, RootConnection, SignedConnection, TxStatus,
-// };
-// use log::info;
-// use primitives::Balance;
+use aleph_client::{
+    api::transaction_payment::events::TransactionFeePaid,
+    pallets::{balances::BalanceUserApi, fee::TransactionPaymentApi},
+    utility::BlocksApi,
+    AccountId, SignedConnection, TxStatus,
+};
+use log::info;
+use primitives::Balance;
+
+// use aleph_client::{RootConnection, pallets::system::SystemSudoApi};
 // use sp_runtime::{FixedPointNumber, FixedU128};
 
 // use crate::{config::setup_test, transfer::setup_for_transfer};
 
+// This test is disabled, as for now we no longer have access to fill_block runtime call
 // #[tokio::test]
 // pub async fn fee_calculation() -> anyhow::Result<()> {
 //     let config = setup_test();
@@ -99,27 +102,27 @@
 //     }
 // }
 
-// pub async fn current_fees(
-//     connection: &SignedConnection,
-//     to: AccountId,
-//     tip: Option<Balance>,
-//     transfer_value: Balance,
-// ) -> (Balance, u128) {
-//     let actual_multiplier = connection.get_next_fee_multiplier(None).await;
+pub async fn current_fees(
+    connection: &SignedConnection,
+    to: AccountId,
+    tip: Option<Balance>,
+    transfer_value: Balance,
+) -> (Balance, u128) {
+    let actual_multiplier = connection.get_next_fee_multiplier(None).await;
 
-//     let tx_info = match tip {
-//         None => connection.transfer(to, transfer_value, TxStatus::Finalized),
-//         Some(tip) => connection.transfer_with_tip(to, transfer_value, tip, TxStatus::Finalized),
-//     }
-//     .await
-//     .unwrap();
+    let tx_info = match tip {
+        None => connection.transfer(to, transfer_value, TxStatus::Finalized),
+        Some(tip) => connection.transfer_with_tip(to, transfer_value, tip, TxStatus::Finalized),
+    }
+    .await
+    .unwrap();
 
-//     let events = connection.get_tx_events(tx_info).await.unwrap();
-//     let event = events.find_first::<TransactionFeePaid>().unwrap().unwrap();
+    let events = connection.get_tx_events(tx_info).await.unwrap();
+    let event = events.find_first::<TransactionFeePaid>().unwrap().unwrap();
 
-//     let fee = event.actual_fee;
+    let fee = event.actual_fee;
 
-//     info!("fee payed: {}", fee);
+    info!("fee payed: {}", fee);
 
-//     (fee, actual_multiplier)
-// }
+    (fee, actual_multiplier)
+}
