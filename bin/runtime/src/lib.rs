c43d9d4dd464098962aa024575501e00c0968765
Marcin <marcin.radecki@cardinals.cc>
2023-02-27 12:43:31 +0100

    Bumped transaction_version, spec_version, runtime crate version, node crate version

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -110,10 +110,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("aleph-node"),
     impl_name: create_runtime_str!("aleph-node"),
     authoring_version: 1,
-    spec_version: 55,
+    spec_version: 57,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 14,
+    transaction_version: 15,
     state_version: 0,
 };
 
