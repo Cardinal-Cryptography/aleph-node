e7565541f739640ebf7aa570b8a182268e54e720
Marcin <marcin.radecki@cardinals.cc>
2023-02-27 12:48:36 +0100

    Removed wrong merge conflict resolution

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -226,8 +226,6 @@ parameter_types! {
 
 impl pallet_authorship::Config for Runtime {
     type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Aura>;
-    type UncleGenerations = UncleGenerations;
-    type FilterUncle = ();
     type EventHandler = (Elections,);
 }
 
