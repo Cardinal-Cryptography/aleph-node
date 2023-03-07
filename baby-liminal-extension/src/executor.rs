b6cb8e181256d414155fe7160ffb0cf9f4f6588f
filip <fbielejec@gmail.com>
2023-03-07 13:31:37 +0100

    type bounds

--- baby-liminal-extension/src/executor.rs
+++ baby-liminal-extension/src/executor.rs
@@ -5,7 +5,7 @@ use obce::substrate::{
 };
 use pallet_baby_liminal::{Config as BabyLiminalConfig, Error, Pallet as BabyLiminal};
 
-use crate::{AccountId, ProvingSystem, Runtime, RuntimeOrigin, Vec, VerificationKeyIdentifier};
+use crate::{AccountId32, ProvingSystem, Vec, VerificationKeyIdentifier};
 
 /// Generalized pallet executor, that can be mocked for testing purposes.
 pub trait Executor<T>: Sized {
@@ -15,7 +15,7 @@ pub trait Executor<T>: Sized {
     type ErrorGenericType;
 
     fn store_key(
-        depositor: AccountId,
+        depositor: AccountId32,
         identifier: VerificationKeyIdentifier,
         key: Vec<u8>,
     ) -> Result<(), Error<Self::ErrorGenericType>>;
@@ -32,16 +32,16 @@ impl<'a, 'b, E, T> Executor<T> for Environment<'a, 'b, E, BufInBufOutState>
 where
     T: SysConfig + BabyLiminalConfig,
     E: Ext<T = T>,
+    <T as SysConfig>::RuntimeOrigin: From<AccountId32>,
 {
     type ErrorGenericType = T;
 
     fn store_key(
-        depositor: AccountId,
+        depositor: AccountId32,
         identifier: VerificationKeyIdentifier,
         key: Vec<u8>,
     ) -> Result<(), Error<Self::ErrorGenericType>> {
-        let origin: RuntimeOrigin = Some(depositor).into();
-        BabyLiminal::<Runtime>::bare_store_key(origin, identifier, key)
+        BabyLiminal::<T>::bare_store_key(depositor.into(), identifier, key)
     }
 
     fn verify(
