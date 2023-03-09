0c80f93c2c74e3bb47e7f6771e43b55439d83fbf
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-03-09 15:02:51 +0100

    Syntax

--- .github/scripts/test_cliain_snark_relations.sh
+++ .github/scripts/test_cliain_snark_relations.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 CLIAIN=./bin/cliain/target/release/cliain
 
 # Test xor relation
-CLIAIN snark-relation generate-keys xor
-CLIAIN snark-relation generate-proof -p xor.groth16.pk.bytes xor -a 11 -b 11 -c 1
-CLIAIN snark-relation verify \
+${CLIAIN} snark-relation generate-keys xor
+${CLIAIN} snark-relation generate-proof -p xor.groth16.pk.bytes xor -a 11 -b 11 -c 1
+${CLIAIN} snark-relation verify \
   --verifying-key-file xor.groth16.vk.bytes \
   --proof-file xor.groth16.proof.bytes \
   --public-input-file xor.groth16.public_input.bytes
