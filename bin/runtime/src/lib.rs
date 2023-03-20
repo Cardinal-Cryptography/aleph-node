0457383b3772a2862efb775219187b366ebbd66f
Damian Straszak <damian.straszak@gmail.com>
2023-03-20 16:29:20 +0100

    Transaction version should be current on mainnet +1.

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -115,7 +115,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_version: 59,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 15,
+    transaction_version: 16,
     state_version: 0,
 };
 
