2830a07f5fa1b052ccd1583da5348912bd2923a6
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-03-23 15:15:34 +0100

    Hide dependencies

--- relations/src/lib.rs
+++ relations/src/lib.rs
@@ -8,5 +8,6 @@ pub mod shielder;
 pub mod utils;
 pub mod xor;
 
+#[cfg(feature = "circuit")]
 pub use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystemRef, Result, SynthesisError};
 pub use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
