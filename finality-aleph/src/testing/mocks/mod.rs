95217e7a0f1bfe5c48793e89297a525062cd33fa
Damian Leśniak <damian.lesniak@cardinals.cc>
2023-03-27 18:55:05 +0200

    fixing tests, some left for later (todo)

--- finality-aleph/src/testing/mocks/mod.rs
+++ finality-aleph/src/testing/mocks/mod.rs
@@ -3,7 +3,6 @@ use aleph_primitives::BlockNumber;
 pub use block_finalizer::MockedBlockFinalizer;
 pub use block_request::MockedBlockRequester;
 pub use client::{TestClient, TestClientBuilder, TestClientBuilderExt};
-pub use justification_handler_config::JustificationRequestSchedulerImpl;
 pub use proposal::{
     aleph_data_from_blocks, aleph_data_from_headers, unvalidated_proposal_from_headers,
 };
@@ -20,7 +19,6 @@ mod acceptance_policy;
 mod block_finalizer;
 mod block_request;
 mod client;
-mod justification_handler_config;
 mod proposal;
 mod session_info;
 mod single_action_mock;
