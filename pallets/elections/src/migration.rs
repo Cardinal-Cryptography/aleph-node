3133813cc5b45f2db8971d82b627d88ddabcc48c
KostekIV <koscis.j@gmail.com>
2023-03-29 09:13:06 +0200

    migration cleanup

--- pallets/elections/src/migration.rs
+++ pallets/elections/src/migration.rs
@@ -3,18 +3,18 @@ use frame_election_provider_support::Weight;
 use frame_support::{
     log,
     pallet_prelude::{StorageVersion, TypeInfo},
-    traits::{OnRuntimeUpgrade, PalletInfoAccess},
+    traits::OnRuntimeUpgrade,
 };
 use primitives::CommitteeSeats;
 use sp_core::Get;
 #[cfg(feature = "try-runtime")]
 use {frame_support::ensure, pallets_support::ensure_storage_version, sp_std::vec::Vec};
 
-use crate::{CommitteeSize, Config, NextEraCommitteeSize, LOG_TARGET};
+use crate::{CommitteeSize, Config, NextEraCommitteeSize, Pallet, LOG_TARGET};
 
-// V3 CommitteeSeats
+// V4 CommitteeSeats
 #[derive(Decode, Encode, TypeInfo, Debug, Clone, Copy, PartialEq, Eq)]
-pub struct CommitteeSeatsV3 {
+pub struct CommitteeSeatsV4 {
     /// Size of reserved validators in a session
     pub reserved_seats: u32,
     /// Size of non reserved validators in a session
@@ -23,20 +23,20 @@ pub struct CommitteeSeatsV3 {
 
 /// Migration add field for `CommitteeSize` and `NextEraCommitteeSize` `finality_committee_non_reserved_seats` to
 /// `CommitteeSeats`.
-pub struct Migration<T, P>(sp_std::marker::PhantomData<(T, P)>);
+pub struct Migration<T>(sp_std::marker::PhantomData<T>);
 
-impl<T: Config, P: PalletInfoAccess> OnRuntimeUpgrade for Migration<T, P> {
+impl<T: Config> OnRuntimeUpgrade for Migration<T> {
     fn on_runtime_upgrade() -> Weight {
         log::info!(
             target: LOG_TARGET,
-            "Running migration from STORAGE_VERSION 3 to 4 for pallet elections"
+            "Running migration from STORAGE_VERSION 4 to 5 for pallet elections"
         );
 
         let reads = 2;
         let mut writes = 1;
 
-        if CommitteeSize::<T>::translate::<CommitteeSeatsV3, _>(|old| {
-            if let Some(CommitteeSeatsV3 {
+        if CommitteeSize::<T>::translate::<CommitteeSeatsV4, _>(|old| {
+            if let Some(CommitteeSeatsV4 {
                 reserved_seats,
                 non_reserved_seats,
             }) = old
@@ -57,8 +57,8 @@ impl<T: Config, P: PalletInfoAccess> OnRuntimeUpgrade for Migration<T, P> {
             log::error!(target: LOG_TARGET, "Could not migrate CommitteeSize");
         }
 
-        if NextEraCommitteeSize::<T>::translate::<CommitteeSeatsV3, _>(|old| {
-            if let Some(CommitteeSeatsV3 {
+        if NextEraCommitteeSize::<T>::translate::<CommitteeSeatsV4, _>(|old| {
+            if let Some(CommitteeSeatsV4 {
                 reserved_seats,
                 non_reserved_seats,
             }) = old
@@ -79,20 +79,20 @@ impl<T: Config, P: PalletInfoAccess> OnRuntimeUpgrade for Migration<T, P> {
             log::error!(target: LOG_TARGET, "Could not migrate NextCommitteeSize");
         }
 
-        StorageVersion::new(4).put::<P>();
+        StorageVersion::new(5).put::<Pallet<T>>();
         T::DbWeight::get().reads(reads) + T::DbWeight::get().writes(writes)
     }
 
     #[cfg(feature = "try-runtime")]
     fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
-        ensure_storage_version::<P>(4)?;
+        ensure_storage_version::<Pallet<T>>(4)?;
 
         Ok(Vec::new())
     }
 
     #[cfg(feature = "try-runtime")]
     fn post_upgrade(_: Vec<u8>) -> Result<(), &'static str> {
-        ensure_storage_version::<P>(5)?;
+        ensure_storage_version::<Pallet<T>>(5)?;
 
         let committee_seats = CommitteeSize::<T>::get();
         ensure!(
