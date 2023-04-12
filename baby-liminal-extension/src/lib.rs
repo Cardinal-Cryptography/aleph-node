bf429924e53fb80007723097340d9c7df8dca414
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-04-12 17:11:40 +0200

    Drop support for Gm17 and Marlin in pallet-baby-liminal

--- baby-liminal-extension/src/lib.rs
+++ baby-liminal-extension/src/lib.rs
@@ -18,7 +18,6 @@ pub mod executor;
 use ::ink::{prelude::vec::Vec, primitives::AccountId as AccountId32};
 #[cfg(feature = "substrate")]
 use obce::substrate::{sp_runtime::AccountId32, sp_std::vec::Vec};
-use scale::{Decode, Encode};
 #[cfg(feature = "std")]
 use scale_info::TypeInfo;
 
@@ -79,26 +78,6 @@ pub type VerificationKeyIdentifier = [u8; 8];
 
 pub type SingleHashInput = (u64, u64, u64, u64);
 
-/// Copied from `pallet_baby_liminal`.
-#[derive(Copy, Clone, Eq, PartialEq, Debug, Decode, Encode)]
-#[cfg_attr(feature = "std", derive(TypeInfo))]
-pub enum ProvingSystem {
-    Groth16,
-    Gm17,
-    Marlin,
-}
-
-#[cfg(feature = "substrate")]
-impl From<ProvingSystem> for pallet_baby_liminal::ProvingSystem {
-    fn from(system: ProvingSystem) -> Self {
-        match system {
-            ProvingSystem::Groth16 => pallet_baby_liminal::ProvingSystem::Groth16,
-            ProvingSystem::Gm17 => pallet_baby_liminal::ProvingSystem::Gm17,
-            ProvingSystem::Marlin => pallet_baby_liminal::ProvingSystem::Marlin,
-        }
-    }
-}
-
 /// BabyLiminal chain extension definition.
 #[obce::definition(id = "baby-liminal-extension@v0.1")]
 pub trait BabyLiminalExtension {
@@ -118,7 +97,6 @@ pub trait BabyLiminalExtension {
         identifier: VerificationKeyIdentifier,
         proof: Vec<u8>,
         input: Vec<u8>,
-        system: ProvingSystem,
     ) -> Result<(), BabyLiminalError>;
 
     #[obce(id = 43)]
