66ea2c84efe4692f5afeb22a230c9b09194cff7a
KostekIV <koscis.j@gmail.com>
2023-05-23 23:00:44 +0200

    fixes

--- bin/node/src/main.rs
+++ bin/node/src/main.rs
@@ -111,7 +111,7 @@ fn main() -> sc_cli::Result<()> {
         }
         #[cfg(feature = "try-runtime")]
         Some(Subcommand::TryRuntime(cmd)) => {
-            use aleph_primitives::MILLISECS_PER_BLOCK;
+            use primitives::MILLISECS_PER_BLOCK;
             use sc_executor::{sp_wasm_interface::ExtendedHostFunctions, NativeExecutionDispatch};
             use try_runtime_cli::block_building_info::timestamp_with_aura_info;
             let runner = cli.create_runner(cmd)?;
