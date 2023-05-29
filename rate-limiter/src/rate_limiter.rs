f79eaaf952f58fecd9330b795e39010ab33fcfc1
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2023-05-29 11:30:27 +0200

    fixed how Sleep is called in rate_limiter

--- rate-limiter/src/rate_limiter.rs
+++ rate-limiter/src/rate_limiter.rs
@@ -33,21 +33,25 @@ impl SleepingRateLimiter {
         }
     }
 
-    fn set_sleep(&mut self, read_size: usize) -> &mut Pin<Box<Sleep>> {
+    fn set_sleep(&mut self, read_size: usize) -> Option<&mut Pin<Box<Sleep>>> {
         let mut now = None;
         let mut now_closure = || *now.get_or_insert_with(Instant::now);
         let next_wait = self.rate_limiter.rate_limit(read_size, &mut now_closure);
         if let Some(next_wait) = next_wait {
             let wait_until = now_closure() + next_wait;
             self.sleep.set(tokio::time::sleep_until(wait_until.into()));
+            Some(&mut self.sleep)
+        } else {
+            None
         }
-        &mut self.sleep
     }
 
     /// Given `read_size`, that is an amount of units of some governed resource, delays return of `Self` to satisfy configure
     /// rate.
     pub async fn rate_limit(mut self, read_size: usize) -> Self {
-        self.set_sleep(read_size).await;
+        if let Some(sleep) = self.set_sleep(read_size) {
+            sleep.await;
+        }
         self
     }
 }
