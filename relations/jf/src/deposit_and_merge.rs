d5b8e0554da4b7f745fea4d76f65a52c08e51ce8
filip <fbielejec@gmail.com>
2023-05-31 11:21:12 +0200

    types def

--- relations/jf/src/deposit_and_merge.rs
+++ relations/jf/src/deposit_and_merge.rs
@@ -41,3 +41,56 @@ pub struct DepositAndMergePrivateInput {
     pub old_token_amount: TokenAmount,
     pub new_token_amount: TokenAmount,
 }
+
+impl Default for DepositAndMergePrivateInput {
+    fn default() -> Self {
+        let index = BigUint::from(0u64);
+        let value = CircuitField::from(0u64);
+
+        let merkle_tree =
+            RescueSparseMerkleTree::from_kv_set(MERKLE_TREE_HEIGHT, &[(index.clone(), value)])
+                .unwrap();
+
+        let (_, merkle_proof) = merkle_tree.lookup(&index).expect_ok().unwrap();
+
+        Self {
+            old_trapdoor: Default::default(),
+            new_trapdoor: Default::default(),
+            new_nullifier: Default::default(),
+            merkle_path: merkle_proof,
+            leaf_index: Default::default(),
+            old_note: Default::default(),
+            old_token_amount: Default::default(),
+            new_token_amount: Default::default(),
+        }
+    }
+}
+
+impl DepositAndMergeRelation {
+    pub fn new(public: DepositAndMergePublicInput, private: DepositAndMergePrivateInput) -> Self {
+        let old_note = SourcedNote {
+            note: private.old_note,
+            token_id: public.token_id,
+            token_amount: private.old_token_amount,
+            trapdoor: private.old_trapdoor,
+            nullifier: public.old_nullifier,
+            note_type: NoteType::Spend,
+        };
+
+        let new_note = SourcedNote {
+            note: public.new_note,
+            token_id: public.token_id,
+            token_amount: private.new_token_amount,
+            trapdoor: private.new_trapdoor,
+            nullifier: private.new_nullifier,
+            note_type: NoteType::Redeposit,
+        };
+
+        Self {
+            old_note,
+            new_note,
+            merkle_path: private.merkle_path,
+            leaf_index: private.leaf_index,
+        }
+    }
+}
