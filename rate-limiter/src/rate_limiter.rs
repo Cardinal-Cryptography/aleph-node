af08cfb22a48256ae960ef301993b7b8873aba5d
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2023-06-12 09:59:46 +0200

    simplified rate_limiter sleep logic

--- rate-limiter/src/rate_limiter.rs
+++ rate-limiter/src/rate_limiter.rs
@@ -1,11 +1,8 @@
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 use futures::{future::BoxFuture, FutureExt};
 use log::trace;
-use tokio::{
-    io::AsyncRead,
-    time::{interval, Interval},
-};
+use tokio::io::AsyncRead;
 
 use crate::{token_bucket::TokenBucket, LOG_TARGET};
 
@@ -13,16 +10,12 @@ use crate::{token_bucket::TokenBucket, LOG_TARGET};
 /// resource, it calculates how long we should delay our next access to that resource in order to satisfy that rate.
 pub struct SleepingRateLimiter {
     rate_limiter: TokenBucket,
-    interval: Interval,
-    last_now: Instant,
 }
 
 impl Clone for SleepingRateLimiter {
     fn clone(&self) -> Self {
         Self {
             rate_limiter: self.rate_limiter.clone(),
-            interval: interval(Duration::from_secs(1)),
-            last_now: self.last_now.clone(),
         }
     }
 }
@@ -32,37 +25,7 @@ impl SleepingRateLimiter {
     pub fn new(rate_per_second: usize) -> Self {
         Self {
             rate_limiter: TokenBucket::new(rate_per_second),
-            interval: interval(Duration::from_secs(1)),
-            last_now: Instant::now(),
         }
-    }
-
-    fn now(&mut self) -> Instant {
-        self.interval
-            .tick()
-            .now_or_never()
-            .map_or(self.last_now, |tick| {
-                let tick = tick.into();
-                self.last_now = tick;
-                tick
-            })
-    }
-
-    async fn compute_sleep(&mut self, read_size: usize) {
-        let now = self.now();
-        let next_wait = self.rate_limiter.rate_limit(read_size, now);
-        let wait_until = if let Some(next_wait) = next_wait {
-            now + next_wait
-        } else {
-            return;
-        };
-        trace!(
-            target: LOG_TARGET,
-            "Rate-Limiter will sleep until {:?} after reading {} byte(s).",
-            wait_until,
-            read_size
-        );
-        tokio::time::sleep_until(wait_until.into()).await
     }
 
     /// Given `read_size`, that is an amount of units of some governed resource, delays return of `Self` to satisfy configure
@@ -73,7 +36,23 @@ impl SleepingRateLimiter {
             "Rate-Limiter attempting to read {}.",
             read_size
         );
-        self.compute_sleep(read_size).await;
+
+        let now = Instant::now();
+        let next_wait = self.rate_limiter.rate_limit(read_size, now);
+        let wait_until = if let Some(next_wait) = next_wait {
+            now + next_wait
+        } else {
+            return self;
+        };
+
+        trace!(
+            target: LOG_TARGET,
+            "Rate-Limiter will sleep until {:?} after reading {} byte(s).",
+            wait_until,
+            read_size
+        );
+
+        tokio::time::sleep_until(wait_until.into()).await;
         self
     }
 }
