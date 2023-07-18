bf7a1861347f431538aa9b84bae8a2fbf2f29756
Grzegorz Gawryał <grzegorz.gawryal@cardinals.cc>
2023-07-18 11:26:34 +0200

    Add eol to rust-toolchains and use repository rust toolchain variable

--- scripts/run_checks_on_aleph_node.sh
+++ scripts/run_checks_on_aleph_node.sh
@@ -5,3 +5,4 @@ set -e
 CARGO_INCREMENTAL=0 cargo clippy --all-targets --all-features -- --no-deps -D warnings
 CARGO_INCREMENTAL=0 cargo +nightly-2023-01-10 fmt --all
 CARGO_INCREMENTAL=0 cargo test --lib
+
