c5237cc2c00506613e2acf2b09d0835fdc66cf90
lesniak43 <lesniak43@gmail.com>
2023-08-04 10:50:03 +0200

    Update finality-aleph/src/sync/handler/mod.rs
    Co-authored-by: kostekIV <27210860+kostekIV@users.noreply.github.com>

--- finality-aleph/src/sync/handler/mod.rs
+++ finality-aleph/src/sync/handler/mod.rs
@@ -810,7 +810,7 @@ mod tests {
         assert_eq!(
             handler.interest_provider().get(&fork_top),
             Interest::Uninterested,
-            "should be pruned"
+            "should be uninterested"
         );
     }
 
