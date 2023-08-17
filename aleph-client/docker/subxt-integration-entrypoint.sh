b1f05de81fdeca0e8c9754ae287df46bff0548b6
Grzegorz Gawryał <grzegorz.gawryal@cardinals.cc>
2023-08-17 10:50:23 +0200

    Remove codegen script

--- aleph-client/docker/subxt-integration-entrypoint.sh
+++ aleph-client/docker/subxt-integration-entrypoint.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-../generate_interface.sh
+
+subxt codegen --derive Clone --derive Debug --derive PartialEq --derive Eq \
+  | sed 's/::[ ]*subxt[ ]*::[ ]*utils[ ]*::[ ]*AccountId32/::subxt::utils::Static<::subxt::ext::sp_core::crypto::AccountId32>/g' \
+  | rustfmt --edition=2021 > aleph_zeroo.rs;
 
 diff -y -W 200 --suppress-common-lines aleph_zero.rs aleph-node/aleph-client/src/aleph_zero.rs
 diff_exit_code=$?
 if [[ ! $diff_exit_code -eq 0 ]]; then
   echo "Current runtime metadata is different than versioned in git!"
-  echo "Run ./generate_interface.sh from aleph-client directory and commit to git."
-   exit 1
+  echo "Run subxt codegen command as in $(basename $0) from aleph-client directory and commit to git."
+  exit 1
 fi
 echo "Current runtime metadata and versioned in git matches."
