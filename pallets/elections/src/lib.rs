38e34c9938819254e5f8360d1c9fd8a405a1d22f
KostekIV <koscis.j@gmail.com>
2023-09-01 01:22:52 +0200

    yet another fix

--- pallets/elections/src/lib.rs
+++ pallets/elections/src/lib.rs
@@ -34,7 +34,6 @@ use sp_std::{
 pub type TotalReward = u32;
 
 const STORAGE_VERSION: StorageVersion = StorageVersion::new(5);
-const LOG_TARGET: &str = "pallet-elections";
 
 #[derive(Decode, Encode, TypeInfo)]
 pub struct ValidatorTotalRewards<T>(pub BTreeMap<T, TotalReward>);
