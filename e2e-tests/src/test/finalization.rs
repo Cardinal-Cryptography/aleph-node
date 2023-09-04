fac7ec0f98f0eb6ea9acb1511fb2b961fd1e28cf
Grzegorz Gawryał <grzegorz.gawryal@cardinals.cc>
2023-09-04 09:45:22 +0200

    Add logging in finalization test

--- e2e-tests/src/test/finalization.rs
+++ e2e-tests/src/test/finalization.rs
@@ -1,10 +1,10 @@
+use crate::config::setup_test;
 use aleph_client::{
     utility::BlocksApi,
     waiting::{AlephWaiting, BlockStatus},
 };
 use anyhow::anyhow;
-
-use crate::config::setup_test;
+use log::info;
 
 #[tokio::test]
 pub async fn finalization() -> anyhow::Result<()> {
@@ -12,12 +12,14 @@ pub async fn finalization() -> anyhow::Result<()> {
     let connection = config.create_root_connection().await;
 
     let finalized = connection.get_finalized_block_hash().await?;
+    info!("Highest finalized block hash = {finalized}");
     let finalized_number = connection
         .get_block_number(finalized)
         .await?
         .ok_or(anyhow!(
             "Failed to retrieve block number for hash {finalized:?}"
         ))?;
+    info!("Waiting for block {} to be finalized", finalized_number + 1);
 
     connection
         .wait_for_block(|n| n > finalized_number, BlockStatus::Finalized)
