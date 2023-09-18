81cf1b3b8dc0bda6426542d45043a1368a73e3a7
Marcin <marcin.radecki@cardinals.cc>
2023-09-18 13:32:09 +0200

    Now both liminal and non-liminal Runtime structs are updated.

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -751,7 +751,7 @@ impl pallet_identity::Config for Runtime {
 // Create the runtime by composing the FRAME pallets that were previously configured.
 #[cfg(not(feature = "liminal"))]
 construct_runtime!(
-    pub enum Runtime where
+    pub struct Runtime where
         Block = Block,
         NodeBlock = opaque::Block,
         UncheckedExtrinsic = UncheckedExtrinsic
