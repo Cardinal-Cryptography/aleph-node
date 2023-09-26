e87b9001b7d5466840ef397c6dcc61ac039945f8
timorleph <tomasz.kisielewski@cardinals.cc>
2023-09-26 13:42:47 +0200

    Better constants in sync oracle

--- finality-aleph/src/sync_oracle.rs
+++ finality-aleph/src/sync_oracle.rs
@@ -9,6 +9,10 @@ use std::{
 use parking_lot::Mutex;
 use sp_consensus::SyncOracle as SyncOracleT;
 
+// This should never exceed 1800 due to the structure of the forest, thus 9000 is a decent marker of being uninitialized.
+const UNINITIALIZED_THRESHOLD: u32 = 9000;
+const OFFLINE_THRESHOLD: Duration = Duration::from_secs(6);
+
 /// A sync oracle implementation tracking how far behind the highest known justification the node is.
 /// It defines being in major sync as knowing of any justification of an unknown block.
 /// It defines being offline as not getting any update for at least 6 seconds (or never at all).
@@ -21,8 +25,7 @@ pub struct SyncOracle {
 impl SyncOracle {
     pub fn new() -> Self {
         SyncOracle {
-            // This should never exceed 1800 due to the structure of the forest, thus 9000 is a decent marker of being uninitialized.
-            behind: Arc::new(AtomicU32::new(9001)),
+            behind: Arc::new(AtomicU32::new(UNINITIALIZED_THRESHOLD + 1)),
             last_update: Arc::new(Mutex::new(Instant::now())),
         }
     }
@@ -44,7 +47,7 @@ impl SyncOracleT for SyncOracle {
     }
 
     fn is_offline(&self) -> bool {
-        self.last_update.lock().elapsed() > Duration::from_secs(6)
-            || self.behind.load(Ordering::Relaxed) > 9000
+        self.last_update.lock().elapsed() > OFFLINE_THRESHOLD
+            || self.behind.load(Ordering::Relaxed) > UNINITIALIZED_THRESHOLD
     }
 }
