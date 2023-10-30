9cac354b27643791292c980349d3b29d9f5a12a4
Jan Koscisz <koscis.j@gmail.com>
2023-10-30 19:36:15 +0100

    qol improvements

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -34,7 +34,6 @@ use frame_system::{EnsureRoot, EnsureSignedBy};
 use frame_try_runtime::UpgradeCheckSelect;
 pub use pallet_balances::Call as BalancesCall;
 use pallet_committee_management::SessionAndEraManager;
-use pallet_session::QueuedKeys;
 pub use pallet_timestamp::Call as TimestampCall;
 use pallet_transaction_payment::{CurrencyAdapter, Multiplier, TargetedFeeAdjustment};
 use primitives::{
@@ -47,7 +46,6 @@ use primitives::{
 };
 pub use primitives::{AccountId, AccountIndex, Balance, Hash, Nonce, Signature};
 use sp_api::impl_runtime_apis;
-use sp_application_crypto::key_types::AURA;
 use sp_consensus_aura::SlotDuration;
 use sp_core::{crypto::KeyTypeId, ConstU128, OpaqueMetadata};
 #[cfg(any(feature = "std", test))]
@@ -1015,12 +1013,6 @@ impl_runtime_apis! {
             session: SessionIndex,
         ) -> Result<SessionCommittee<AccountId>, SessionValidatorError> {
             CommitteeManagement::predict_session_committee_for_session(session)
-        }
-
-        fn next_session_aura_authorities() -> Vec<AuraId> {
-            let queued_keys = QueuedKeys::<Runtime>::get();
-
-            queued_keys.into_iter().filter_map(|(_, keys)| keys.get(AURA)).collect()
         }
 
          fn key_owner(key: AlephId) -> Option<AccountId> {
--- finality-aleph/src/runtime_api.rs
+++ finality-aleph/src/runtime_api.rs
@@ -7,7 +7,6 @@ use std::{
 use aleph_runtime::SessionKeys;
 use parity_scale_codec::Decode;
 use sc_client_api::Backend;
-use sc_consensus_aura::AuraApi;
 use sp_application_crypto::key_types::AURA;
 use sp_core::twox_128;
 use sp_runtime::traits::{Block, OpaqueKeys};
@@ -20,6 +19,7 @@ use crate::{
 /// Trait handling connection between host code and runtime storage
 pub trait RuntimeApi {
     type Error: Display;
+    /// Returns aura authorities for the next session using state from block `at`
     fn next_aura_authorities(&self, at: BlockHash) -> Result<Vec<AuraId>, Self::Error>;
 }
 
@@ -29,7 +29,6 @@ type QueuedKeys = Vec<(AccountId, SessionKeys)>;
 pub struct RuntimeApiImpl<C, B, BE>
 where
     C: ClientForAleph<B, BE> + Send + Sync + 'static,
-    C::Api: crate::aleph_primitives::AlephSessionApi<B> + AuraApi<B, AuraId>,
     B: Block<Hash = BlockHash>,
     BE: Backend<B> + 'static,
 {
@@ -40,7 +39,6 @@ where
 impl<C, B, BE> RuntimeApiImpl<C, B, BE>
 where
     C: ClientForAleph<B, BE> + Send + Sync + 'static,
-    C::Api: crate::aleph_primitives::AlephSessionApi<B> + AuraApi<B, AuraId>,
     B: Block<Hash = BlockHash>,
     BE: Backend<B> + 'static,
 {
@@ -51,7 +49,12 @@ where
         }
     }
 
-    fn read_storage<D: Decode>(&self, pallet: &str, item: &str, at_block: BlockHash) -> Option<D> {
+    fn read_storage<D: Decode>(
+        &self,
+        pallet: &str,
+        item: &str,
+        at_block: BlockHash,
+    ) -> Result<D, ApiError> {
         let storage_key = [twox_128(pallet.as_bytes()), twox_128(item.as_bytes())].concat();
 
         let encoded = match self
@@ -59,16 +62,17 @@ where
             .storage(at_block, &sc_client_api::StorageKey(storage_key))
         {
             Ok(Some(e)) => e,
-            _ => return None,
+            _ => return Err(ApiError::NoStorage),
         };
 
-        D::decode(&mut encoded.0.as_ref()).ok()
+        D::decode(&mut encoded.0.as_ref()).map_err(|_| ApiError::DecodeError)
     }
 }
 
 #[derive(Copy, Clone, Debug)]
 pub enum ApiError {
     NoStorage,
+    DecodeError,
 }
 
 impl Display for ApiError {
@@ -80,17 +84,13 @@ impl Display for ApiError {
 impl<C, B, BE> RuntimeApi for RuntimeApiImpl<C, B, BE>
 where
     C: ClientForAleph<B, BE> + Send + Sync + 'static,
-    C::Api: crate::aleph_primitives::AlephSessionApi<B> + AuraApi<B, AuraId>,
     B: Block<Hash = BlockHash>,
     BE: Backend<B> + 'static,
 {
     type Error = ApiError;
 
     fn next_aura_authorities(&self, at: BlockHash) -> Result<Vec<AuraId>, Self::Error> {
-        let queued_keys: QueuedKeys = match self.read_storage("Session", "QueuedKeys", at) {
-            Some(qk) => qk,
-            None => return Err(ApiError::NoStorage),
-        };
+        let queued_keys: QueuedKeys = self.read_storage("Session", "QueuedKeys", at)?;
 
         Ok(queued_keys
             .into_iter()
