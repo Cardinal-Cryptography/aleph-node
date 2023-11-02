1816b24a61c742e5556af1c0581a7baba7bf7a97
Lukasz Gniecki <lukasgniecki@gmail.com>
2023-11-02 20:28:24 +0100

    local test for recover

---
+++ local-tests/test_recover.py
@@ -0,0 +1,60 @@
+#!/bin/env python
+import os
+from os.path import abspath, join
+from time import ctime, sleep
+from chainrunner import Chain, Seq, generate_keys
+
+
+def printt(s): print(ctime() + ' | ' + s)
+
+
+# Path to working directory, where chainspec, logs and nodes' dbs are written:
+workdir = abspath(os.getenv('WORKDIR', '/tmp/workdir'))
+# Path to the aleph-node binary (important DON'T use short-session feature):
+binary = abspath(os.getenv('ALEPH_NODE_BINARY', join(workdir, 'aleph-node')))
+
+phrases = [f'//{i}' for i in range(8)]
+keys = generate_keys(binary, phrases)
+chain = Chain(workdir)
+printt('Bootstrapping the chain with binary')
+
+chain.bootstrap(binary,
+                keys.values(),
+                sudo_account_id=keys[phrases[0]],
+                chain_type='local')
+chain.set_flags('no-mdns',
+                port=Seq(30334),
+                validator_port=Seq(30343),
+                rpc_port=Seq(9944),
+                unit_creation_delay=200,
+                execution='Native')
+
+addresses = [n.address() for n in chain]
+validator_addresses = [n.validator_address() for n in chain]
+chain.set_flags(bootnodes=addresses[1])
+chain.set_flags_validator(public_addr=addresses, public_validator_addresses=validator_addresses)
+
+BLOCKS_PER_STAGE = 100
+chain.set_flags_validator('validator')
+
+printt('Starting the chain')
+chain.start('aleph')
+
+chain.wait_for_finalization(BLOCKS_PER_STAGE, catchup=True, catchup_delta=5)  # run normally for some time
+
+printt('Stopping all nodes')
+chain.stop(nodes=range(8))
+
+sleep(10)
+
+printt('Starting the chain')
+chain.start('aleph-recovered', nodes=range(7))  # restart all except the last
+
+f1 = chain.get_highest_finalized()
+assert f1 >= BLOCKS_PER_STAGE
+
+chain.wait_for_finalization(2 * BLOCKS_PER_STAGE, catchup=True, catchup_delta=5, nodes=range(7))
+f2 = chain.get_highest_finalized()
+assert f2 >= 2 * BLOCKS_PER_STAGE
+
+print('Ok')
