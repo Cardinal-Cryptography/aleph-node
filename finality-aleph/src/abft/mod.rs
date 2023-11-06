c069e14f3385225c1e90bb63e857de4dd0ad21af
Lukasz Gniecki <lukasgniecki@gmail.com>
2023-11-06 09:51:57 +0100

    use single signature in abft/mod

--- finality-aleph/src/abft/mod.rs
+++ finality-aleph/src/abft/mod.rs
@@ -72,7 +72,7 @@ impl<S: 'static> IntoIterator for SignatureSet<S> {
     }
 }
 
-impl<S: legacy_aleph_bft::Signature> legacy_aleph_bft::PartialMultisignature for SignatureSet<S> {
+impl<S: Signature> legacy_aleph_bft::PartialMultisignature for SignatureSet<S> {
     type Signature = S;
 
     fn add_signature(
@@ -84,7 +84,7 @@ impl<S: legacy_aleph_bft::Signature> legacy_aleph_bft::PartialMultisignature for
     }
 }
 
-impl<S: current_aleph_bft::Signature> current_aleph_bft::PartialMultisignature for SignatureSet<S> {
+impl<S: Signature> current_aleph_bft::PartialMultisignature for SignatureSet<S> {
     type Signature = S;
 
     fn add_signature(
