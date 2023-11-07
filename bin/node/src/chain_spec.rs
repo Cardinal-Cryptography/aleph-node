f48800599a863e496e617818fcc4f6c4a90ca309
Marcin <marcin.radecki@cardinals.cc>
2023-11-07 12:42:18 +0100

    Removed not used import.

--- bin/node/src/chain_spec.rs
+++ bin/node/src/chain_spec.rs
@@ -20,7 +20,7 @@ use sp_core::{sr25519, Pair};
 use crate::aleph_primitives::{
     staking::{MIN_NOMINATOR_BOND, MIN_VALIDATOR_BOND},
     AuraId, AuthorityId as AlephId, SessionValidators, Version as FinalityVersion,
-    ADDRESSES_ENCODING, LEGACY_FINALITY_VERSION, TOKEN, TOKEN_DECIMALS,
+    ADDRESSES_ENCODING, LEGACY_FINALITY_VERSION, TOKEN_DECIMALS,
 };
 
 pub const CHAINTYPE_DEV: &str = "dev";
