ad1c0aeed284a7457c741a93b2a22a9f8ea231f1
timorleph <tomasz.kisielewski@cardinals.cc>
2023-11-14 15:09:03 +0100

    Proposals with headers

--- finality-aleph/src/data_io/legacy/mod.rs
+++ finality-aleph/src/data_io/legacy/mod.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Debug, hash::Hash, num::NonZeroUsize};
+use std::{fmt::Debug, hash::Hash};
 
 use parity_scale_codec::{Decode, Encode};
 
@@ -8,6 +8,7 @@ mod data_store;
 mod proposal;
 mod status_provider;
 
+pub use super::ChainInfoCacheConfig;
 pub use data_interpreter::OrderedDataInterpreter;
 pub use data_provider::{ChainTracker, DataProvider};
 pub use data_store::{DataStore, DataStoreConfig};
@@ -28,15 +29,27 @@ pub trait AlephNetworkMessage: Clone + Debug {
     fn included_data(&self) -> Vec<AlephData>;
 }
 
-#[derive(Clone, Debug)]
-pub struct ChainInfoCacheConfig {
-    pub block_cache_capacity: NonZeroUsize,
-}
+#[cfg(test)]
+mod test {
+    use crate::{
+        data_io::legacy::{AlephData, UnvalidatedAlephProposal},
+        testing::mocks::{TBlock, THeader},
+    };
 
-impl Default for ChainInfoCacheConfig {
-    fn default() -> ChainInfoCacheConfig {
-        ChainInfoCacheConfig {
-            block_cache_capacity: NonZeroUsize::new(2000).unwrap(),
+    pub fn unvalidated_proposal_from_headers(headers: Vec<THeader>) -> UnvalidatedAlephProposal {
+        let num = headers.last().unwrap().number;
+        let hashes = headers.into_iter().map(|header| header.hash()).collect();
+        UnvalidatedAlephProposal::new(hashes, num)
+    }
+
+    pub fn aleph_data_from_blocks(blocks: Vec<TBlock>) -> AlephData {
+        let headers = blocks.into_iter().map(|b| b.header).collect();
+        aleph_data_from_headers(headers)
+    }
+
+    pub fn aleph_data_from_headers(headers: Vec<THeader>) -> AlephData {
+        AlephData {
+            head_proposal: unvalidated_proposal_from_headers(headers),
         }
     }
 }
--- finality-aleph/src/testing/mocks/mod.rs
+++ finality-aleph/src/testing/mocks/mod.rs
@@ -5,14 +5,60 @@ pub use proposal::{
     aleph_data_from_blocks, aleph_data_from_headers, unvalidated_proposal_from_headers,
 };
 use sp_runtime::traits::BlakeTwo256;
+use std::fmt::{Display, Error as FmtError, Formatter};
 use substrate_test_runtime::Extrinsic;
 
-use crate::aleph_primitives::BlockNumber;
+use crate::{
+    aleph_primitives::BlockNumber,
+    block::{EquivocationProof, HeaderVerifier, VerifiedHeader},
+};
 
 type Hashing = BlakeTwo256;
 pub type TBlock = sp_runtime::generic::Block<THeader, Extrinsic>;
 pub type THeader = sp_runtime::generic::Header<BlockNumber, Hashing>;
 pub type THash = substrate_test_runtime::Hash;
+
+#[derive(Clone)]
+pub struct TestVerifier;
+
+pub struct TestEquivocationProof;
+
+impl Display for TestEquivocationProof {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
+        write!(f, "this should never get created")
+    }
+}
+
+impl EquivocationProof for TestEquivocationProof {
+    fn are_we_equivocating(&self) -> bool {
+        false
+    }
+}
+
+#[derive(Debug)]
+pub struct TestVerificationError;
+
+impl Display for TestVerificationError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
+        write!(f, "this should never get created")
+    }
+}
+
+impl HeaderVerifier<THeader> for TestVerifier {
+    type EquivocationProof = TestEquivocationProof;
+    type Error = TestVerificationError;
+
+    fn verify_header(
+        &mut self,
+        header: THeader,
+        _just_created: bool,
+    ) -> Result<VerifiedHeader<THeader, Self::EquivocationProof>, Self::Error> {
+        Ok(VerifiedHeader {
+            header,
+            maybe_equivocation_proof: None,
+        })
+    }
+}
 
 mod acceptance_policy;
 mod block_finalizer;
