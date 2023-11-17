4fd70b5c100c9ac9b9f599e74d1261293b6d97af
Grzegorz Gawryał <grzegorz.gawryal@cardinals.cc>
2023-11-17 11:55:38 +0100

    Promote oldest txns if they are still in the pool

--- bin/node/src/metrics.rs
+++ bin/node/src/metrics.rs
@@ -16,6 +16,8 @@ const LOG_TARGET: &str = "aleph-metrics";
 
 // Size of transaction cache: 32B (Hash) + 16B (Instant) * `100_000` is approximately 4.8MB
 const TRANSACTION_CACHE_SIZE: usize = 100_000;
+// Maximum number of transactions to recheck if they are still in the pool, per single loop iteration.
+const MAX_RECHECKED_TRANSACTIONS: usize = 4;
 
 pub type ExtrinsicHash<A> = <<A as ChainApi>::Block as traits::Block>::Hash;
 
@@ -43,11 +45,13 @@ pub async fn run_metrics<A, B, BE>(
             maybe_block = best_block_notifications.next() => {
                 match maybe_block {
                     Some(block) => {
-                        for xt in backend.block(block.hash).unwrap().unwrap().block.extrinsics() {
-                            let hash = pool.hash_of(xt);
-                            if let Some(insert_time) = cache.pop(&hash) {
-                                let elapsed = insert_time.elapsed();
-                                log::trace!(target: LOG_TARGET, "[transaction_pool_metrics] extrinsic {hash:?} included after {elapsed:?}, lru size = {:?}", cache.len());
+                        if let Ok(Some(body)) = backend.block_body(block.hash) {
+                            for xt in body {
+                                let hash = pool.hash_of(&xt);
+                                if let Some(insert_time) = cache.pop(&hash) {
+                                    let elapsed = insert_time.elapsed();
+                                    log::trace!(target: LOG_TARGET, "[transaction_pool_metrics] extrinsic {hash:?} included after {elapsed:?}, lru size = {:?}", cache.len());
+                                }
                             }
                         }
                     }
@@ -59,43 +63,41 @@ pub async fn run_metrics<A, B, BE>(
             maybe_transaction = transaction_notifications.next() => {
                 match maybe_transaction {
                     Some(hash) => {
-                        let maybe_popped = cache.put(hash, Instant::now());
-                        log::trace!(target: LOG_TARGET, "[transaction_pool_metrics] inserted extrinsic {hash:?}, lru size = {:?}", cache.len());
-                        if let Some(_insert_time) = maybe_popped {
-                            // TODO: check if still in validated pool and maybe report
-                        }
+                        // Putting new transaction can evict the oldest one. However, even if the
+                        // removed transaction was actually still in the pool, we don't have
+                        // any guarantees that it could be included in the block. Therefore, we
+                        // we ignore such transaction.
+                        cache.put(hash, Instant::now());
                     }
                     None => {
-                        log::warn!(target: LOG_TARGET, "Tx stream ended unexpectedly");
+                        log::warn!(target: LOG_TARGET, "Transaction stream ended unexpectedly");
                     }
                 }
             },
         }
-        let mut popped_transactions = 0;
+        let mut rechecked_transactions = 0;
         while let Some((&hash, _)) = cache.peek_lru() {
-            match pool
-                .pool()
-                .validated_pool()
-                .check_is_known(&hash, false)
-                .map_err(|e| e.into_pool_error())
-            {
-                Err(Ok(Error::AlreadyImported(_))) => break,
-                Ok(()) | Err(Ok(Error::TemporarilyBanned)) => {
-                    cache.pop_lru();
-                    log::trace!(
-                        "[transaction_pool_metrics] Extrinsic {hash:?} is no longer valid, removing, lru size = {:?}",
-                        cache.len()
-                    );
-                }
-                _ => {
-                    log::trace!("Unknown error in from transaction pool check_is_known");
-                    break;
-                }
-            };
-            popped_transactions += 1;
-            if popped_transactions > 4 {
+            if !is_in_the_pool(&hash, pool) {
+                cache.pop_lru();
+            } else {
+                cache.promote(&hash);
+            }
+            rechecked_transactions += 1;
+            if rechecked_transactions > MAX_RECHECKED_TRANSACTIONS {
                 break;
             }
         }
     }
+}
+
+fn is_in_the_pool<A, B>(hash: &ExtrinsicHash<A>, pool: &BasicPool<A, B>) -> bool
+where
+    B: BlockT,
+    A: ChainApi<Block = B> + 'static,
+{
+    let knowledge = pool.pool().validated_pool().check_is_known(hash, false);
+    matches!(
+        knowledge.map_err(|e| e.into_pool_error()),
+        Err(Ok(Error::AlreadyImported(_)))
+    )
 }
