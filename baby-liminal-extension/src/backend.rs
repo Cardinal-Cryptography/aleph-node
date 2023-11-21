260c76f6f6bffbd3427407ef434b1337890de7c3
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-11-21 16:29:11 +0100

    Backend executor is runtime

--- baby-liminal-extension/src/backend.rs
+++ baby-liminal-extension/src/backend.rs
@@ -1,22 +1,24 @@
-use frame_support::{__private::log, pallet_prelude::DispatchError};
+use frame_support::pallet_prelude::DispatchError;
 use log::error;
-use pallet_contracts::chain_extension::{ChainExtension, Environment, Ext, InitState, RetVal};
+use pallet_contracts::chain_extension::{
+    ChainExtension, Environment, Ext, InitState, Result as ChainExtensionResult, RetVal,
+};
 
 use crate::extension_ids::{STORE_KEY_EXT_ID, VERIFY_EXT_ID};
 
 /// The actual implementation of the chain extension. This is the code on the runtime side that will
 /// be executed when the chain extension is called.
-pub struct BabyLiminalChainExtension<PalletConfig> {
-    _config: std::marker::PhantomData<PalletConfig>,
+pub struct BabyLiminalChainExtension<ContractsConfig> {
+    _config: std::marker::PhantomData<ContractsConfig>,
 }
 
-impl<PalletConfig: pallet_contracts::Config> ChainExtension<PalletConfig>
-    for BabyLiminalChainExtension<PalletConfig>
+impl<ContractsConfig: pallet_contracts::Config> ChainExtension<ContractsConfig>
+    for BabyLiminalChainExtension<ContractsConfig>
 {
-    fn call<E: Ext<T = PalletConfig>>(
+    fn call<E: Ext<T = ContractsConfig>>(
         &mut self,
         env: Environment<E, InitState>,
-    ) -> pallet_contracts::chain_extension::Result<RetVal> {
+    ) -> ChainExtensionResult<RetVal> {
         let func_id = env.func_id() as u32;
 
         match func_id {
--- baby-liminal-extension/src/backend_executor.rs
+++ baby-liminal-extension/src/backend_executor.rs
@@ -3,7 +3,7 @@ use frame_system::Config as SystemConfig;
 use pallet_baby_liminal::{
     Config as PalletConfig, Error as PalletError, Pallet, VerificationKeyIdentifier,
 };
-use pallet_contracts::chain_extension::{BufInBufOutState, Environment, Ext};
+use pallet_contracts::Config as ContractsConfig;
 use sp_std::vec::Vec;
 
 /// Generalized pallet executor, that can be mocked for testing purposes.
@@ -27,20 +27,18 @@ pub trait BackendExecutor {
 }
 
 /// Default implementation for the chain extension mechanics.
-impl<'a, 'b, E, T> BackendExecutor for Environment<'a, 'b, E, BufInBufOutState>
+impl<Runtime: SystemConfig + PalletConfig + ContractsConfig> BackendExecutor for Runtime
 where
-    T: SystemConfig + PalletConfig,
-    E: Ext<T = T>,
-    <T as SystemConfig>::RuntimeOrigin: From<Option<AccountId32>>,
+    <Runtime as SystemConfig>::RuntimeOrigin: From<Option<AccountId32>>,
 {
-    type ErrorGenericType = T;
+    type ErrorGenericType = Runtime;
 
     fn store_key(
         depositor: AccountId32,
         identifier: VerificationKeyIdentifier,
         key: Vec<u8>,
     ) -> Result<(), PalletError<Self::ErrorGenericType>> {
-        Pallet::<T>::bare_store_key(Some(depositor).into(), identifier, key)
+        Pallet::<Runtime>::bare_store_key(Some(depositor).into(), identifier, key)
     }
 
     fn verify(
@@ -48,6 +46,6 @@ where
         proof: Vec<u8>,
         public_input: Vec<u8>,
     ) -> Result<(), (PalletError<Self::ErrorGenericType>, Option<Weight>)> {
-        Pallet::<T>::bare_verify(verification_key_identifier, proof, public_input)
+        Pallet::<Runtime>::bare_verify(verification_key_identifier, proof, public_input)
     }
 }
