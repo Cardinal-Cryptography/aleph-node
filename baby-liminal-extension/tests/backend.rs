2355cf75e89a6f7b17232b9b2a9dc430ad852a15
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-11-23 11:11:14 +0100

    First test

--- baby-liminal-extension/tests/backend.rs
+++ baby-liminal-extension/tests/backend.rs
@@ -1,1 +1,13 @@
+#![cfg(feature = "runtime-std")]
+#![allow(incomplete_features)]
+#![feature(adt_const_params)]
+#![feature(generic_const_exprs)]
 
+use aleph_runtime::Runtime as AlephRuntime;
+use baby_liminal_extension::BabyLiminalChainExtension;
+use pallet_contracts::chain_extension::ChainExtension;
+
+#[test]
+fn extension_is_enabled() {
+    assert!(BabyLiminalChainExtension::<AlephRuntime>::enabled())
+}
