f3f725372f54313170253db244fe349f7208f603
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-11-23 14:04:33 +0100

    Handling errors tests

--- baby-liminal-extension/src/backend/tests.rs
+++ baby-liminal-extension/src/backend/tests.rs
@@ -3,6 +3,7 @@ mod environment;
 mod executor;
 
 use aleph_runtime::Runtime as AlephRuntime;
+use pallet_baby_liminal::Error::*;
 use pallet_contracts::chain_extension::{ChainExtension, RetVal};
 
 use crate::{
@@ -11,10 +12,10 @@ use crate::{
         tests::{
             arguments::{store_key_args, verify_args},
             environment::{MockedEnvironment, StandardMode, StoreKeyMode, VerifyMode},
-            executor::{StoreKeyOkayer, VerifyOkayer},
+            executor::{StoreKeyErrorer, StoreKeyOkayer, VerifyErrorer, VerifyOkayer},
         },
     },
-    status_codes::{STORE_KEY_SUCCESS, VERIFY_SUCCESS},
+    status_codes::*,
     BabyLiminalChainExtension,
 };
 
@@ -43,6 +44,60 @@ fn store_key__positive_scenario() {
 
 #[test]
 #[allow(non_snake_case)]
+fn store_key__pallet_says_too_long_vk() {
+    simulate_store_key::<StoreKeyErrorer<{ VerificationKeyTooLong }>>(STORE_KEY_TOO_LONG_KEY)
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn store_key__pallet_says_identifier_in_use() {
+    simulate_store_key::<StoreKeyErrorer<{ IdentifierAlreadyInUse }>>(STORE_KEY_IDENTIFIER_IN_USE)
+}
+
+#[test]
+#[allow(non_snake_case)]
 fn verify__positive_scenario() {
     simulate_verify::<VerifyOkayer>(VERIFY_SUCCESS)
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn verify__pallet_says_proof_deserialization_failed() {
+    simulate_verify::<VerifyErrorer<{ DeserializingProofFailed }>>(VERIFY_DESERIALIZING_PROOF_FAIL)
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn verify__pallet_says_input_deserialization_failed() {
+    simulate_verify::<VerifyErrorer<{ DeserializingPublicInputFailed }>>(
+        VERIFY_DESERIALIZING_INPUT_FAIL,
+    )
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn verify__pallet_says_no_such_vk() {
+    simulate_verify::<VerifyErrorer<{ UnknownVerificationKeyIdentifier }>>(
+        VERIFY_UNKNOWN_IDENTIFIER,
+    )
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn verify__pallet_says_vk_deserialization_failed() {
+    simulate_verify::<VerifyErrorer<{ DeserializingVerificationKeyFailed }>>(
+        VERIFY_DESERIALIZING_KEY_FAIL,
+    )
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn verify__pallet_says_verification_failed() {
+    simulate_verify::<VerifyErrorer<{ VerificationFailed }>>(VERIFY_VERIFICATION_FAIL)
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn verify__pallet_says_incorrect_proof() {
+    simulate_verify::<VerifyErrorer<{ IncorrectProof }>>(VERIFY_INCORRECT_PROOF)
 }
