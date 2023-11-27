38f9633a487716b8e41286c32ac738858410732d
Lukasz Gniecki <lukasgniecki@gmail.com>
2023-11-27 13:57:07 +0100

    documentation

--- finality-aleph/src/metrics/all_block.rs
+++ finality-aleph/src/metrics/all_block.rs
@@ -3,6 +3,7 @@ use primitives::BlockHash;
 use super::{timing::DefaultClock, Checkpoint};
 use crate::TimingBlockMetrics;
 
+/// Wrapper around various block-related metrics.
 #[derive(Clone)]
 pub struct AllBlockMetrics {
     timing_metrics: TimingBlockMetrics<DefaultClock>,
@@ -13,6 +14,7 @@ impl AllBlockMetrics {
         AllBlockMetrics { timing_metrics }
     }
 
+    /// Triggers all contained block metrics.
     pub fn report_block(&self, hash: BlockHash, checkpoint: Checkpoint) {
         self.timing_metrics.report_block(hash, checkpoint);
     }
