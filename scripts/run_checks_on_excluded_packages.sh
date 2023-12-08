15f8b5c1403d201e47dc63bcb021d39531d79750
Marcin <marcin.radecki@cardinals.cc>
2023-12-08 09:44:34 +0100

    Removed more dead code

--- scripts/run_checks_on_excluded_packages.sh
+++ scripts/run_checks_on_excluded_packages.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# set -x
 set -eo pipefail
 
 TOML_FILE="Cargo.toml"
 
 # Read the TOML file and extract the `exclude` entries
 packages=$(awk -F ' *= *' '/^exclude *= *\[/ {found=1} found && /^\]$/ {found=0} found' "$TOML_FILE")
-
 packages="$(echo ${packages} | sed 's/[][,]/ /g' | sed 's/\s\+/\n/g' | sed '/^$/d')"
 
 # Remove leading and trailing whitespace, and quotes from the entries
@@ -30,16 +28,8 @@ for p in ${packages[@]}; do
       --name ink_builder \
       --platform linux/amd64 \
       --rm public.ecr.aws/p6e8q1z1/ink-dev:2.0.0 cargo contract check
-
-  elif [[ "$p" == "baby-liminal-extension" ]]; then
-    # cargo clippy --release --no-default-features --features substrate \
-      #  --target wasm32-unknown-unknown -- --no-deps -D warnings
-    :
-  elif [[ "$p" == "pallets/baby-liminal" ]]; then
-    cargo test --features runtime-benchmarks
   else
     cargo clippy -- --no-deps -D warnings
   fi
-
   popd
 done
