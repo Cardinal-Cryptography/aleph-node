a34629cfe61266c08a5e4b55620154a56054a288
Marcin <marcin.radecki@cardinals.cc>
2024-02-27 09:23:19 +0100

    rustfmt

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -1186,11 +1186,10 @@ impl_runtime_apis! {
     }
 
     impl pallet_staking_runtime_api::StakingApi<Block, Balance> for Runtime {
-		fn nominations_quota(balance: Balance) -> u32 {
-			MAX_NOMINATORS
-		}
-	}
-
+        fn nominations_quota(balance: Balance) -> u32 {
+            MAX_NOMINATORS
+        }
+    }
 
     impl pallet_contracts::ContractsApi<Block, AccountId, Balance, AlephBlockNumber, Hash, EventRecord>
         for Runtime
