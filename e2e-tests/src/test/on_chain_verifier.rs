aada102d3966101d9ccb305c2ecb248f95108f89
Piotr Mikołajczyk <piomiko41@gmail.com>
2024-03-05 10:49:20 +0100

    ink without default features, wat contract

--- e2e-tests/src/test/on_chain_verifier.rs
+++ e2e-tests/src/test/on_chain_verifier.rs
@@ -1,4 +1,9 @@
-use aleph_client::{pallet_feature_control::Feature, pallets::feature_control::FeatureControlApi};
+use aleph_client::{
+    pallet_feature_control::Feature,
+    pallets::{contract::ContractsUserApi, feature_control::FeatureControlApi},
+    sp_weights::weight_v2::Weight,
+    TxStatus,
+};
 use anyhow::Result;
 
 use crate::config::setup_test;
@@ -6,6 +11,10 @@ use crate::config::setup_test;
 const FEATURE: Feature = Feature::OnChainVerifier;
 const IS_ON: bool = true;
 const IS_OFF: bool = false;
+const GAS_LIMIT: Weight = Weight {
+    ref_time: 5_000_000_000,
+    proof_size: 0,
+};
 
 #[tokio::test]
 pub async fn fresh_chain_has_verifier_enabled() -> Result<()> {
@@ -13,10 +22,36 @@ pub async fn fresh_chain_has_verifier_enabled() -> Result<()> {
     let conn = config.get_first_signed_connection().await;
 
     assert_feature_status(IS_ON, &conn).await;
+    assert_contracts_can_use_verifier(&conn).await;
 
     Ok(())
 }
 
 async fn assert_feature_status<Conn: FeatureControlApi>(active: bool, c: &Conn) {
     assert_eq!(c.is_feature_active(FEATURE, None).await, active)
+}
+
+async fn assert_contracts_can_use_verifier<Conn: ContractsUserApi>(c: &Conn) {
+    c.instantiate_with_code(
+        compile_contract(),
+        0,
+        GAS_LIMIT,
+        None,
+        vec![],
+        vec![],
+        TxStatus::Finalized,
+    )
+    .await
+    .unwrap();
+}
+
+fn compile_contract() -> Vec<u8> {
+    let path = [
+        std::env::var("CARGO_MANIFEST_DIR")
+            .as_deref()
+            .unwrap_or("e2e-tests"),
+        "/resources/snark_verifying.wat",
+    ]
+    .concat();
+    wat::parse_file(path).expect("Failed to parse wat file")
 }
