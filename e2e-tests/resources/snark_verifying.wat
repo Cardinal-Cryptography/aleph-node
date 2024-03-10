882690b92a592e72bcf365242362b75572c207ee
Piotr Mikołajczyk <piomiko41@gmail.com>
2024-03-10 11:44:00 +0100

    Working contract

--- e2e-tests/resources/snark_verifying.wat
+++ e2e-tests/resources/snark_verifying.wat
@@ -1,43 +1,38 @@
-;; Call chain extension by passing through input and output of this contract
 (module
 	(import "seal0" "call_chain_extension"
 		(func $call_chain_extension (param i32 i32 i32 i32 i32) (result i32))
 	)
 	(import "seal0" "seal_input" (func $seal_input (param i32 i32)))
+	(import "seal0" "seal_return" (func $seal_return (param i32 i32 i32)))
 	(import "env" "memory" (memory 16 16))
 
-	(func $assert (param i32)
-		(block $ok
-			(br_if $ok (get_local 0))
-			(unreachable)
-		)
-	)
+	;; bytes [0, 4)  reserved for the length of input
+	;; bytes [4, 38) reserved for the input to be read by the $seal_input function
+	;;   - 4  bytes for extension method id
+	;;   - 32 bytes for verifying key hash
+	;;   - 2  bytes for empty proof and empty public input
+	(data (i32.const 0) "\26")
 
-	;; [0, 4) len of input output
-	(data (i32.const 0) "\08")
-
-	;; [4, 12) buffer for input
-
-	;; [12, 48) len of output buffer
-	(data (i32.const 12) "\20")
-
-	;; [16, inf) buffer for output
-
+	;; function for instantiating the contract
 	(func (export "deploy"))
 
+  ;; function for calling the contract
 	(func (export "call")
-		(call $seal_input (i32.const 4) (i32.const 0))
+		(call $seal_input
+		  (i32.const 4) ;; input_ptr
+		  (i32.const 0) ;; input_len_ptr
+    )
 
-		;; the chain extension passes through the input and returns it as output
 		(call $call_chain_extension
 			(i32.load (i32.const 4))	;; id
-			(i32.const 4)				;; input_ptr
-			(i32.load (i32.const 0))	;; input_len
-			(i32.const 16)				;; output_ptr
-			(i32.const 12)				;; output_len_ptr
+			(i32.const 8)				      ;; input_ptr
+			(i32.const 34)          	;; input_len
+			(i32.const 0)             ;; output_ptr
+			(i32.const 0)				      ;; output_len_ptr
 		)
 
-		;; the chain extension passes through the id
-		(call $assert (i32.eq (i32.load (i32.const 4))))
+		drop
+
+		(call $seal_return (i32.const 0) (i32.const 0) (i32.const 0))
 	)
 )
--- e2e-tests/src/test/on_chain_verifier.rs
+++ e2e-tests/src/test/on_chain_verifier.rs
@@ -2,9 +2,11 @@ use aleph_client::{
     pallet_feature_control::Feature,
     pallets::{contract::ContractsUserApi, feature_control::FeatureControlApi},
     sp_weights::weight_v2::Weight,
+    utility::BlocksApi,
     TxStatus,
 };
 use anyhow::Result;
+use codec::Encode;
 
 use crate::config::setup_test;
 
@@ -12,9 +14,16 @@ const FEATURE: Feature = Feature::OnChainVerifier;
 const IS_ON: bool = true;
 const IS_OFF: bool = false;
 const GAS_LIMIT: Weight = Weight {
-    ref_time: 5_000_000_000,
-    proof_size: 0,
+    ref_time: 50_000_000_000,
+    proof_size: 50_000_000_000,
 };
+
+#[derive(Debug, Encode)]
+pub struct VerifyArgs {
+    pub verification_key_hash: sp_core::H256,
+    pub proof: Vec<u8>,
+    pub public_input: Vec<u8>,
+}
 
 #[tokio::test]
 pub async fn fresh_chain_has_verifier_enabled() -> Result<()> {
@@ -22,7 +31,7 @@ pub async fn fresh_chain_has_verifier_enabled() -> Result<()> {
     let conn = config.get_first_signed_connection().await;
 
     assert_feature_status(IS_ON, &conn).await;
-    assert_contracts_can_use_verifier(&conn).await;
+    assert_contracts_can_use_verifier(&conn).await?;
 
     Ok(())
 }
@@ -31,18 +40,38 @@ async fn assert_feature_status<Conn: FeatureControlApi>(active: bool, c: &Conn) 
     assert_eq!(c.is_feature_active(FEATURE, None).await, active)
 }
 
-async fn assert_contracts_can_use_verifier<Conn: ContractsUserApi>(c: &Conn) {
-    c.instantiate_with_code(
-        compile_contract(),
+async fn assert_contracts_can_use_verifier<Conn: ContractsUserApi + BlocksApi>(
+    c: &Conn,
+) -> Result<()> {
+    let tx_info = c
+        .instantiate_with_code(
+            compile_contract(),
+            0,
+            GAS_LIMIT,
+            None,
+            vec![],
+            vec![],
+            TxStatus::Finalized,
+        )
+        .await?;
+    let address = c
+        .get_tx_events(tx_info)
+        .await?
+        .find_first::<aleph_client::api::contracts::events::Instantiated>()?
+        .unwrap()
+        .contract;
+
+    c.call(
+        address.0,
         0,
         GAS_LIMIT,
         None,
-        vec![],
-        vec![],
+        extension_input(),
         TxStatus::Finalized,
     )
-    .await
-    .unwrap();
+    .await?;
+
+    Ok(())
 }
 
 fn compile_contract() -> Vec<u8> {
@@ -54,4 +83,19 @@ fn compile_contract() -> Vec<u8> {
     ]
     .concat();
     wat::parse_file(path).expect("Failed to parse wat file")
+}
+
+fn extension_input() -> Vec<u8> {
+    (41u32 << 16 | 0u32)
+        .to_le_bytes()
+        .into_iter()
+        .chain(
+            VerifyArgs {
+                verification_key_hash: sp_core::H256::zero(),
+                proof: vec![],
+                public_input: vec![],
+            }
+            .encode(),
+        )
+        .collect()
 }
