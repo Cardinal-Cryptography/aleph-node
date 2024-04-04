aee4422497964801547a4cf38e05ba1dd314c1b9
Jan Koscisz <koscis.j@gmail.com>
2024-04-04 14:40:27 +0200

    versions and tests

--- bin/runtime/src/lib.rs
+++ bin/runtime/src/lib.rs
@@ -97,7 +97,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("aleph-node"),
     impl_name: create_runtime_str!("aleph-node"),
     authoring_version: 1,
-    spec_version: 71,
+    spec_version: 72,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 18,
