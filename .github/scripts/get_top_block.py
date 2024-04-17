0881a6b85f08ff024770fb398253be6728ac4f06
Marcin <marcin.radecki@cardinals.cc>
2024-04-17 09:34:11 +0200

    A0-4265: Implement retry connection mechanism in weekly snapshot tests

--- .github/scripts/get_top_block.py
+++ .github/scripts/get_top_block.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+import sys
 from sys import argv
+import time
+
 from substrateinterface import SubstrateInterface
-chain = SubstrateInterface(url=argv[1])
-number = chain.get_block()['header']['number']
-print(number)
+connection_attempts = 0
+
+while connection_attempts < 30:
+    try:
+        chain = SubstrateInterface(url=argv[1])
+        number = chain.get_block()['header']['number']
+        print(number)
+        sys.exit(0)
+    except ConnectionRefusedError:
+        connection_attempts += 1
+        time.sleep(10)
+raise ConnectionRefusedError
+
