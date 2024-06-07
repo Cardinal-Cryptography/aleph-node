5c7801443e6913f0c2984ec4ae04f178b217d3e5
Marcin <marcin.radecki@cardinals.cc>
2024-06-07 15:24:04 +0200

    L1-66: Script changes for fixing consumers counter

--- scripts/accounts-invariants/utils.py
+++ scripts/accounts-invariants/utils.py
@@ -16,3 +16,16 @@ def chunks(list_of_elements, n):
     """
     for i in range(0, len(list_of_elements), n):
         yield list_of_elements[i:i + n]
+
+
+def format_balance(chain_connection, amount):
+    """
+    Helper method to display underlying U128 Balance type in human-readable form
+    :param chain_connection: WS connection handler (for retrieving token symbol metadata)
+    :param amount: ammount to be formatted
+    :return: balance in human-readable form
+    """
+    decimals = chain_connection.token_decimals or 12
+    amount = format(amount / 10 ** decimals)
+    token = chain_connection.token_symbol
+    return f"{amount} {token}"
