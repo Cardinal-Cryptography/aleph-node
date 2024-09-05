7decdf7214c8d9b6c04d19bfa323e020475624f3
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2024-09-05 15:04:01 +0200

    FIX: rush binary (synthetic-network) was copied to `/` instead of `/opt/lib` in `Dockerfile.synthetic_network.build`.

--- scripts/synthetic-network/build_synthetic-network.sh
+++ scripts/synthetic-network/build_synthetic-network.sh
@@ -15,6 +15,6 @@ log "building docker image for synthetic-network"
 docker build --tag syntheticnet --file docker/Dockerfile.synthetic_network.build scripts/synthetic-network/vendor/synthetic-network
 
 log "building docker image for aleph-node that supports synthetic-network"
-docker build -t aleph-node:syntheticnet -f docker/Dockerfile.synthetic_network .
+docker build --tag aleph-node:syntheticnet --file docker/Dockerfile.synthetic_network .
 
 exit 0
