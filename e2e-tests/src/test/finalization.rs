fae0ab410e7bd4de1c9e6b7d360349ac8308babb
Łukasz Lachowski <lukasz.lachowski@cardinals.cc>
2024-11-05 14:47:49 +0100

    added FINALIZATION_WAIT param for the `finalization` e2e-test

--- e2e-tests/src/test/finalization.rs
+++ e2e-tests/src/test/finalization.rs
@@ -11,6 +11,7 @@ use crate::config::setup_test;
 pub async fn finalization() -> anyhow::Result<()> {
     let config = setup_test();
     let connection = config.create_root_connection().await;
+    let wait_blocks = config.test_case_params.finalization_wait.unwrap_or(1);
 
     let finalized = connection.get_finalized_block_hash().await?;
     info!("Highest finalized block hash = {finalized}");
@@ -20,10 +21,11 @@ pub async fn finalization() -> anyhow::Result<()> {
         .ok_or(anyhow!(
             "Failed to retrieve block number for hash {finalized:?}"
         ))?;
-    info!("Waiting for block {} to be finalized", finalized_number + 1);
+    let wait_for_block = finalized_number.saturating_add(wait_blocks);
+    info!("Waiting for block {} to be finalized", wait_for_block);
 
     connection
-        .wait_for_block(|n| n > finalized_number, BlockStatus::Finalized)
+        .wait_for_block(|n| n > wait_for_block, BlockStatus::Finalized)
         .await;
 
     Ok(())
