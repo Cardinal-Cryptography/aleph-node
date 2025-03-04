dcae42355c10a6a4909c85fe612d5169a926003d
Marcin <Marcin.Radecki@cardinals.cc>
2025-03-04 12:24:50 +0100

    Increased buffer to 500

--- clique/src/lib.rs
+++ clique/src/lib.rs
@@ -26,7 +26,7 @@ pub use crypto::{PublicKey, SecretKey};
 pub use rate_limiting::{RateLimitingDialer, RateLimitingListener};
 pub use service::{Service, SpawnHandleExt, SpawnHandleT};
 
-const SEND_DATA_BUFFER: usize = 100;
+const SEND_DATA_BUFFER: usize = 500;
 
 const LOG_TARGET: &str = "network-clique";
 /// A basic alias for properties we expect basic data to satisfy.
