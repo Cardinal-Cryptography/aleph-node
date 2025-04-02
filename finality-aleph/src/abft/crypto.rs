2e9f2832040b95411ad26726d1c8224f971cb218
timorleph <tomasz.kisielewski@cardinals.cc>
2025-04-02 14:26:15 +0200

    New ABFT version

--- finality-aleph/src/abft/crypto.rs
+++ finality-aleph/src/abft/crypto.rs
@@ -48,14 +48,18 @@ impl Keychain {
     }
 }
 
-// Currently the traits for legacy and current match, so only one implementation needed.
 impl legacy_aleph_bft::Index for Keychain {
     fn index(&self) -> legacy_aleph_bft::NodeIndex {
         Keychain::index(self).into()
     }
 }
 
-// Currently the traits for legacy and current match, so only one implementation needed.
+impl current_aleph_bft::Index for Keychain {
+    fn index(&self) -> current_aleph_bft::NodeIndex {
+        Keychain::index(self).into()
+    }
+}
+
 impl legacy_aleph_bft::Keychain for Keychain {
     type Signature = Signature;
 
@@ -72,7 +76,22 @@ impl legacy_aleph_bft::Keychain for Keychain {
     }
 }
 
-// Currently the traits for legacy and current match, so only one implementation needed.
+impl current_aleph_bft::Keychain for Keychain {
+    type Signature = Signature;
+
+    fn node_count(&self) -> current_aleph_bft::NodeCount {
+        Keychain::node_count(self).into()
+    }
+
+    fn sign(&self, msg: &[u8]) -> Signature {
+        Keychain::sign(self, msg)
+    }
+
+    fn verify(&self, msg: &[u8], sgn: &Signature, index: current_aleph_bft::NodeIndex) -> bool {
+        Keychain::verify(self, msg, sgn, index)
+    }
+}
+
 impl legacy_aleph_bft::MultiKeychain for Keychain {
     // Using `SignatureSet` is slow, but Substrate has not yet implemented aggregation.
     // We probably should do this for them at some point.
@@ -84,6 +103,30 @@ impl legacy_aleph_bft::MultiKeychain for Keychain {
         index: legacy_aleph_bft::NodeIndex,
     ) -> Self::PartialMultisignature {
         legacy_aleph_bft::PartialMultisignature::add_signature(
+            SignatureSet(aleph_bft_crypto::SignatureSet::with_size(
+                aleph_bft_crypto::Keychain::node_count(self),
+            )),
+            signature,
+            index,
+        )
+    }
+
+    fn is_complete(&self, msg: &[u8], partial: &Self::PartialMultisignature) -> bool {
+        Keychain::is_complete(self, msg, partial)
+    }
+}
+
+impl current_aleph_bft::MultiKeychain for Keychain {
+    // Using `SignatureSet` is slow, but Substrate has not yet implemented aggregation.
+    // We probably should do this for them at some point.
+    type PartialMultisignature = SignatureSet<Signature>;
+
+    fn bootstrap_multi(
+        &self,
+        signature: &Signature,
+        index: current_aleph_bft::NodeIndex,
+    ) -> Self::PartialMultisignature {
+        current_aleph_bft::PartialMultisignature::add_signature(
             SignatureSet(aleph_bft_crypto::SignatureSet::with_size(
                 aleph_bft_crypto::Keychain::node_count(self),
             )),
